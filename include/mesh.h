94b058695d343709b0e02f310d91cd665ac2ce7b
Arnaud Botella <botella@gocad.org>
2014-10-22 22:30:52 +0200

    First commit

---
+++ include/mesh.h
@@ -0,0 +1,108 @@
+/*[
+ * Association Scientifique pour la Geologie et ses Applications (ASGA)
+ * Copyright (c) 1993-2013 ASGA. All Rights Reserved.
+ *
+ * This program is a Trade Secret of the ASGA and it is not to be:
+ * - reproduced, published, or disclosed to other,
+ * - distributed or displayed,
+ * - used for purposes or on Sites other than described
+ *   in the GOCAD Advancement Agreement,
+ * without the prior written authorization of the ASGA. Licencee
+ * agrees to attach or embed this Notice on all copies of the program,
+ * including partial copies or modified versions thereof.
+ ]*/
+
+#ifndef __GRGMESH_MESH__
+#define __GRGMESH_MESH__
+
+#include <common.h>
+#include <vecn.h>
+
+#include <vector>
+
+namespace GRGMesh {
+
+    template< int DIM = 3 > class GRGMESH_API Mesh {
+
+    public:
+        typedef vecn< DIM > vec ;
+
+        void clear()
+        {
+            vertices_.clear() ;
+            vertex_indices_.clear() ;
+        }
+
+        uint64 nb_v() const
+        {
+            return vertices_.size() ;
+        }
+        virtual uint64 nb_c() const = 0 ;
+
+        /*!
+         * Get the vertex first index of a cell
+         * @param c Cell index
+         */
+        virtual uint64 cell_begin( uint64 c ) const = 0 ;
+        /*!
+         * Get the vertex last index of a cell (the first of the next cell)
+         * @param c Cell index
+         */
+        virtual uint64 cell_end( uint64 c ) const = 0 ;
+        virtual uint64 nb_vertices_in_cell( uint64 c ) const = 0 ;
+        virtual uint64 nb_facets_in_cell( uint64 c ) const = 0 ;
+        virtual uint64 nb_vertices_in_cell_facet( uint64 c, uint64 f ) const = 0 ;
+
+        uint64 cell_vertex_index( uint64 c, uint64 v ) const
+        {
+            grgmesh_debug_assert( v < nb_vertices_in_cell( c ) ) ;
+            return vertex_indices_[cell_begin( c ) + v] ;
+        }
+        const vec& cell_vertex( uint64 c, uint64 v ) const
+        {
+            grgmesh_debug_assert( v < nb_vertices_in_cell( c ) ) ;
+            return vertices_[vertex_indices_[cell_begin( c ) + v]] ;
+        }
+
+
+        virtual CellType cell_type( uint64 c ) const = 0 ;
+        virtual CellDescriptor* cell_descriptor( uint64 c ) const = 0 ;
+        uint64 cell_facet_vertex( uint64 c, uint64 f, uint64 v ) const
+        {
+            CellDescriptor* desc = cell_descriptor( c ) ;
+            grgmesh_debug_assert( f < desc->nb_facets ) ;
+            grgmesh_debug_assert( v < desc->nb_vertices_in_facet[f] ) ;
+            return cell_vertex( c, desc->facet[f][v] ) ;
+        }
+
+        vec cell_centroid( uint64 c ) const
+        {
+            vec result ;
+            for( uint64 i = cell_begin( c ); i < cell_end( c ); i++ ) {
+                result += cell_vertex( c, i ) ;
+            }
+            return result / nb_vertices_in_cell( c );
+        }
+
+    private:
+        Mesh()
+        {
+        }
+        ~Mesh()
+        {
+        }
+        void copy( const Mesh& rhs )
+        {
+        }
+
+    private:
+        ///List of the vertices
+        std::vector< vec > vertices_ ;
+        ///Mapping between the list of the vertices in a cell and the actual vertices
+        std::vector< uint64 > vertex_indices_ ;
+
+    } ;
+
+}
+
+#endif
