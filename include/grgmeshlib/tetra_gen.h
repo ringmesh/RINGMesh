914c294fed715e115c9cc288dfefbc65819c1002
Arnaud Botella <botella@gocad.org>
2014-10-24 11:01:18 +0200

    Add TetGen

---
+++ include/grgmeshlib/tetra_gen.h
@@ -0,0 +1,183 @@
+/*[
+* Association Scientifique pour la Geologie et ses Applications (ASGA)
+* Copyright (c) 1993-2013 ASGA. All Rights Reserved.
+*
+* This program is a Trade Secret of the ASGA and it is not to be:
+* - reproduced, published, or disclosed to other,
+* - distributed or displayed,
+* - used for purposes or on Sites other than described
+*   in the GOCAD Advancement Agreement,
+* without the prior written authorization of the ASGA. Licencee
+* agrees to attach or embed this Notice on all copies of the program,
+* including partial copies or modified versions thereof.
+]*/
+
+#ifndef __GRGMESH_TETRA_GEN__
+#define __GRGMESH_TETRA_GEN__
+
+#include <grgmeshlib/common.h>
+#include <grgmeshlib/smart_pointer.h>
+#include <grgmeshlib/vecn.h>
+#include <grgmeshlib/utils.h>
+#include <grgmeshlib/mixed_mesh.h>
+
+#include <third_party/tetgen.h>
+
+#include <vector>
+
+#ifdef USE_MG_TETRA
+    extern "C" {
+        #include <meshgems/meshgems.h>
+        #include <meshgems/tetra.h>
+    }
+#endif
+
+namespace GRGMesh {
+
+    class BoundaryModelElement ;
+    class MixedMesh ;
+    class TetraGen ;
+
+    typedef SmartPointer< TetraGen > TetraGen_var ;
+
+    static const std::vector< vec3 > vector_vec3 ;
+    static const std::vector< std::vector< Edge > > vector_edge ;
+    class GRGMESH_API TetraGen : public Counted {
+    public:
+        virtual ~TetraGen() {} ;
+        static TetraGen_var instantiate(
+            const Tetra_method& method,
+            MixedMesh& tetmesh,
+            const BoundaryModelElement* region,
+            bool add_steiner_points = true,
+            const std::vector< vec3 >& internal_vertices = vector_vec3,
+            const std::vector< std::vector< Edge > >& well_vertices = vector_edge,
+            MixedMesh* background = nil ) ;
+
+        virtual bool tetrahedralize() = 0 ;
+
+        unsigned int nb_points() const { return points_.size() ; }
+        unsigned int nb_internal_points() const { return internal_points_.size() ; }
+        unsigned int nb_total_points() const { return nb_points() + nb_internal_points() ; }
+        unsigned int nb_triangles() const { return triangles_.size() / 3 ; }
+        unsigned int point_index( unsigned int f, unsigned int v ) const { return triangles_[3*f+v] ; }
+        const vec3& point( unsigned int f, unsigned int v ) const { return points_[triangles_[3*f+v]] ; }
+        const vec3& point( unsigned int v ) const { return points_[v] ; }
+        int surface_id( unsigned int f ) const {
+            for( unsigned int i = 1; i < surface_id_.size(); i++ ) {
+                if( f < surface_ptr_[i] ) return surface_id_[i-1] ;
+            }
+            return  surface_id_.back() ;
+        }
+        int* surface_id_ptr( unsigned int f ) {
+            for( unsigned int i = 1; i < surface_id_.size(); i++ ) {
+                if( f < surface_ptr_[i] ) return &surface_id_[i-1] ;
+            }
+            return  &surface_id_.back() ;
+        }
+        int well_id( unsigned int f ) const {
+            for( unsigned int i = 1; i < well_ptr_.size(); i++ ) {
+                if( f < well_ptr_[i] ) return i-1 ;
+            }
+            return  well_ptr_.size()-1 ;
+        }
+
+    protected:
+        TetraGen(
+            MixedMesh& tetmesh,
+            const BoundaryModelElement* region,
+            const std::vector< vec3 >& internal_vertices,
+            const std::vector< std::vector< Edge > >& well_edges,
+            MixedMesh* background ) ;
+
+        void initialize_sorage( unsigned int nb_points, unsigned int nb_tets ) ;
+        void set_point( unsigned int index, double* point ) ;
+        void set_tetra( unsigned int index, int* tet ) ;
+        void set_tetra_adjacent( unsigned int index, unsigned int face, int adj ) ;
+        void set_face_marker(
+            unsigned int tet1,
+            unsigned int tet2,
+            unsigned int marker ) ;
+        void set_tetra_face_marker(
+            unsigned int tet,
+            unsigned int adj,
+            unsigned int marker ) ;
+
+        void store_edge_attrib() const ;
+
+    protected:
+        std::vector< vec3 > points_ ;
+        std::vector< vec3 > internal_points_ ;
+        std::vector< Edge > well_edges_ ;
+        std::vector< unsigned int > well_ptr_ ;
+        std::vector< int > well_indices_ ;
+        std::vector< int > triangles_ ;
+        std::vector< int > surface_id_ ;
+        std::vector< unsigned int > surface_ptr_ ;
+        MixedMesh& tetmesh_ ;
+        MixedMeshMutator tetmesh_mutator_ ;
+        double resolution_ ;
+        MixedMesh* background_ ;
+    } ;
+
+
+    class GRGMESH_API TetraGen_TetGen: public TetraGen {
+    public:
+        TetraGen_TetGen(
+            MixedMesh& tetmesh,
+            const BoundaryModelElement* region,
+            bool add_steiner_points,
+            const std::vector< vec3 >& internal_vertices,
+            const std::vector< std::vector< Edge > >& well_vertices,
+            MixedMesh* background ) ;
+        virtual ~TetraGen_TetGen() {} ;
+
+        virtual bool tetrahedralize() ;
+
+    private:
+        tetgenio tetgen_input_ ;
+        tetgenio tetgen_output_ ;
+        tetgenio tetgen_background_ ;
+        tetgenbehavior tetgen_args_ ;
+    } ;
+
+#ifdef USE_MG_TETRA
+    class VORTEX_API TetraGen_MG_Tetra: public TetraGen {
+    public:
+        TetraGen_MG_Tetra(
+            TetraMesh& tetmesh,
+            const BoundaryModelElement* region,
+            bool add_steiner_points,
+            const std::vector< vec3 >& internal_vertices,
+            const std::vector< std::vector< Edge > >& well_vertices,
+            TetraMesh* background ) ;
+        virtual ~TetraGen_MG_Tetra() ;
+
+        virtual bool tetrahedralize() ;
+
+        static status_t my_message_cb( message_t * msg, void *user_data ) ;
+        static status_t get_size_value(
+            meshgems_integer i,
+            meshgems_real* size,
+            void *user_data ) {
+            *size = static_cast< TetraGen_MG_Tetra* >( user_data )->get_resolution_value( i ) ;
+            return STATUS_OK ;
+        }
+
+    private:
+        double get_resolution_value( int i ) { return background_->resolution( i ) ; }
+
+    private:
+        bool add_steiner_points_ ;
+        context_t* context_ ;
+        mesh_t* mesh_input_ ;
+        mesh_t* mesh_output_ ;
+        mesh_t* mesh_background_ ;
+        sizemap_t* sizemap_ ;
+        tetra_session_t* tms_ ;
+    } ;
+#endif
+
+}
+
+#endif
--- src/grgmeshlib/utils.cpp
+++ src/grgmeshlib/utils.cpp
@@ -13,6 +13,8 @@
 ]*/
 
 #include <grgmeshlib/utils.h>
+#include <grgmeshlib/boundary_model_element.h>
+#include <grgmeshlib/mixed_mesh.h>
 
 #include <iostream>
 #include <sstream>
@@ -25,13 +27,9 @@ namespace GRGMesh {
     {
         int nb_points = points_.size() ;
         indices_.resize( nb_points ) ;
-        ann_points_ = annAllocPts( nb_points, 3 ) ;
         for( unsigned int i = 0; i < nb_points; i++ ) {
             indices_[i] = i ;
-            std::copy( points_[i].data(), points_[i].data() + 3,
-                &ann_points_[i][0] ) ;
         }
-        ann_tree_ = new ANNkd_tree( &ann_points_[0], nb_points, 3 ) ;
     }
 
     static bool inexact_equal( const vec3& v1, const vec3& v2 ) {
@@ -42,27 +40,6 @@ namespace GRGMesh {
             }
         }
         return true ;
-    }
-
-    bool MakeUnique::get_colocated(
-        vec3& v,
-        std::vector< unsigned int >& result,
-        int nb_neighbors ) const
-    {
-        nb_neighbors = std::min( nb_neighbors, ann_tree_->nPoints() ) ;
-        std::vector< int > neighbors( nb_neighbors ) ;
-        ANNdistArray dist = new ANNdist[nb_neighbors] ;
-        ann_tree_->annkSearch( v.data(), nb_neighbors, &neighbors[0], dist ) ;
-        delete[] dist ;
-
-        result.clear() ;
-        for( int i = 0; i < neighbors.size(); ++i ) {
-            if( inexact_equal( v, vec3( ann_points_[neighbors[i]] ) ) ) {
-                result.push_back( neighbors[i] ) ;
-            }
-        }
-
-        return !result.empty() ;
     }
 
     void MakeUnique::unique_points( std::vector< vec3 >& results ) const
@@ -81,19 +58,17 @@ namespace GRGMesh {
 
     void MakeUnique::unique( int nb_neighbors )
     {
+        ColocaterANN ann( points_ ) ;
         for( unsigned int i = 0; i < indices_.size(); i++ ) {
             if( indices_[i] != i ) continue ;
             std::vector< unsigned int > results ;
-            int cur_neighbor = 0 ;
-            do {
-                cur_neighbor += nb_neighbors ;
-                get_colocated( points_[i], results, cur_neighbor ) ;
-            } while( results.size() == cur_neighbor ) ;
-            unsigned int id = *std::min_element( results.begin(),
-                results.end() ) ;
-            for( unsigned int j = 0; j < results.size(); j++ ) {
-                if( id == results[j] ) continue ;
-                indices_[results[j]] = id ;
+            if( ann.get_colocated( points_[i], results, nb_neighbors ) ) {
+                unsigned int id = *std::min_element( results.begin(),
+                    results.end() ) ;
+                for( unsigned int j = 0; j < results.size(); j++ ) {
+                    if( id == results[j] ) continue ;
+                    indices_[results[j]] = id ;
+                }
             }
         }
         int offset = 0 ;
@@ -105,6 +80,149 @@ namespace GRGMesh {
                 indices_[i] -= offset ;
             }
         }
+    }
+    void MakeUnique::add_edges( const std::vector< Edge >& points ) {
+        int offset = points_.size() ;
+        points_.resize( offset+(points.size()*2) ) ;
+        indices_.resize( offset+(points.size()*2) ) ;
+        for( unsigned int p = 0; p < points.size(); p++ ) {
+            points_[offset] = points[p].value( 0 ) ;
+            indices_[offset] = offset ;
+            offset++ ;
+            points_[offset] = points[p].value( 1 ) ;
+            indices_[offset] = offset ;
+            offset++ ;
+        }
+    }
+    void MakeUnique::add_points( const std::vector< vec3 >& points ) {
+        int offset = points_.size() ;
+        points_.resize( offset+points.size() ) ;
+        indices_.resize( offset+points.size() ) ;
+        for( unsigned int p = 0; p < points.size(); p++, offset++ ) {
+            points_[offset] = points[p] ;
+            indices_[offset] = offset ;
+        }
+    }
+
+    ColocaterANN::ColocaterANN( const SurfacePart& mesh )
+    {
+        int nb_vertices = mesh.nb_points() ;
+        ann_points_ = annAllocPts( nb_vertices, 3 ) ;
+        for( unsigned int i = 0; i < mesh.nb_points(); i++ ) {
+            std::copy( mesh.point( i ).data(), mesh.point( i ).data() + 3,
+                &ann_points_[i][0] ) ;
+        }
+        ann_tree_ = new ANNkd_tree( &ann_points_[0], nb_vertices, 3 ) ;
+    }
+
+    ColocaterANN::ColocaterANN( const ContactPart& mesh )
+    {
+        int nb_vertices = mesh.nb_points() ;
+        ann_points_ = annAllocPts( nb_vertices, 3 ) ;
+        for( unsigned int i = 0; i < mesh.nb_points(); i++ ) {
+            std::copy( mesh.point( i ).data(), mesh.point( i ).data() + 3,
+                &ann_points_[i][0] ) ;
+        }
+        ann_tree_ = new ANNkd_tree( &ann_points_[0], nb_vertices, 3 ) ;
+    }
+
+    ColocaterANN::ColocaterANN( const MixedMesh& mesh )
+    {
+        int nb_vertices = mesh.nb_vertices() ;
+        ann_points_ = annAllocPts( nb_vertices, 3 ) ;
+        for( unsigned int i = 0; i < nb_vertices; i++ ) {
+            vec3 vertex = mesh.vertex( i ) ;
+            std::copy( vertex.data(), vertex.data() + 3, &ann_points_[i][0] ) ;
+        }
+        ann_tree_ = new ANNkd_tree( &ann_points_[0], nb_vertices, 3 ) ;
+    }
+
+    ColocaterANN::ColocaterANN( const MixedMesh& mesh, bool use_surface_id )
+    {
+        grgmesh_assert_not_reached ;
+        //todo
+        /*
+        std::vector< vec3 > barycenters ;
+        barycenters.reserve( 4*mesh.nb_tetra() ) ;
+        for( unsigned int i = 0; i < mesh.nb_tetra(); i++ ) {
+            for( unsigned int f = 0; f < 4; f++ ) {
+                if( mesh.surface_id( i, f ) != -1 ) {
+                    barycenters.push_back(
+                        VorteXUtils::barycenter( mesh.vertex( i, f2e[f][0] ),
+                            mesh.vertex( i, f2e[f][1] ),
+                            mesh.vertex( i, f2e[f][2] ) ) ) ;
+                    mapped_indices_.push_back( 4*i + f ) ;
+                }
+            }
+        }
+        ann_points_ = annAllocPts( barycenters.size(), 3 ) ;
+        for( unsigned int i = 0; i < barycenters.size(); i++ ) {
+            std::copy( barycenters[i].data(), barycenters[i].data() + 3, &ann_points_[i][0] ) ;
+        }
+        ann_tree_ = new ANNkd_tree( &ann_points_[0], barycenters.size(), 3 ) ;
+        */
+    }
+
+    ColocaterANN::ColocaterANN( const std::vector< vec3 >& vertices )
+    {
+        int nb_vertices = vertices.size() ;
+        ann_points_ = annAllocPts( nb_vertices, 3 ) ;
+        for( unsigned int i = 0; i < nb_vertices; i++ ) {
+            std::copy( vertices[i].data(), vertices[i].data() + 3, &ann_points_[i][0] ) ;
+        }
+        ann_tree_ = new ANNkd_tree( &ann_points_[0], nb_vertices, 3 ) ;
+    }
+
+    ColocaterANN::ColocaterANN( const std::vector< Edge >& edges )
+    {
+        int nb_vertices = edges.size() ;
+        ann_points_ = annAllocPts( nb_vertices, 3 ) ;
+        for( unsigned int i = 0; i < nb_vertices; i++ ) {
+            vec3 barycenter( ( edges[i].value( 0 ) + edges[i].value( 1 ) ) / 2.0 ) ;
+            std::copy( barycenter.data(), barycenter.data() + 3, &ann_points_[i][0] ) ;
+        }
+        ann_tree_ = new ANNkd_tree( &ann_points_[0], nb_vertices, 3 ) ;
+    }
+
+    void ColocaterANN::get_mapped_colocated(
+        vec3& v,
+        std::vector< unsigned int >& result,
+        int nb_neighbors ) const
+    {
+        grgmesh_debug_assert( !mapped_indices_.empty() ) ;
+        get_colocated( v, result, nb_neighbors ) ;
+        for( unsigned int i = 0; i < result.size(); i++ ) {
+            result[i] = mapped_indices_[result[i]] ;
+        }
+    }
+
+    bool ColocaterANN::get_colocated(
+        vec3& v,
+        std::vector< unsigned int >& result,
+        int nb_neighbors ) const
+    {
+        result.clear() ;
+        std::vector< int > neighbors(nb_neighbors) ;
+        get_neighbors( v, neighbors, nb_neighbors ) ;
+        for( int i = 0; i < neighbors.size(); ++i ) {
+            if( Utils::inexact_equal( v, ann_points_[neighbors[i]] ) ) {
+                result.push_back( neighbors[i] ) ;
+            }
+        }
+
+        return !result.empty() ;
+    }
+
+    void ColocaterANN::get_neighbors(
+        vec3& v,
+        std::vector< int >& result,
+        int nb_neighbors ) const
+    {
+        nb_neighbors = std::min( nb_neighbors, ann_tree_->nPoints() ) ;
+        result.resize( nb_neighbors ) ;
+        ANNdistArray dist = new ANNdist[nb_neighbors] ;
+        ann_tree_->annkSearch( v.data(), nb_neighbors, &result[0], dist ) ;
+        delete[] dist ;
     }
 
 }
