617355cd7c53475192892fa8efcf32c8c47a31d5
Petit Con <32493098+AntoineMazuyer@users.noreply.github.com>
2014-12-11 11:08:00 +0100

    Remove static linking

---
+++ configure.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+if [ $# -eq 0 ]; then
+    CMAKE=cmake
+else
+    CMAKE=$1
+fi
+
+echo ============= Checking for cmake ============
+if ($CMAKE --version)
+then
+   echo "found CMake"
+else
+   echo "cmake not found, please install it (see http://www.cmake.org/)" 
+   exit
+fi
+
+os="Linux64-gcc"
+
+#  Import plaform specific environment
+
+. src/third_party/geogram/cmake/platforms/$os/setvars.sh || exit 1
+
+# Generate the Makefiles
+
+echo
+echo ================== GeoGram ====================
+
+for config in Release Debug
+do
+   platform=$os-$config
+   echo
+   echo ============= Creating makefiles for $platform ============
+   build_dir=build/geogram/$platform
+
+   mkdir -p $build_dir
+   (cd $build_dir; $CMAKE -Wno-dev -DCMAKE_BUILD_TYPE:STRING=$config -DCMAKE_CXX_FLAGS:STRING="-fPIC" -DGEOGRAM_WITH_TETGEN:BOOL=TRUE -DVORPALINE_BUILD_DYNAMIC:BOOL=TRUE -DCMAKE_C_FLAGS:STRING="-fPIC" -DVORPALINE_PLATFORM:STRING=$os ../../../src/third_party/geogram/; $CMAKE --build .)
+done
+echo
+echo ============== GeoGram build configured ==================
+echo
+
+
+echo
+echo ================== GRGMesh ====================
+
+for config in Release Debug
+do
+   platform=$os-$config
+   echo
+   echo ============= Creating makefiles for $platform ============
+   build_dir=build/grgmesh/$platform
+
+   mkdir -p $build_dir
+   (cd $build_dir; $CMAKE -Wno-dev -DCMAKE_BUILD_TYPE:STRING=$config -DGEOGRAM_PLATFORM:STRING=$os ../../../)
+done
+echo
+echo ============== GRGMesh build configured ==================
+
+cat << EOF
+to build:
+  go to build/$os-Release or build/$os-Debug
+  and 'make'
+Note: local configuration can be specified in CMakeOptions.txt
+(see CMakeOptions.txt.sample for an example)
+You'll need to re-run configure.sh if you create or modify CMakeOptions.txt
+EOF
+
