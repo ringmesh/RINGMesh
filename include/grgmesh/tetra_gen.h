bae75a6bd22ae51d5dbce55bd3817fb742630b4e
Arnaud Botella <3213882+BotellaA@users.noreply.github.com>
2015-01-06 20:39:58 +0100

    Add automatic geogram initialization at GRGMesh loading (v3)

---
+++ include/grgmesh/tetra_gen.h
@@ -0,0 +1,182 @@
+/*[
+* Association Scientifique pour la Geologie et ses Applications (ASGA)
+* Copyright (c) 1993-2013 ASGA. All Rights Reserved.
+*
+* This program is a Trade Secret of the ASGA and it is not to be:
+* - reproduced, published, or disclosed to other,
+* - distributed or displayed,
+* - used for purposes or on Sites other than described
+*   in the GOCAD Advancement Agreement,
+* without the prior written authorization of the ASGA. Licencee
+* agrees to attach or embed this Notice on all copies of the program,
+* including partial copies or modified versions thereof.
+]*/
+
+#ifndef __GRGMESH_TETRA_GEN__
+#define __GRGMESH_TETRA_GEN__
+
+#include <grgmesh/common.h>
+#include <grgmesh/utils.h>
+
+#include <geogram/mesh/mesh.h>
+#include <geogram/mesh/mesh_builder.h>
+#include <geogram/basic/counted.h>
+#include <geogram/basic/smart_pointer.h>
+#include <geogram/third_party/tetgen/tetgen.h>
+
+#include <vector>
+
+#ifdef USE_MG_TETRA
+    extern "C" {
+        #include <meshgems/meshgems.h>
+        #include <meshgems/tetra.h>
+    }
+#endif
+
+namespace GRGMesh {
+
+    class BoundaryModelElement ;
+    class TetraGen ;
+
+    typedef GEO::SmartPointer< TetraGen > TetraGen_var ;
+
+    static const std::vector< vec3 > vector_vec3 ;
+    static const std::vector< std::vector< Edge > > vector_edge ;
+    class GRGMESH_API TetraGen : public GEO::Counted {
+    public:
+        virtual ~TetraGen() ;
+        static TetraGen_var instantiate(
+            const TetraMethod& method,
+            GEO::Mesh& tetmesh,
+            const BoundaryModelElement* region,
+            bool add_steiner_points = true,
+            const std::vector< vec3 >& internal_vertices = vector_vec3,
+            const std::vector< std::vector< Edge > >& well_vertices = vector_edge,
+            GEO::Mesh* background = nil ) ;
+
+        virtual bool tetrahedralize() = 0 ;
+
+        index_t nb_points() const { return points_.size() ; }
+        index_t nb_internal_points() const { return internal_points_.size() ; }
+        index_t nb_total_points() const { return nb_points() + nb_internal_points() ; }
+        index_t nb_triangles() const { return triangles_.size() / 3 ; }
+        index_t point_index( index_t f, index_t v ) const { return triangles_[3*f+v] ; }
+        const vec3& point( index_t f, index_t v ) const { return points_[triangles_[3*f+v]] ; }
+        const vec3& point( index_t v ) const { return points_[v] ; }
+        signed_index_t surface_id( index_t f ) const {
+            for( index_t i = 1; i < surface_id_.size(); i++ ) {
+                if( f < surface_ptr_[i] ) return surface_id_[i-1] ;
+            }
+            return  surface_id_.back() ;
+        }
+        signed_index_t* surface_id_ptr( index_t f ) {
+            for( index_t i = 1; i < surface_id_.size(); i++ ) {
+                if( f < surface_ptr_[i] ) return &surface_id_[i-1] ;
+            }
+            return  &surface_id_.back() ;
+        }
+        signed_index_t well_id( index_t f ) const {
+            for( index_t i = 1; i < well_ptr_.size(); i++ ) {
+                if( f < well_ptr_[i] ) return i-1 ;
+            }
+            return  well_ptr_.size()-1 ;
+        }
+
+    protected:
+        TetraGen(
+            GEO::Mesh& tetmesh,
+            const BoundaryModelElement* region,
+            const std::vector< vec3 >& internal_vertices,
+            const std::vector< std::vector< Edge > >& well_edges,
+            GEO::Mesh* background ) ;
+
+        void initialize_storage( index_t nb_points, index_t nb_tets, index_t nb_triangles, index_t nb_lines ) ;
+        void set_point( index_t index, double* point ) ;
+        void set_tetra( index_t index, int* tet, index_t nb_lines, index_t nb_triangles ) ;
+        void set_triangle( index_t index, int * triangle, index_t nb_lines ) ;
+        void set_line( index_t index, int * line ) ;
+        void set_tetra_adjacent( index_t index, index_t face, signed_index_t adj ) ;
+        void set_face_marker(
+            index_t tri,
+            index_t marker ) ;
+        void set_tetra_face_marker(
+            index_t tet,
+            index_t adj,
+            index_t marker ) ;
+
+        void store_edge_attrib() const ;
+
+    protected:
+        std::vector< vec3 > points_ ;
+        std::vector< vec3 > internal_points_ ;
+        std::vector< Edge > well_edges_ ;
+        std::vector< index_t > well_ptr_ ;
+        std::vector< signed_index_t > well_indices_ ;
+        std::vector< signed_index_t > triangles_ ;
+        std::vector< signed_index_t > surface_id_ ;
+        std::vector< index_t > surface_ptr_ ;
+        GEO::Mesh& tetmesh_ ;
+        GEO::MeshBuilder tetmesh_builder_ ;
+        double resolution_ ;
+        GEO::Mesh* background_ ;
+        const BoundaryModelElement* region_ ;
+    } ;
+
+
+    class GRGMESH_API TetraGen_TetGen: public TetraGen {
+    public:
+        TetraGen_TetGen(
+            GEO::Mesh& tetmesh,
+            const BoundaryModelElement* region,
+            bool add_steiner_points,
+            const std::vector< vec3 >& internal_vertices,
+            const std::vector< std::vector< Edge > >& well_vertices,
+            GEO::Mesh* background ) ;
+        virtual ~TetraGen_TetGen() {} ;
+
+        virtual bool tetrahedralize() ;
+
+    private:
+        tetgenio tetgen_input_ ;
+        tetgenio tetgen_output_ ;
+        tetgenio tetgen_background_ ;
+        tetgenbehavior tetgen_args_ ;
+    } ;
+
+#ifdef USE_MG_TETRA
+    class GRGMESH_API TetraGen_MG_Tetra: public TetraGen {
+    public:
+        TetraGen_MG_Tetra(
+            GEO::Mesh& tetmesh,
+            const BoundaryModelElement* region,
+            bool add_steiner_points,
+            const std::vector< vec3 >& internal_vertices,
+            const std::vector< std::vector< Edge > >& well_vertices,
+            GEO::Mesh* background ) ;
+        virtual ~TetraGen_MG_Tetra() ;
+
+        virtual bool tetrahedralize() ;
+
+        static status_t my_message_cb( message_t * msg, void *user_data ) ;
+        static status_t get_size_value(
+            meshgems_integer i,
+            meshgems_real* size,
+            void *user_data ) ;
+
+    private:
+        double get_resolution_value( signed_index_t i ) ;
+
+    private:
+        bool add_steiner_points_ ;
+        context_t* context_ ;
+        mesh_t* mesh_input_ ;
+        mesh_t* mesh_output_ ;
+        mesh_t* mesh_background_ ;
+        sizemap_t* sizemap_ ;
+        tetra_session_t* tms_ ;
+    } ;
+#endif
+
+}
+
+#endif
---
+++ src/grgmesh/macro_mesh.cpp
@@ -0,0 +1,152 @@
+/*[
+ * Association Scientifique pour la Geologie et ses Applications (ASGA)
+ * Copyright (c) 1993-2013 ASGA. All Rights Reserved.
+ *
+ * This program is a Trade Secret of the ASGA and it is not to be:
+ * - reproduced, published, or disclosed to other,
+ * - distributed or displayed,
+ * - used for purposes or on Sites other than described
+ *   in the GOCAD Advancement Agreement,
+ * without the prior written authorization of the ASGA. Licencee
+ * agrees to attach or embed this Notice on all copies of the program,
+ * including partial copies or modified versions thereof.
+ ]*/
+
+#include <grgmesh/macro_mesh.h>
+#include <grgmesh/boundary_model.h>
+#include <grgmesh/tetra_gen.h>
+
+#include <geogram/mesh/mesh_AABB.h>
+
+namespace GRGMesh {
+
+    MacroMesh::MacroMesh( const BoundaryModel* model, uint8 dim )
+        :
+            model_( model ),
+            meshes_( model->nb_regions(), nil ),
+            background_meshes_( model->nb_regions(), nil ),
+            vertices_( model->nb_regions() ),
+            well_vertices_( model->nb_regions() ),
+            nb_vertices_( -1 ),
+            facet_aabb_( model->nb_regions(), nil ),
+            tet_aabb_( model->nb_regions(), nil )
+    {
+        for( unsigned int r = 0; r < model_->nb_regions(); r++ ) {
+            meshes_[r] = new GEO::Mesh( dim ) ;
+        }
+    }
+
+    MacroMesh::~MacroMesh()
+    {
+        for( unsigned int r = 0; r < model_->nb_regions(); r++ ) {
+            delete meshes_[r] ;
+            if( background_meshes_[r] ) delete background_meshes_[r] ;
+            if( facet_aabb_[r] ) delete facet_aabb_[r] ;
+            if( tet_aabb_[r] ) delete tet_aabb_[r] ;
+        }
+    }
+
+    void MacroMesh::initialize_background_meshes( uint8 dim )
+    {
+        for( unsigned int r = 0; r < model_->nb_regions(); r++ ) {
+            background_meshes_[r] = new GEO::Mesh( dim ) ;
+        }
+    }
+
+    const GEO::MeshFacetsAABB& MacroMesh::facet_aabb( index_t region )
+    {
+        init_facet_aabb( region ) ;
+        return *facet_aabb_[region] ;
+    }
+    void MacroMesh::init_facet_aabb( index_t region )
+    {
+        if( facet_aabb_[region] ) return ;
+        facet_aabb_[region] = new GEO::MeshFacetsAABB( mesh( region ) ) ;
+    }
+    void MacroMesh::init_all_facet_aabb()
+    {
+        for( index_t region = 0; region < nb_meshes(); region++ ) {
+            init_facet_aabb( region ) ;
+        }
+    }
+    const GEO::MeshTetsAABB& MacroMesh::tet_aabb( index_t region )
+    {
+        init_tet_aabb( region ) ;
+        return *tet_aabb_[region] ;
+    }
+    void MacroMesh::init_tet_aabb( index_t region )
+    {
+        if( tet_aabb_[region] ) return ;
+        tet_aabb_[region] = new GEO::MeshTetsAABB( mesh( region ) ) ;
+    }
+    void MacroMesh::init_all_tet_aabb()
+    {
+        for( index_t region = 0; region < nb_meshes(); region++ ) {
+            init_tet_aabb( region ) ;
+        }
+    }
+
+    /*!
+     * Compute the tetrahedral mesh of the input structural model
+     * @param method Mesher used
+     * @param region_id Region to mesh, -1 for all
+     * @param add_steiner_points if true, the mesher will add some points inside the region
+     * to improve the mesh quality
+     */
+    void MacroMesh::compute_tetmesh(
+        const TetraMethod& method,
+        int region_id,
+        bool add_steiner_points )
+    {
+        if( region_id == -1 ) {
+            for( unsigned int i = 0; i < nb_meshes(); i++ ) {
+                TetraGen_var tetragen = TetraGen::instantiate( method, mesh( i ),
+                    &model_->region( i ), add_steiner_points, vertices( i ),
+                    well_vertices( i ), background_mesh( i ) ) ;
+                tetragen->tetrahedralize() ;
+            }
+        } else {
+            TetraGen_var tetragen = TetraGen::instantiate( method, mesh( region_id ),
+                &model_->region( region_id ), add_steiner_points,
+                vertices( region_id ), well_vertices( region_id ),
+                background_mesh( region_id ) ) ;
+            tetragen->tetrahedralize() ;
+        }
+    }
+
+    void MacroMesh::unique_points(
+        std::vector< vec3 >& unique_vertices,
+        std::vector< int >& indices ) const
+    {
+        index_t nb_non_unique_vertices = 0 ;
+        for( index_t i = 0; i < meshes_.size(); i++ ) {
+            nb_non_unique_vertices += meshes_[i]->nb_vertices() ;
+        }
+        std::vector< vec3 > all_vertices( nb_non_unique_vertices ) ;
+        index_t index = 0 ;
+        for( index_t i = 0; i < meshes_.size(); i++ ) {
+            for( index_t j = 0; j < meshes_[i]->nb_vertices(); j++ ) {
+                all_vertices[index] = vec3( meshes_[i]->vertex_ptr( j )[0],
+                    meshes_[i]->vertex_ptr( j )[1],
+                    meshes_[i]->vertex_ptr( j )[2] ) ;
+                index++ ;
+            }
+        }
+        MakeUnique mu( all_vertices ) ;
+        mu.unique() ;
+        mu.unique_points( unique_vertices ) ;
+        indices = mu.indices() ;
+    }
+
+    index_t MacroMesh::nb_vertices() {
+        if( nb_vertices_ != -1) {
+            return nb_vertices_ ;
+        }
+        std::vector< vec3 > unique_vertices ;
+        std::vector< int > indices ;
+        unique_points( unique_vertices, indices ) ;
+        nb_vertices_ = unique_vertices.size() ;
+        return nb_vertices_ ;
+    }
+}
+
