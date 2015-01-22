7d13ea9ebeddf18694808a1e812a003653585c13
Arnaud Botella <botella@gocad.org>
2015-01-22 09:25:36 +0100

    Add factories in GRGMeshIO

--- src/grgmesh/io.cpp
+++ src/grgmesh/io.cpp
@@ -15,10 +15,11 @@
 #include <grgmesh/io.h>
 #include <grgmesh/boundary_model.h>
 
-#include <geogram/mesh/mesh_io.h>
-#include <geogram/basic/string.h>
+#include <geogram/basic/command_line.h>
 #include <geogram/basic/file_system.h>
 #include <geogram/basic/line_stream.h>
+#include <geogram/basic/string.h>
+#include <geogram/mesh/mesh_io.h>
 
 #include <geogram/mesh/mesh_private.h>
 
@@ -87,103 +88,253 @@ namespace GRGMesh {
             fclose( out ) ;
             unzCloseCurrentFile( uz ) ;
         }
-        bool load_BoundaryModel_from_Model3D(
-            const std::string& filename,
-            BoundaryModel& model )
+
+        //    ___                   _               __  __         _     _
+        //   | _ ) ___ _  _ _ _  __| |__ _ _ _ _  _|  \/  |___  __| |___| |
+        //   | _ \/ _ \ || | ' \/ _` / _` | '_| || | |\/| / _ \/ _` / -_) |
+        //   |___/\___/\_,_|_||_\__,_\__,_|_|  \_, |_|  |_\___/\__,_\___|_|
+        //                                     |__/
+
+        bool load( const std::string& filename, BoundaryModel& model )
         {
-            if( filename.empty() ) {
-                GEO::Logger::err( "I/O" )
-                    << "No filename provided for structural model, use in:model"
-                    << std::endl ;
-                return false ;
+            GEO::Logger::out( "I/O" ) << "Loading file " << filename << "..."
+                << std::endl ;
+
+            BoundaryModelIOHandler_var handler = BoundaryModelIOHandler::get_handler(
+                filename ) ;
+            if( handler && handler->load( filename, model ) ) {
+                GEO::CmdLine::ui_clear_line() ;
+                GEO::Logger::out( "I/O" ) << "Structural model loaded from: "
+                    << filename << std::endl ;
+                return true ;
             }
 
-            std::ifstream input( filename.c_str() ) ;
-            if( !input ) {
-                GEO::Logger::err( "I/O" ) << "Cannot open file : " << filename
-                    << std::endl ;
-                return false ;
-            }
-
-            BoundaryModelBuilder builder( model ) ;
-            builder.load_file( input ) ;
-            return true ;
+            GEO::Logger::err( "I/O" ) << "Could not load file: " << filename
+                << std::endl ;
+            return false ;
         }
 
-        bool save_BoundaryModel( BoundaryModel& model, const std::string& filename )
+        bool save( BoundaryModel& model, const std::string& filename )
         {
-            std::ofstream out( filename.c_str() ) ;
-            return model.save_gocad_model3d( out ) ;
+            GEO::Logger::out( "I/O" ) << "Saving file " << filename << "..."
+                << std::endl ;
+
+            BoundaryModelIOHandler_var handler = BoundaryModelIOHandler::get_handler(
+                filename ) ;
+            if( handler && handler->save( model, filename ) ) {
+                GEO::CmdLine::ui_clear_line() ;
+                GEO::Logger::out( "I/O" ) << "Structural model saved in: "
+                    << filename << std::endl ;
+                return true ;
+            }
+
+            GEO::Logger::err( "I/O" ) << "Could not save file: " << filename
+                << std::endl ;
+            return false ;
         }
 
-        /// Save a \param[in] macro mesh in a .zip file which contains all the mesh file. Type of the export is
-        /// determined by the extension given in \param[in] filename
-        bool save_macro_mesh( const MacroMesh& mm, const std::string& filename )
-        {
-            std::string pwd = GEO::FileSystem::get_current_working_directory() ;
-            GEO::FileSystem::set_current_working_directory(
-                GEO::FileSystem::dir_name( filename ) ) ;
-            zipFile zf = zipOpen( filename.c_str(), APPEND_STATUS_CREATE ) ;
-            for( index_t m = 0; m < mm.nb_meshes(); m++ ) {
-                GEO::MeshIOFlags flags ;
-                flags.set_element( GEO::MESH_FACETS ) ;
-                flags.set_element( GEO::MESH_CELLS ) ;
-                flags.set_attribute( GEO::MESH_FACET_REGION ) ;
+        /************************************************************************/
 
-                const GEO::Mesh& cur_mesh = mm.mesh( m ) ;
-                std::string name_mesh_file = "region_" +GEO::String::to_string( m ) + ".meshb" ;
-
-                GEO::Logger::instance()->set_quiet( true ) ;
-                GEO::mesh_save( cur_mesh, name_mesh_file, flags ) ;
-                GEO::Logger::instance()->set_quiet( false ) ;
-
-                zip_file( zf, name_mesh_file ) ;
-
-                GEO::FileSystem::delete_file( name_mesh_file ) ;
-
-            }
-            zipClose( zf, NULL ) ;
-            GEO::FileSystem::set_current_working_directory( pwd ) ;
-            return true ;
-        }
-
-        bool load_macro_mesh( MacroMesh& mm, const std::string& mesh_file )
-        {
-            unzFile uz = unzOpen( mesh_file.c_str() ) ;
-            unz_global_info global_info ;
-            if( unzGetGlobalInfo( uz, &global_info ) != UNZ_OK ) {
-                GEO::Logger::err( "could not read file global info" ) ;
-                unzClose( uz ) ;
-                return false ;
-            }
-            for( index_t r = 0; r < mm.model()->nb_regions(); r++ ) {
-                char filename[MAX_FILENAME] ;
-                unzip_file( uz, filename ) ;
-                GEO::MeshIOFlags flags ;
-                flags.set_element( GEO::MESH_FACETS ) ;
-                flags.set_element( GEO::MESH_CELLS ) ;
-                flags.set_attribute( GEO::MESH_FACET_REGION ) ;
-                GEO::Mesh& m = mm.mesh( r ) ;
-                std::string ext = GEO::FileSystem::extension( filename ) ;
-                if( ext == "meshb" ) {
-                    GEO::Logger::instance()->set_quiet( true ) ;
-                    GEO::mesh_load( GEO::String::to_string( filename ), m, flags ) ;
-                    GEO::Logger::instance()->set_quiet( false ) ;
-                } else {
-                    grgmesh_assert_not_reached;
+        class MLIOHandler: public BoundaryModelIOHandler {
+        public:
+            virtual bool load( const std::string& filename, BoundaryModel& model )
+            {
+                if( filename.empty() ) {
+                    GEO::Logger::err( "I/O" )
+                        << "No filename provided for structural model, use in:model"
+                        << std::endl ;
+                    return false ;
                 }
-                GEO::FileSystem::delete_file( filename ) ;
 
-                if( ( r + 1 ) < global_info.number_entry ) {
-                    if( unzGoToNextFile( uz ) != UNZ_OK ) {
-                        GEO::Logger::err( "Could not read next file" ) ;
-                        unzClose( uz ) ;
-                        return false ;
+                std::ifstream input( filename.c_str() ) ;
+                if( !input ) {
+                    GEO::Logger::err( "I/O" ) << "Cannot open file : " << filename
+                        << std::endl ;
+                    return false ;
+                }
+
+                BoundaryModelBuilder builder( model ) ;
+                builder.load_file( input ) ;
+                return true ;
+            }
+
+            virtual bool save( BoundaryModel& model, const std::string& filename )
+            {
+                std::ofstream out( filename.c_str() ) ;
+                return model.save_gocad_model3d( out ) ;
+            }
+        } ;
+
+        /************************************************************************/
+
+        BoundaryModelIOHandler* BoundaryModelIOHandler::create(
+            const std::string& format )
+        {
+            grgmesh_register_BoundaryModelIOHandler_creator( MLIOHandler, "ml" ) ;
+
+            BoundaryModelIOHandler* handler = BoundaryModelIOHandlerFactory::create_object(format) ;
+            if( handler ) {
+                return handler ;
+            }
+
+            GEO::Logger::err("I/O")
+                << "Unsupported file format: " << format
+                << std::endl ;
+            return nil ;
+        }
+
+        BoundaryModelIOHandler* BoundaryModelIOHandler::get_handler(
+            const std::string& filename )
+        {
+            std::string ext = GEO::FileSystem::extension( filename ) ;
+            return create( ext ) ;
+        }
+
+        //    __  __                 __  __        _
+        //   |  \/  |__ _ __ _ _ ___|  \/  |___ __| |_
+        //   | |\/| / _` / _| '_/ _ \ |\/| / -_|_-< ' \
+        //   |_|  |_\__,_\__|_| \___/_|  |_\___/__/_||_|
+        //
+
+        bool load( const std::string& filename, MacroMesh& model )
+        {
+            GEO::Logger::out( "I/O" ) << "Loading file " << filename << "..."
+                << std::endl ;
+
+            MacroMeshIOHandler_var handler = MacroMeshIOHandler::get_handler(
+                filename ) ;
+            if( handler && handler->load( filename, model ) ) {
+                GEO::CmdLine::ui_clear_line() ;
+                GEO::Logger::out( "I/O" ) << "Mesh loaded from: "
+                    << filename << std::endl ;
+                return true ;
+            }
+
+            GEO::Logger::err( "I/O" ) << "Could not load file: " << filename
+                << std::endl ;
+            return false ;
+        }
+
+        bool save( const MacroMesh& model, const std::string& filename )
+        {
+            GEO::Logger::out( "I/O" ) << "Saving file " << filename << "..."
+                << std::endl ;
+
+            MacroMeshIOHandler_var handler = MacroMeshIOHandler::get_handler(
+                filename ) ;
+            if( handler && handler->save( model, filename ) ) {
+                GEO::CmdLine::ui_clear_line() ;
+                GEO::Logger::out( "I/O" ) << "Mesh saved in: "
+                    << filename << std::endl ;
+                return true ;
+            }
+
+            GEO::Logger::err( "I/O" ) << "Could not save file: " << filename
+                << std::endl ;
+            return false ;
+        }
+
+
+        /************************************************************************/
+
+        class MMIOHandler: public MacroMeshIOHandler {
+        public:
+            virtual bool load( const std::string& filename, MacroMesh& mm )
+            {
+                unzFile uz = unzOpen( filename.c_str() ) ;
+                unz_global_info global_info ;
+                if( unzGetGlobalInfo( uz, &global_info ) != UNZ_OK ) {
+                    GEO::Logger::err( "could not read file global info" ) ;
+                    unzClose( uz ) ;
+                    return false ;
+                }
+                for( index_t r = 0; r < mm.model()->nb_regions(); r++ ) {
+                    char filename[MAX_FILENAME] ;
+                    unzip_file( uz, filename ) ;
+                    GEO::MeshIOFlags flags ;
+                    flags.set_element( GEO::MESH_FACETS ) ;
+                    flags.set_element( GEO::MESH_CELLS ) ;
+                    flags.set_attribute( GEO::MESH_FACET_REGION ) ;
+                    GEO::Mesh& m = mm.mesh( r ) ;
+                    std::string ext = GEO::FileSystem::extension( filename ) ;
+                    if( ext == "meshb" ) {
+                        GEO::Logger::instance()->set_quiet( true ) ;
+                        GEO::mesh_load( GEO::String::to_string( filename ), m,
+                            flags ) ;
+                        GEO::Logger::instance()->set_quiet( false ) ;
+                    } else {
+                        grgmesh_assert_not_reached;
+                    }
+                    GEO::FileSystem::delete_file( filename ) ;
+
+                    if( ( r + 1 ) < global_info.number_entry ) {
+                        if( unzGoToNextFile( uz ) != UNZ_OK ) {
+                            GEO::Logger::err( "Could not read next file" ) ;
+                            unzClose( uz ) ;
+                            return false ;
+                        }
                     }
                 }
+                unzClose( uz ) ;
             }
-            unzClose( uz ) ;
 
+            /// Save a \param[in] macro mesh in a .zip file which contains all the mesh file. Type of the export is
+            /// determined by the extension given in \param[in] filename
+            virtual bool save( const MacroMesh& mm, const std::string& filename )
+            {
+                std::string pwd = GEO::FileSystem::get_current_working_directory() ;
+                GEO::FileSystem::set_current_working_directory(
+                    GEO::FileSystem::dir_name( filename ) ) ;
+                zipFile zf = zipOpen( filename.c_str(), APPEND_STATUS_CREATE ) ;
+                for( index_t m = 0; m < mm.nb_meshes(); m++ ) {
+                    GEO::MeshIOFlags flags ;
+                    flags.set_element( GEO::MESH_FACETS ) ;
+                    flags.set_element( GEO::MESH_CELLS ) ;
+                    flags.set_attribute( GEO::MESH_FACET_REGION ) ;
+
+                    const GEO::Mesh& cur_mesh = mm.mesh( m ) ;
+                    std::string name_mesh_file = "region_"
+                        + GEO::String::to_string( m ) + ".meshb" ;
+
+                    GEO::Logger::instance()->set_quiet( true ) ;
+                    GEO::mesh_save( cur_mesh, name_mesh_file, flags ) ;
+                    GEO::Logger::instance()->set_quiet( false ) ;
+
+                    zip_file( zf, name_mesh_file ) ;
+
+                    GEO::FileSystem::delete_file( name_mesh_file ) ;
+
+                }
+                zipClose( zf, NULL ) ;
+                GEO::FileSystem::set_current_working_directory( pwd ) ;
+                return true ;
+
+            }
+        } ;
+
+        /************************************************************************/
+
+        MacroMeshIOHandler* MacroMeshIOHandler::create(
+            const std::string& format )
+        {
+            grgmesh_register_MacroMeshIOHandler_creator( MMIOHandler, "mm" ) ;
+
+            MacroMeshIOHandler* handler = MacroMeshIOHandlerFactory::create_object(format) ;
+            if( handler ) {
+                return handler ;
+            }
+
+            GEO::Logger::err("I/O")
+                << "Unsupported file format: " << format
+                << std::endl ;
+            return nil ;
+        }
+
+        MacroMeshIOHandler* MacroMeshIOHandler::get_handler(
+            const std::string& filename )
+        {
+            std::string ext = GEO::FileSystem::extension( filename ) ;
+            return create( ext ) ;
         }
 
     }
