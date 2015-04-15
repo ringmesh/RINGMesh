8ecf771d858d1b0461ac168a90802aa70508fb5f
Benjamin Chauvin <31856424+benjaminchauvin@users.noreply.github.com>
2015-04-15 21:06:34 +0200

    Add load boundary model and macro mesh using const char* filename to avoid to use the load with const std::string param. This is only to avoid a crash in Gocad in windows (problem with different version of the std).

---
+++ include/ringmesh/io.h
@@ -0,0 +1,152 @@
+/*
+ * Copyright (c) 2012-2015, Association Scientifique pour la Geologie et ses Applications (ASGA)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the <organization> nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  Contacts:
+ *     Arnaud.Botella@univ-lorraine.fr
+ *     Antoine.Mazuyer@univ-lorraine.fr
+ *     Jeanne.Pellerin@wias-berlin.de
+ *
+ *     http://www.gocad.org
+ *
+ *     GOCAD Project
+ *     Ecole Nationale Superieure de Geologie - Georessources
+ *     2 Rue du Doyen Marcel Roubault - TSA 70605
+ *     54518 VANDOEUVRE-LES-NANCY
+ *     FRANCE
+ */
+
+#ifndef __RINGMESH_IO__
+#define __RINGMESH_IO__
+
+#include <ringmesh/common.h>
+
+#include <geogram/basic/factory.h>
+
+namespace RINGMesh {
+    class BoundaryModel ;
+    class MacroMesh ;
+}
+
+namespace RINGMesh {
+    namespace RINGMeshIO {
+        //    ___                   _               __  __         _     _
+        //   | _ ) ___ _  _ _ _  __| |__ _ _ _ _  _|  \/  |___  __| |___| |
+        //   | _ \/ _ \ || | ' \/ _` / _` | '_| || | |\/| / _ \/ _` / -_) |
+        //   |___/\___/\_,_|_||_\__,_\__,_|_|  \_, |_|  |_\___/\__,_\___|_|
+        //                                     |__/
+        bool RINGMESH_API load(
+			const char* filename,
+			BoundaryModel& model );
+		
+		bool RINGMESH_API load(
+            const std::string& filename,
+            BoundaryModel& model ) ;
+
+        bool RINGMESH_API save(
+            BoundaryModel& model,
+            const std::string& filename ) ;
+
+        //    __  __                 __  __        _
+        //   |  \/  |__ _ __ _ _ ___|  \/  |___ __| |_
+        //   | |\/| / _` / _| '_/ _ \ |\/| / -_|_-< ' \
+        //   |_|  |_\__,_\__|_| \___/_|  |_\___/__/_||_|
+        //
+        
+		bool RINGMESH_API load(
+			const char* mesh_file,
+			MacroMesh& model );
+
+		bool RINGMESH_API load(
+            const std::string& mesh_file,
+            MacroMesh& mm ) ;
+
+        bool RINGMESH_API save(
+            const MacroMesh& mm,
+            const std::string& filename ) ;
+
+        class RINGMESH_API BoundaryModelIOHandler : public GEO::Counted {
+    public:
+            static BoundaryModelIOHandler* create( const std::string& format ) ;
+
+            static BoundaryModelIOHandler* get_handler( const std::string& filename ) ;
+
+            virtual bool load(
+                const std::string& filename,
+                BoundaryModel& model ) = 0 ;
+
+            virtual bool save(
+                BoundaryModel& model,
+                const std::string& filename ) = 0 ;
+
+    protected:
+            BoundaryModelIOHandler()
+            {
+            }
+
+            virtual ~BoundaryModelIOHandler()
+            {
+            }
+        } ;
+
+        typedef GEO::SmartPointer< BoundaryModelIOHandler >
+        BoundaryModelIOHandler_var ;
+        typedef GEO::Factory0< BoundaryModelIOHandler >
+        BoundaryModelIOHandlerFactory ;
+#define ringmesh_register_BoundaryModelIOHandler_creator( type, name ) \
+    geo_register_creator( BoundaryModelIOHandlerFactory, type, name )
+
+        class RINGMESH_API MacroMeshIOHandler : public GEO::Counted {
+    public:
+            static MacroMeshIOHandler* create( const std::string& format ) ;
+
+            static MacroMeshIOHandler* get_handler( const std::string& filename ) ;
+
+            virtual bool load(
+                const std::string& filename,
+                MacroMesh& mesh ) = 0 ;
+
+            virtual bool save(
+                const MacroMesh& mesh,
+                const std::string& filename ) = 0 ;
+
+    protected:
+            MacroMeshIOHandler()
+            {
+            }
+
+            virtual ~MacroMeshIOHandler()
+            {
+            }
+        } ;
+
+        typedef GEO::SmartPointer< MacroMeshIOHandler > MacroMeshIOHandler_var ;
+        typedef GEO::Factory0< MacroMeshIOHandler > MacroMeshIOHandlerFactory ;
+#define ringmesh_register_MacroMeshIOHandler_creator( type, name ) \
+    geo_register_creator( MacroMeshIOHandlerFactory, type, name )
+
+    }
+}
+#endif
