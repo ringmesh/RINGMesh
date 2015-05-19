18abb4cb9bd0b8704b6d4073f762fb062edd54f9
Arnaud Botella <3213882+BotellaA@users.noreply.github.com>
2015-05-19 13:30:51 +0200

    Add logo

---
+++ src/third_party/geogram/tools/update_copyright_all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+scriptdir=`dirname "$0"`
+cd $scriptdir/.. || exit 1
+tools/update_copyright.pl \
+    --verbose \
+    --replace \
+    --no-backup \
+    --exclude '/third_party/' \
+    --exclude '/bin/medit/' \
+    --exclude '/filtered_predicates/side\d\.h' \
+    --exclude '/basic/license_msgs\.h' \
+    src
+
+tools/generate_message.pl > \
+    src/lib/vorpalib/basic/license_msgs\.h
+
