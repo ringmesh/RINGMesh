70be640d28a9f73520a06b332a4935b0964582ae
Benjamin Chauvin <31856424+benjaminchauvin@users.noreply.github.com>
2015-05-29 10:49:16 +0200

    Revome elements of a BM begin.

---
+++ make_package.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+if [ $# -eq 0 ]; then
+    echo Enter a package version
+    exit
+else
+    version=$1
+fi
+
+files="configure.bat
+      configure.sh
+      include
+      src
+      doc/Doxyfile
+      doc/dox
+      doc/logo_ringmesh_doc.png
+      doc/logo_ringmesh.png
+      doc/ringmesh.png
+      doc/bme_doc.png
+      tests
+      CMakeOptions.txt.sample
+      CMakeLists.txt
+      cmake"
+       
+tar vczf RINGMesh-$version.tar.gz $files
+zip -r RINGMesh-$version.zip $files
+
