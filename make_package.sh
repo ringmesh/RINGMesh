d1b107987523f737dc1b44b78c8b188bc7255c6d
Jeanne Pellerin <Jeanne Pellerin@localhost>
2015-06-02 16:28:39 +0200

    loading BM functions return now a boolean

---
+++ make_package.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+if [ $# -eq 0 ]; then
+    echo Enter a package version
+    exit
+else
+    version=$1
+fi
+
+cd `dirname $0`
+
+files="configure.bat
+      configure.sh
+      include
+      src
+      doc/Doxyfile
+      doc/dox
+      doc/logo_ringmesh_doc.png
+      doc/logo_ringmesh.png
+      doc/ringmesh.png
+      doc/bme_doc.png
+      tests
+      CMakeOptions.txt.sample
+      CMakeLists.txt
+      cmake"
+
+main_directory=RINGMesh
+if [ -d ${main_directory} ]
+then
+    rm -rf ${main_directory}
+fi
+mkdir ${main_directory}
+cp -R ${files} ${main_directory}
+       
+tar vczf RINGMesh-$version.tar.gz ${main_directory}
+zip -r RINGMesh-$version.zip ${main_directory}
+
+rm -rf ${main_directory}
+
