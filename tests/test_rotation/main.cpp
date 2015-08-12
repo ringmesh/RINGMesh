c1bd316988ae4bf97a5e0e35a4a3296cd5584c6c
Benjamin Chauvin <benjamin.chauvin@univ-lorraine.fr>
2015-08-12 14:55:26 +0200

    add tests rotation bm and mm.

--- tests/test_rotation/main.cpp
+++ tests/test_rotation/main.cpp
@@ -38,62 +38,68 @@
  *     FRANCE
  */
 
-#include <ringmesh/boundary_model.h>
-#include <ringmesh/io.h>
+//#include <ringmesh/boundary_model.h>
+//#include <ringmesh/io.h>
+#include <ringmesh/utils.h>
+#include <geogram/basic/logger.h>
 
-#include <geogram/points/colocate.h>
+using namespace RINGMesh ;
+
+namespace {
+    bool are_equal_matrices(
+        const GEO::Matrix< float64, 4 >& lhs,
+        const GEO::Matrix< float64, 4 >& rhs )
+    {
+        for( index_t mat_i = 0; mat_i < 4; ++mat_i ) {
+            for( index_t mat_j = 0; mat_j < 4; ++mat_j ) {
+                float64 diff = lhs( mat_i, mat_j ) - rhs( mat_i, mat_j ) ;
+                if( diff > epsilon || diff < -epsilon ) {
+                    return false ;
+                }
+            }
+        }
+        return true ;
+    }
+}
 
 int main( int argc, char** argv )
 {
-    using namespace RINGMesh ;
 
-    GEO::Logger::out("TEST") << "Test MakeUnique" << std::endl ;
+    GEO::Logger::out( "TEST" )
+        << "Test rotations of a boundary model and a macro mesh" << std::endl ;
 
-    BoundaryModel in ;
-    if( !RINGMeshIO::load( "../data/model3.ml", in ) )
+    const vec3 origin( 0, 0, 0 ) ;
+    const vec3 axis( 0, 0, 1 ) ;
+    float64 angle = 90 ;
+    GEO::Matrix< float64, 4 > rot_mat ;
+    Math::rotation_matrix_about_arbitrary_axis( origin, axis, angle, true,
+        rot_mat ) ;
+    GEO::Matrix< float64, 4 > result ;
+    result( 0, 0 ) = 0 ;
+    result( 0, 1 ) = -1 ;
+    result( 0, 2 ) = 0 ;
+    result( 0, 3 ) = 0 ;
+
+    result( 1, 0 ) = 1 ;
+    result( 1, 1 ) = 0 ;
+    result( 1, 2 ) = 0 ;
+    result( 1, 3 ) = 0 ;
+
+    result( 2, 0 ) = 0 ;
+    result( 2, 1 ) = 0 ;
+    result( 2, 2 ) = 1 ;
+    result( 2, 3 ) = 0 ;
+
+    result( 3, 0 ) = 0 ;
+    result( 3, 1 ) = 0 ;
+    result( 3, 2 ) = 0 ;
+    result( 3, 3 ) = 1 ;
+
+    if( !are_equal_matrices( rot_mat, result ) ) {
+        GEO::Logger::out( "TEST" ) << "FAILED" << std::endl ;
         return 1 ;
-
-    index_t nb_non_unique_vertices = in.nb_corners() ;
-
-    for( index_t l = 0; l < in.nb_lines(); l++ ) {
-        nb_non_unique_vertices += in.line( l ).nb_vertices() ;
-    }
-    for( index_t s = 0; s < in.nb_surfaces(); s++ ) {
-        nb_non_unique_vertices += in.surface( s ).nb_vertices() ;
     }
 
-    std::vector< vec3 > all_vertices( nb_non_unique_vertices ) ;
-    index_t index = 0 ;
-    for( index_t c = 0; c < in.nb_corners(); c++ ) {
-        all_vertices[index++] = in.corner( c ).vertex() ;
-    }
-    for( index_t l = 0; l < in.nb_lines(); l++ ) {
-        const Line& line = in.line( l ) ;
-        for( index_t v = 0; v < line.nb_vertices(); v++ ) {
-            all_vertices[index++] = line.vertex( v ) ;
-        }
-    }
-    for( index_t s = 0; s < in.nb_surfaces(); s++ ) {
-        const Surface& surface = in.surface( s ) ;
-        for( index_t v = 0; v < surface.nb_vertices(); v++ ) {
-            all_vertices[index++] = surface.vertex( v ) ;
-        }
-    }
-
-    GEO::vector< index_t > old2new ;
-    index_t geo_nb = GEO::Geom::colocate( all_vertices[0].data(), 3, nb_non_unique_vertices, old2new,
-        epsilon ) ;
-
-    index_t ringmesh_nb = in.vertices.nb() ;
-
-    bool res = ringmesh_nb == geo_nb ;
-    if( res )
-        GEO::Logger::out("TEST") << "SUCCESS" << std::endl ;
-    else {
-        GEO::Logger::out( "TEST" ) << "FAILED:" << std::endl ;
-        GEO::Logger::out( "TEST" ) << "initial_nb=" << nb_non_unique_vertices << std::endl ;
-        GEO::Logger::out( "TEST" ) << "geo_nb=" << geo_nb << std::endl ;
-        GEO::Logger::out( "TEST" ) << "ringmesh_nb=" << ringmesh_nb << std::endl ;
-    }
-    return !res ;
+    GEO::Logger::out( "TEST" ) << "SUCCESS" << std::endl ;
+    return 0 ;
 }
