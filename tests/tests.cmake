a31dfaae38af60b850674603cacd860115c56b5e
Arnaud Botella <3213882+BotellaA@users.noreply.github.com>
2015-09-21 11:32:31 +0200

    Merge with e91cf95c408d0b1f14bcf40f876ebf144130eb61

---
+++ tests/tests.cmake
@@ -0,0 +1,21 @@
+# if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+#On windows ? nothing done even in debug ? Cmake version ?
+    function(add_test folder)
+        add_subdirectory(tests/${folder})
+    endfunction()
+
+    IF(WIN32)
+        link_directories(build/ringmesh/${GEOGRAM_PLATFORM}/lib/${CMAKE_BUILD_TYPE})
+    ELSE()
+        link_directories(build/ringmesh/Linux64-gcc-${CMAKE_BUILD_TYPE}/lib)
+    ENDIF()
+    set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_SOURCE_DIR}/tests/bin)
+
+    add_test(test_io_ml)
+    add_test(test_io_bm)
+    add_test(test_make_unique)
+	add_test(test_build_bm_surface)
+	add_test(test_rotation)
+	add_test(test_get_dependent_elements)
+	add_test(test_tetrahedralize)
+#ENDIF()
