474a04cb31bd45bd3379295bc6e8b6f7df8fcb77
Arnaud Botella <3213882+BotellaA@users.noreply.github.com>
2015-09-29 09:46:33 +0200

    Replace GOCAD -> RING et gocad.org -> ring-team.org

--- src/ringmesh/ringmesh_assert.cpp
+++ src/ringmesh/ringmesh_assert.cpp
@@ -29,10 +29,10 @@
  *     Antoine.Mazuyer@univ-lorraine.fr
  *     Jeanne.Pellerin@wias-berlin.de
  *
- *     http://www.gocad.org
+ *     http://www.ring-team.org
  *
- *     GOCAD Project
- *     Ecole Nationale Supérieure de Géologie - Georessources
+ *     RING Project
+ *     Ecole Nationale Supï¿½rieure de Gï¿½ologie - Georessources
  *     2 Rue du Doyen Marcel Roubault - TSA 70605
  *     54518 VANDOEUVRE-LES-NANCY
  *     FRANCE
