2666b6983033ccf26fd9e040aac621b0a236a540
Arnaud Botella <3213882+BotellaA@users.noreply.github.com>
2015-09-30 12:08:01 +0200

    Remove BMVertices -> GeoModelMeshVertices

--- include/ringmesh/geo_model_mesh.h
+++ include/ringmesh/geo_model_mesh.h
@@ -56,6 +56,7 @@ namespace RINGMesh {
 
 
     class RINGMESH_API GeoModelMeshVertices {
+        friend class GeoModelMesh ;
     public:
         /*!
          * @brief Identification of a vertex in a GeoModelElement
@@ -94,18 +95,125 @@ namespace RINGMesh {
 
     public:
         GeoModelMeshVertices( GeoModelMesh& gmm, GEO::Mesh& mesh ) ;
+        ~GeoModelMeshVertices() ;
 
+        /*!
+         * Test if the mesh vertices are initialized
+         */
         bool is_initialized() const ;
-        void test_and_initialize() const ;
+
+        /*!
+         * @brief Number of vertices stored.
+         * @details Calls initialize() if vertices are not filled yet
+         */
+        index_t nb() const ;
+
+        /*!
+         * @brief Coordinates of a vertex of the GeoModel
+         * @pre v < nb()
+         */
+        const vec3& vertex( index_t v ) const ;
+
+        /*!
+         * @brief Returns the index of the given vertex in the model
+         * @param[in] p input point coordinates
+         * @return index of the vertex in the model if found
+         * (distance < epsilon), otherwise NO_ID
+         */
+        index_t index( const vec3& p ) const ;
+
+        /*!
+         * @brief Get the vertices in GME corresponding to the given unique vertex
+         */
+        const std::vector< VertexInGME >& gme_vertices( index_t v ) const ;
+
+        /*!
+         * @brief To use when building the model by first adding its vertices
+         * @warning The client is responsible for setting the mapping between the points
+         * of the BME and the unique vertex
+         */
+        index_t add_vertex( const vec3& point ) ;
+
+        /*!
+         * @brief Add a vertex in a GeoModelElement
+         *        corresponding to an existing vertex of the model
+         */
+        void add_to_bme( index_t v, const VertexInGME& v_gme ) ;
+
+        /*!
+         * @brief Change one of the GME vertex associated to a vertex
+         * @param v Index of the vertex
+         * @param i Index of the GME vertex
+         * @param v_gme Id of GME and of the vertex in that GME
+         */
+        void set_gme( index_t v, index_t i, const VertexInGME& v_gme ) ;
+
+        /*!
+         * @brief Set the point coordinates of all the vertices that
+         *        share this unique vertex, including the unique vertex itself.
+         * @param[in] v Index of the vertex
+         * @param[in] point New coordinates
+         */
+        void update_point( index_t v, const vec3& point ) ;
+
+        /*!
+         * @brief Clear the vertices - clear the gme_vertices_ -
+         *        clear global vertex information in the all BMME
+         * @warning Not stable - crashes if atributes are still bound
+         */
+        void clear() ;
 
     private:
         /*!
+         * Test if the mesh vertices need to be initialized,
+         * if so initialize them.
+         */
+        void test_and_initialize() const ;
+        /*!
          * @brief Initialize the vertices from the vertices
          *        of the GeoModel Corners, Lines, and Surfaces
-         * @details Fills the mesh_.vertices, bme_vertices_ and
+         * @details Fills the mesh_.vertices, gme_vertices_ and
          *         delete colocated vertices
          */
         void initialize() ;
+
+        /*!
+         * @brief Delete the KdTree and set the pointer to nil.
+         */
+        void clear_kdtree() ;
+
+        /*!
+         * Test if the kdtree need to be initialized,
+         * if so initialize it.
+         */
+        void test_kdtree_and_initialize() const ;
+        /*!
+         * Initialize the kdtree with the mesh vertices
+         */
+        void initialize_kdtree() ;
+        /*!
+         * @brief Remove colocated vertices
+         */
+        void remove_colocated() ;
+
+        /*!
+         * @brief Delete vertices for which to_delete[i] != i
+         * @detail The global vertices are deleted, gme_vertices_
+         * is updated and the model_vertx_id in the GeoModelMeshElement
+         * of the BoudnaryModel are updated too.
+         *
+         * @param[in,out] to_delete can be NO_ID or give the index of a
+         *  kept vertex with wich information should be merged.
+         *  It is recyled to give the mapping between old and new vertex indices
+         * @pre to_delete[ v ] is either NO_ID, or is equal or inferior to v
+         */
+        void erase_vertices( std::vector< index_t >& to_delete ) ;
+
+        /*!
+         * @brief Remove all invalid VertexInGME and delete the vertices
+         * that are not anymore in any GeoModelElement
+         */
+        void erase_invalid_vertices() ;
 
     private:
         /// Attached GeoModelMesh owning the vertices
@@ -121,6 +229,8 @@ namespace RINGMesh {
          * @todo Change this extremely expensive storage !!!
          */
         std::vector< std::vector< VertexInGME > > gme_vertices_ ;
+        /// Kd-tree of the model vertices
+        ColocaterANN* kdtree_ ;
 
     } ;
 
@@ -144,12 +254,44 @@ namespace RINGMesh {
         GeoModelMesh( const GeoModel& gm ) ;
         ~GeoModelMesh() ;
 
-        const GEO::Mesh& mesh() const {
-            return *mesh_ ;
-        }
         const GeoModel& model() const {
             return gm_ ;
         }
+
+        GEO::AttributesManager& vertex_attribute_manager() const {
+            return mesh_->vertices.attributes() ;
+        }
+        GEO::AttributesManager& facet_attribute_manager() const {
+            return mesh_->facets.attributes() ;
+        }
+        GEO::AttributesManager& cell_attribute_manager() const {
+            return mesh_->cells.attributes() ;
+        }
+
+
+        /*!
+         * @brief Remove colocated vertices
+         */
+        void remove_colocated_vertices() ;
+
+        /*!
+         * @brief Delete vertices for which to_delete[i] != i
+         * @detail The global vertices are deleted, gme_vertices_
+         * is updated and the model_vertx_id in the GeoModelMeshElement
+         * of the BoudnaryModel are updated too.
+         *
+         * @param[in,out] to_delete can be NO_ID or give the index of a
+         *  kept vertex with wich information should be merged.
+         *  It is recyled to give the mapping between old and new vertex indices
+         * @pre to_delete[ v ] is either NO_ID, or is equal or inferior to v
+         */
+        void erase_vertices( std::vector< index_t >& to_delete ) ;
+
+        /*!
+         * @brief Remove all invalid VertexInGME and delete the vertices
+         * that are not anymore in any GeoModelElement
+         */
+        void erase_invalid_vertices() ;
 
     private:
         /// Attached GeoMode
