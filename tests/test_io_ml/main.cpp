693b93cd9404f70f5db0fe108cd9b7e609742c56
Arnaud Botella <3213882+BotellaA@users.noreply.github.com>
2015-10-01 16:41:50 +0200

    starting

---
+++ tests/test_io_ml/main.cpp
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2012-2015, Association Scientifique pour la Geologie et ses Applications (ASGA)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the <organization> nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  Contacts:
+ *     Arnaud.Botella@univ-lorraine.fr
+ *     Antoine.Mazuyer@univ-lorraine.fr
+ *     Jeanne.Pellerin@wias-berlin.de
+ *
+ *     http://www.ring-team.org
+ *
+ *     RING Project
+ *     Ecole Nationale Sup�rieure de G�ologie - Georessources
+ *     2 Rue du Doyen Marcel Roubault - TSA 70605
+ *     54518 VANDOEUVRE-LES-NANCY
+ *     FRANCE
+ */
+
+#include <ringmesh/geo_model.h>
+#include <ringmesh/io.h>
+#include <ringmesh/utils.h>
+
+#include <geogram/basic/logger.h>
+
+int main( int argc, char** argv )
+{
+    using namespace RINGMesh ;
+
+    //GEO::Logger::out("TEST") << "Test IO for a GeoModel in .ml" << std::endl ;
+
+    GeoModel in ;
+    if( !model_load( "../data/model1.ml", in ) )
+        return 1 ;
+    if( !model_save( in, "out.ml" ) )
+        return 1 ;
+
+    GeoModel in2 ;
+    if( !model_load( "out.ml", in2 ) )
+        return 1 ;
+    if( !model_save( in2, "out2.ml" ) )
+        return 1 ;
+	
+	// Test a bad fixable input annot
+	GeoModel in3 ;
+	if( !model_load( "../data/annot.ml", in3 ) )
+        return 1 ;
+		
+    bool res = Utils::compare_file( "out.ml", "out2.ml" ) ;
+    if( res )
+        GEO::Logger::out("TEST") << "SUCCESS" << std::endl ;
+    else
+        GEO::Logger::out("TEST") << "FAILED" << std::endl ;
+    return !res ;
+}
