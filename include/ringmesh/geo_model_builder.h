b9062c31998ee6f812866eea526d57eb5375cc71
Jeanne Pellerin <Jeanne Pellerin@localhost>
2015-10-02 18:35:22 +0200

    Creation of the GeoModelEditor - friend of GeoModel who edit the topology of the model and its elements

--- include/ringmesh/geo_model_builder.h
+++ include/ringmesh/geo_model_builder.h
@@ -45,6 +45,7 @@
 
 #include <ringmesh/common.h>
 #include <ringmesh/geo_model.h>
+#include <ringmesh/geo_model_editor.h>
 
 #include <vector>
 #include <string>
@@ -58,11 +59,13 @@ namespace RINGMesh {
     /*!
      * @brief Base class for all classes building a GeoModel.
      * @details Derive from this class to build or modify a GeoModel
+     * 
+     * NON Geometry related function have been moved in the GeoModelEditor
      */
-    class RINGMESH_API GeoModelBuilder {
+    class RINGMESH_API GeoModelBuilder : public GeoModelEditor {
     public:
         GeoModelBuilder( GeoModel& model )
-            : model_( model )
+            : GeoModelEditor( model )
         {
         }
         virtual ~GeoModelBuilder()
@@ -72,171 +75,21 @@ namespace RINGMesh {
         /*! @}
          * \name Access - Modification of the GeoModel
          * @{
-         */
-
-        /*!
-         *@brief Set the name of the model  
-         */
-        void set_model_name( const std::string& name )
-        {
-            model_.name_ = name ;
-        }
+         */       
 
         /*! 
          *@brief Copy elements and element connectivity of model @param from 
-         */
+         *
         void copy_macro_topology( const GeoModel& from )
         {
             model_.copy_macro_topology( from ) ;
-        }
-
-        /*!
-         *@brief The model under construction
-         */
-        const GeoModel& model() const
-        {
-            return model_ ;
-        }
+        }*/        
 
         bool end_model() ;
+
         bool complete_element_connectivity() ;
 
-        /*! @}
-         * \name Creation - Deletion - Access to GeoModelElements.
-         * @{
-         */
-
-        GME::gme_t create_element( GME::TYPE e_type ) ;
-
-        /*!
-         * @brief Set an element of the model.
-         * @details It is on purpose that element validity is not checked.
-         *          This way nil pointers can be set for a further element removal.
-         * @param id Id card of the element to modify. The ownership of the previous element
-         *           is given up by the GeoModel.
-         * @param E Element to set. The ownership is transferred to the GeoModel.
-         */
-        void set_element( const GME::gme_t& id, GeoModelElement* E ) const
-        {
-            if( id.type < GME::NO_TYPE ) {
-                model_.modifiable_elements( id.type )[id.index] = E ;
-            } else {
-                ringmesh_assert_not_reached;
-            }
-        }
-
-        /*!
-         * @brief Reference to a modifiable element of the model
-         * @pre The id must refer to a valid element of the model
-         */
-        GeoModelElement& element(
-            const GME::gme_t& id ) const
-        {
-            return *element_ptr(id) ;
-        }
-
-        /*!
-         * @brief Reference to a modifiable meshed element of the model
-         * @pre Assert in debug model that the given id refers to a meshed element.
-         *      The id must refer to a valid element.
-         */
-        GeoModelMeshElement& mesh_element(
-            const GME::gme_t& id ) const
-        {
-            ringmesh_debug_assert( GME::has_mesh( id.type ) ) ;
-            return dynamic_cast<GeoModelMeshElement&>( element( id ) ) ;
-        }
-
-        /*!
-         * @brief Modifiable pointer to an element of the model
-         */
-        GeoModelElement* element_ptr( const GME::gme_t& id ) const
-        {
-            if( id.type < GME::NO_TYPE ) {
-                return model_.elements( id.type )[ id.index ] ;
-            } else if( id.type == GME::ALL_TYPES ) {
-                return element_ptr( model_.global_to_typed_id( id ) ) ;
-            } else {
-                ringmesh_assert_not_reached ;
-                return &model_.universe_ ;
-            }
-        }
-
-        void remove_elements( const std::set< GME::gme_t >& elements ) ;
-        bool get_dependent_elements( std::set< GME::gme_t >& elements ) const ;
-        void remove_elements_and_dependencies( 
-            const std::set< GME::gme_t >& elements_to_remove ) ;
-
-
-        /*! @}
-         * \name Filling GeoModelElement attributes.
-         * @{
-         */
-        void set_model(
-            const GME::gme_t& t,
-            GeoModel* m )
-        {
-            element( t ).set_model( m ) ;
-        }
-
-        void set_element_index(
-            const GME::gme_t& t )
-        {
-            element( t ).set_id( t.index ) ;
-        }
-
-        void set_element_name(
-            const GME::gme_t& t,
-            const std::string& name )
-        {
-            element( t ).set_name( name ) ;
-        }
-
-        void set_element_geol_feature(
-            const GME::gme_t& t,
-            GME::GEOL_FEATURE geol )
-        {
-            element( t ).set_geological_feature( geol ) ;
-        }
-
-        void add_element_boundary(
-            const GME::gme_t& t,
-            const GME::gme_t& boundary,
-            bool side = false )
-        {
-            if( t.type == GME::REGION ) {
-                dynamic_cast< Region& >(
-                    element( t ) ).add_boundary( boundary, side ) ;
-            } else {
-                element( t ).add_boundary( boundary ) ;
-            }
-        }
-
-        void add_element_in_boundary(
-            const GME::gme_t& t,
-            const GME::gme_t& in_boundary )
-        {
-            element( t ).add_in_boundary( in_boundary ) ;
-        }
-
-        void set_parent(
-            const GME::gme_t& t,
-            const GME::gme_t& parent_index )
-        {
-            element( t ).set_parent( parent_index ) ;
-        }
-
-        void add_child(
-            const GME::gme_t& t,
-            const GME::gme_t& child_index )
-        {
-            element( t ).add_child( child_index ) ;
-        }
-
-        // Universe
-        void set_universe( const std::vector<
-            std::pair< index_t, bool > >& boundaries ) ;
-
+    
         /*! @}
          * \name Set element geometry from geometrical positions   
          * @{
@@ -292,16 +145,7 @@ namespace RINGMesh {
 
         /*!
          * @}
-         */
-        
-    protected: 
-        void delete_elements( std::vector< std::vector< index_t > >& to_erase ) ;
-        void init_global_model_element_access() ;
-        void resize_elements( GME::TYPE type, index_t nb ) ;
-
-    protected:
-        GeoModel& model_ ;
-
+         */       
     } ;
 
     /*!
