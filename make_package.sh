4ea7ef6d64a0a9d5938b2ec4944ae25734b1b48b
Margaux Raguenel <32702400+margauxraguenel@users.noreply.github.com>
2015-12-14 09:44:15 +0100

    .cproject back to previous

---
+++ make_package.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+# if [ $# -eq 0 ]; then
+#     echo Enter a package version
+#     exit
+# else
+#     version=$1
+# fi
+
+version=$1
+
+cd `dirname $0`
+RINGMesh_HOME=$(pwd)
+
+files="configure.bat
+      configure.sh
+      include
+      src
+      doc/Doxyfile
+      doc/dox
+      doc/logo_ringmesh_doc.png
+      doc/logo_ringmesh.png
+      doc/ringmesh.png
+      doc/bme_doc.png
+      tests
+      CMakeOptions.txt.sample
+      CMakeLists.txt
+      cmake"
+
+main_directory=RINGMesh
+if [ -d ${main_directory} ]
+then
+    rm -rf ${main_directory}
+fi
+mkdir ${main_directory}
+cp -R --parents ${files} ${main_directory}
+       
+tar vczf RINGMesh-${version}.tar.gz ${main_directory}
+zip -r RINGMesh-${version}.zip ${main_directory}
+
+rm -rf ${main_directory}
+
+doc_target=doc_online
+cd doc
+rm -rf $doc_target
+doxygen Doxyfile_online
+cd $doc_target/html
+tar vczf ${RINGMesh_HOME}/doc.tar.gz ./*
+
+
