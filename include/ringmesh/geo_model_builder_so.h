046c1b164202101ea229756f7b8f5324b691d02b
Pierre Anquez <pierre.anquez3@gmail.com>
2016-01-27 12:05:40 +0100

    Refactoring again (code splitted in small functions, set maps between gocad id and local id in attributes of the Builder)

--- include/ringmesh/geo_model_builder_so.h
+++ include/ringmesh/geo_model_builder_so.h
@@ -117,9 +117,7 @@ namespace RINGMesh {
          * indices to vertex local indices (in region)
          * @param[out] corners_id Indices of the four vertices
          */
-        void read_tetraedra(
-            const std::vector< index_t >& gocad_vertices2region_vertices,
-            std::vector< index_t >& corners_id ) const ;
+        void read_tetraedra( std::vector< index_t >& corners_id ) const ;
 
         /*!
          * @brief Sets the boundaries of the GeoModel regions
@@ -259,12 +257,61 @@ namespace RINGMesh {
             const std::vector< bool >& surf_minus_side,
             const std::vector< bool >& surf_plus_side ) ;
 
+        /*!
+         * Builds surface by setting the points and facets of the surfaces
+         * @param[in] surface_id Index of the surface to build
+         * @param[in,out] facet_corners Vector of the (gocad) indices of the
+         * three corners of each facet (gocad) indices of the surface.
+         * Re-initialized at the end of the function.
+         * @param[in,out] facet_ptr Pointer to the beginning of a facet in
+         * facets. Re-initialized at the end of the function.
+         */
         void build_surface(
-            index_t surface_id,
+            const index_t surface_id,
             std::vector< index_t >& facet_corners,
-            std::vector< index_t >& facet_ptr,
-            const std::vector< index_t >& gocad_vertices2region_id,
-            const std::vector< index_t >& gocad_vertices2region_vertices ) ;
+            std::vector< index_t >& facet_ptr ) ;
+
+        /*!
+         * @brief Gets the points and the indices in the points vector to
+         * build the facets
+         * @param[in] facet_corners Vector of the (gocad) indices of the
+         * three corners of each facet (gocad) indices
+         * @param[out] cur_surf_points Vector of unique point coordinates
+         * belonging to the surface
+         * @param[out] cur_surf_facets Vector of each facet corner indices in
+         * the cur_surf_points vector to build facets
+         */
+        void get_surface_points_and_facets_from_gocad_indices(
+            const std::vector< index_t >& facet_corners,
+            std::vector< vec3 >& cur_surf_points,
+            std::vector< index_t >& cur_surf_facets ) const ;
+
+        /*!
+         * @brief Gets the point and the index in the points vector to
+         * build the facets for one read gocad vertex
+         * @param[in] vertex_gocad_id Gocad index of the vertex
+         * @param[in] gocad_vertices2cur_surf_points Map between vertices with
+         * gocad indices and position of the corresponding point
+         * in the points vector
+         * @param[out] cur_surf_points Vector of unique point coordinates
+         * belonging to the surface
+         * @param[out] cur_surf_facets Vector of each facet corner indices in
+         * the cur_surf_points vector to build facets
+         */
+        void get_surface_point_and_facet_from_gocad_index(
+            const index_t vertex_gocad_id,
+            std::vector< index_t >& gocad_vertices2cur_surf_points,
+            std::vector< vec3 >& cur_surf_points,
+            std::vector< index_t >& cur_surf_facets ) const ;
+
+        /*!
+         * @brief Gets the coordinates of the point from gocad index
+         * @param[in] point_gocad_id Gocad index of the point to get
+         * @param[out] point Coordinates of the point
+         */
+        void get_point_from_gocad_id(
+            const index_t point_gocad_id,
+            vec3& point ) const ;
 
         void compute_internal_borders() ;
 
@@ -289,6 +336,16 @@ namespace RINGMesh {
         std::string gocad_coordinates_system_name_ ;
         std::vector< std::string > gocad_coordinates_system_axis_name_ ;
         std::vector< std::string > gocad_coordinates_system_axis_unit_ ;
+        /*!
+         * Vector which maps the indices of vertices from Gocad .so file
+         * to the local (in region) indices of vertices
+         */
+        std::vector< index_t > gocad_vertices2region_vertices_ ;
+        /*!
+         * Vector which maps the indices of vertices from Gocad .so file
+         * to the index of the region they belong to
+         */
+        std::vector< index_t > gocad_vertices2region_id_ ;
     } ;
 }
 
