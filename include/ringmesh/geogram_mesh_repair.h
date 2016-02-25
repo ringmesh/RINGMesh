facf4fecab31fc46c5b102c1f04dec89bd83acd4
Jeanne Pellerin <Jeanne Pellerin@localhost>
2016-02-25 16:07:13 +0100

    refactoring repair facet connection functions buried template stuff in implementation file

--- include/ringmesh/geogram_mesh_repair.h
+++ include/ringmesh/geogram_mesh_repair.h
@@ -58,214 +58,57 @@
 #ifndef __RINGMESH_GEOGRAM_MESH_REPAIR__
 #define __RINGMESH_GEOGRAM_MESH_REPAIR__
 
-#include <algorithm>
-
-#include <geogram/basic/memory.h>
-#include <geogram/mesh/mesh.h>
-
 #include <ringmesh/common.h>
 
+#include <vector>
 
 /* 
  * @file High level repair operations on GEO::Mesh modified from geogram/mesh/mesh_repair.cpp
  * @author Jeanne Pellerin
  */
 
+namespace GEO {
+    class Mesh ;
+}
+
 namespace RINGMesh {
-
-    /**
-    * \brief Predicate to disable connection of facets in repair_connect_facet
-    * 
-    * @todo Encapsulate in a namespace, this is pollution
-    */
-    class MeshEdgesOnBorder
-    {
-    public:
-        typedef std::pair< index_t, index_t > Edge ;
-       /*!
-        * @param[in] mesh_edges_on_border Pair of mesh vertex indices on a boundary.
-        *            In each pair first < second.
-        */
-        MeshEdgesOnBorder( const std::vector<Edge>& mesh_edges_on_border )
-            : edges_on_border_( mesh_edges_on_border.begin(), mesh_edges_on_border.end() )
-        {}
-
-        bool operator()( index_t v0, index_t v1 ) const
-        {
-            Edge edge( std::min( v0, v1 ), std::max( v0, v1 ) ) ;
-            return edges_on_border_.find( edge ) != edges_on_border_.end() ;
-        }
-        void debug( index_t v0, index_t v1 ) const
-        {
-            ringmesh_assert_not_reached ;
-        }
-    private:
-        std::set< Edge > edges_on_border_ ;
-    } ;
-
-
-    /*!
-    * @brief Connects the facets in a TRIANGULATED GEO::Mesh.
-    * @details Reconstructs the corners.adjacent_facet links.
-    *          Orientation is not checked
-    * @note Modified from geogram to take into account a predicate to disconnect facets
-    *       along identified edges [JP]
-    *       The predicate should implement
-    *            bool operator() (index_t v1, index_t v2) const ;
-    *            void debug(index_t v1, index_t v2) ;
-    *
-    * @note It is template and inline on purpose - Theorem from Bruno Levy.
-    * 
-    * @warning DIFFICULT NOT CLEAN CODE
-    */
-    template< typename P > inline
-    void repair_connect_facets( GEO::Mesh& M, P is_border )
-    {
-        using GEO::index_t ;
-
-        const index_t NO_FACET = index_t( -1 ) ;
-        const index_t NO_CORNER = index_t( -1 ) ;
-        const index_t NON_MANIFOLD = index_t( -2 ) ;
-
-        // Reset all facet-facet adjacencies.
-        for( index_t c = 0; c < M.facet_corners.nb(); ++c ) {
-            M.facet_corners.set_adjacent_facet( c, NO_FACET ) ;
-        }
-
-        // For each vertex v, v2c[v] gives the index of a 
-        // corner incident to vertex v.
-        GEO::vector< index_t > v2c( M.vertices.nb(), NO_CORNER ) ;
-
-        // For each corner c, next_c_around_v[c] is the 
-        // linked list of all the corners incident to 
-        // vertex v.
-        GEO::vector< index_t > next_c_around_v( M.facet_corners.nb(), NO_CORNER ) ;
-
-        // Compute v2c and next_c_around_v
-        for( index_t c = 0; c < M.facet_corners.nb(); ++c ) {
-            index_t v = M.facet_corners.vertex( c ) ;
-            next_c_around_v[ c ] = v2c[ v ] ;
-            v2c[ v ] = c ;
-        }
-
-        for( index_t f1 = 0; f1 < M.facets.nb(); ++f1 ) {
-            for( index_t c1 = M.facets.corners_begin( f1 );
-                 c1 < M.facets.corners_end( f1 ); ++c1 ) {
-
-                if( M.facet_corners.adjacent_facet( c1 ) == NO_FACET ) {
-                    index_t adj_corner = NO_CORNER ;
-                    index_t v1 = M.facet_corners.vertex( c1 ) ;
-                    index_t v2 = M.facet_corners.vertex(
-                        M.facets.next_corner_around_facet( f1, c1 ) ) ;
-
-                    index_t c2 = v2c[ v1 ] ;
-
-                    // Lookup candidate adjacent edges from incident
-                    // edges list.
-                    while( c2 != NO_CORNER ) {
-                        if( c2 != c1 ) {
-                            index_t f2 = c2 / 3 ;
-                            index_t c3 = M.facets.prev_corner_around_facet( f2, c2 ) ;
-                            index_t v3 = M.facet_corners.vertex( c3 ) ;
-                            // Check with standard orientation.
-                            if( v3 == v2 ) {
-                                if( !is_border( v1, v2 ) ) {
-                                    if( adj_corner == NO_CORNER ) {
-                                        adj_corner = c3 ;
-                                    } else {
-                                        // Unexpected non-manifold edge store it
-                                        is_border.debug( v1, v2 ) ;
-                                        adj_corner = NON_MANIFOLD ;
-                                    }
-                                }
-                            } else {
-                                // Check with the other ("wrong") orientation
-                                c3 = M.facets.next_corner_around_facet( f2, c2 ) ;
-                                v3 = M.facet_corners.vertex( c3 ) ;
-                                if( v3 == v2 ) {
-                                    if( !is_border( v1, v2 ) ) {
-                                        if( adj_corner == NO_CORNER ) {
-                                            adj_corner = c2 ;
-                                        } else {
-                                            // Unexpected non-manifold edge store it
-                                            is_border.debug( v1, v2 ) ;
-                                            adj_corner = NON_MANIFOLD ;
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                        c2 = next_c_around_v[ c2 ] ;
-                    }
-                    if( adj_corner != NO_CORNER && adj_corner != NON_MANIFOLD ) {
-                        M.facet_corners.set_adjacent_facet( adj_corner, f1 ) ;
-                        index_t f2 = adj_corner / 3 ;
-                        M.facet_corners.set_adjacent_facet( c1, f2 ) ;
-                    }
-                }
-            }
-        }
-    }
-
-
+    
     /*!
      * @brief Connects the facets of a Mesh except along edges flagged so.
-     * @note inline otherwise linking trouble on Windows. No idea why [JP]
+     * @details Resistent to non-manifold edges AND resistent to badly oriented facets
      */
-    inline void connect_mesh_facets( GEO::Mesh& mesh,
-        const std::vector<std::pair<index_t, index_t> >& border_edges )
-    {
-        MeshEdgesOnBorder edges_to_keep_disconnected( border_edges ) ;
-        return repair_connect_facets< MeshEdgesOnBorder >( mesh, edges_to_keep_disconnected ) ;
-    }
+    void RINGMESH_API connect_mesh_facets( GEO::Mesh& mesh,
+        const std::vector<index_t>& border_edges ) ;
+    
+    /*!
+    * @brief Connects the facets of a Mesh except along the mesh edges.
+    * @details Resistent to non-manifold edges AND resistent to badly oriented facets
+    */
+    void RINGMESH_API connect_mesh_facets_except_on_mesh_edges( GEO::Mesh& mesh ) ;
 
-    inline void connect_mesh_facets_except_on_mesh_edges( GEO::Mesh& mesh )
-    {
-        index_t nb_edges = mesh.edges.nb() ;
-        std::vector< std::pair<index_t, index_t > > edges( nb_edges ) ;
-        for( index_t i = 0; i< nb_edges; ++i ) {
-            index_t v0 = mesh.edges.vertex( i, 0 );
-            index_t v1 = mesh.edges.vertex( i, 1 );
-            if( v1 < v0 ) {
-                std::swap( v0, v1 ) ;
-            }
-            edges[ i ] = std::pair<index_t, index_t>( v0, v1 ) ;
-        }
-        connect_mesh_facets( mesh, edges ) ;
-    }
-   
-    class DoNothing {
-    public:
-        bool operator()( index_t v0, index_t v1 ) const
-        {
-            return false ;
-        }
-        void debug( index_t v0, index_t v1 ) const
-        {}
-    };
+    void RINGMESH_API connect_mesh_facets_except_on_mesh_edges( GEO::Mesh& mesh,
+        std::vector<index_t>& non_manifold_edges ) ;
 
-    /*! 
-     * @brief Connect facets resistent to non-manifold edges unlike mesh.facets.connect()
-     *        which fails big time in that case.     
+    /*!     
+     * @brief Connects the facets of a Mesh with non-manifold edges 
+     * @details Resistent to badly oriented facets     
+     * @note For a Mesh with non-manifold edges (not the normal state of a Mesh) 
+     *       mesh.facets.connect() fails big time without any warning.     
      */
-    inline void RINGMESH_API connect_facets( GEO::Mesh& mesh )
-    {
-        DoNothing predicate ;
-        return repair_connect_facets( mesh, predicate ) ;
-    }
+    void RINGMESH_API connect_facets( GEO::Mesh& mesh ) ;
 
     /*!
     * @brief Returns true if there are colocated vertices in the Mesh
-    * @details This is a wrapper around Geogram colocate functions
+    * @details This is a wrapper around Geogram colocate functions.
     */
     bool RINGMESH_API has_mesh_colocate_vertices( const GEO::Mesh& M, double tolerance ) ;
 
     /*!
     * @brief Merges the vertices of a mesh that are at the same geometric location
-    * @note Copied from geogram/mes/mesh_repair.cpp. No choice since BL will not give access to it.
+    * @note Copied and modified from geogram/mes/mesh_repair.cpp. 
+    *       No choice since BL will not give access to it.
     */
     void RINGMESH_API repair_colocate_vertices( GEO::Mesh& M, double tolerance ) ;
-
 }
 
 #endif
