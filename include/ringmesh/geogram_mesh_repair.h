22aa351b088917bdf022168c2a609f86cf4fa772
Jeanne Pellerin <Jeanne Pellerin@localhost>
2016-02-26 14:29:57 +0100

    new repair functions on mesh

--- include/ringmesh/geogram_mesh_repair.h
+++ include/ringmesh/geogram_mesh_repair.h
@@ -1,51 +1,28 @@
 /*
-*  Copyright (c) 2012-2014, Bruno Levy
-*  All rights reserved.
-*
-*  Redistribution and use in source and binary forms, with or without
-*  modification, are permitted provided that the following conditions are met:
-*
-*  * Redistributions of source code must retain the above copyright notice,
-*  this list of conditions and the following disclaimer.
-*  * Redistributions in binary form must reproduce the above copyright notice,
-*  this list of conditions and the following disclaimer in the documentation
-*  and/or other materials provided with the distribution.
-*  * Neither the name of the ALICE Project-Team nor the names of its
-*  contributors may be used to endorse or promote products derived from this
-*  software without specific prior written permission.
-*
-*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-*  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-*  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-*  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-*  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-*  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-*  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-*  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-*  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-*  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-*  POSSIBILITY OF SUCH DAMAGE.
-*
-*  If you modify this software, you should include a notice giving the
-*  name of the person performing the modification, the date of modification,
-*  and the reason for such modification.
-*
-*  Contact: Bruno Levy
-*
-*     Bruno.Levy@inria.fr
-*     http://www.loria.fr/~levy
-*
-*     ALICE Project
-*     LORIA, INRIA Lorraine,
-*     Campus Scientifique, BP 239
-*     54506 VANDOEUVRE LES NANCY CEDEX
-*     FRANCE
-*
-*
-*  MODIFIED BY 
-* 
 * Copyright (c) 2012-2016, Association Scientifique pour la Geologie et ses Applications (ASGA)
 * All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * Neither the name of ASGA nor the
+*       names of its contributors may be used to endorse or promote products
+*       derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+* ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL ASGA BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 *
 *     http://www.ring-team.org
 *
@@ -55,6 +32,7 @@
 *     54518 VANDOEUVRE-LES-NANCY
 *     FRANCE
 */
+
 #ifndef __RINGMESH_GEOGRAM_MESH_REPAIR__
 #define __RINGMESH_GEOGRAM_MESH_REPAIR__
 
@@ -63,7 +41,7 @@
 #include <vector>
 
 /* 
- * @file High level repair operations on GEO::Mesh modified from geogram/mesh/mesh_repair.cpp
+ * @file Access to high level repair operations on GEO::Mesh
  * @author Jeanne Pellerin
  */
 
@@ -96,6 +74,15 @@ namespace RINGMesh {
      *       mesh.facets.connect() fails big time without any warning.     
      */
     void RINGMESH_API connect_facets( GEO::Mesh& mesh ) ;
+
+    void RINGMESH_API remove_degenerate_facets( GEO::Mesh& mesh ) ;
+
+    void RINGMESH_API remove_facet_duplicates( GEO::Mesh& mesh ) ;
+    
+    void RINGMESH_API remove_degenerate_and_duplicate_facets( GEO::Mesh& mesh ) ;
+
+    void RINGMESH_API repair_mesh_facet_orientation( GEO::Mesh& mesh ) ;
+   
 
     /*!
     * @brief Returns true if there are colocated vertices in the Mesh
