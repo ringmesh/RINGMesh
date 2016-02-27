22021f664fca6db55820b541b00a5765edf1e2a4
Arnaud Botella <botella@gocad.org>
2016-02-27 19:33:26 +0100

    Add IO for .lin files

--- src/ringmesh/geogram_extension.cpp
+++ src/ringmesh/geogram_extension.cpp
@@ -1,53 +1,53 @@
 /*
-* Copyright (c) 2012-2016, Association Scientifique pour la Geologie et ses Applications (ASGA)
-* All rights reserved.
-*
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided that the following conditions are met:
-*     * Redistributions of source code must retain the above copyright
-*       notice, this list of conditions and the following disclaimer.
-*     * Redistributions in binary form must reproduce the above copyright
-*       notice, this list of conditions and the following disclaimer in the
-*       documentation and/or other materials provided with the distribution.
-*     * Neither the name of the <organization> nor the
-*       names of its contributors may be used to endorse or promote products
-*       derived from this software without specific prior written permission.
-*
-* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-* ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-* DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
-* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*
-*
-*
-*
-*
-*
-*     http://www.ring-team.org
-*
-*     RING Project
-*     Ecole Nationale Superieure de Geologie - GeoRessources
-*     2 Rue du Doyen Marcel Roubault - TSA 70605
-*     54518 VANDOEUVRE-LES-NANCY
-*     FRANCE
-*/
+ * Copyright (c) 2012-2016, Association Scientifique pour la Geologie et ses Applications (ASGA)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of ASGA nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL ASGA BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *     http://www.ring-team.org
+ *
+ *     RING Project
+ *     Ecole Nationale Superieure de Geologie - GeoRessources
+ *     2 Rue du Doyen Marcel Roubault - TSA 70605
+ *     54518 VANDOEUVRE-LES-NANCY
+ *     FRANCE
+ */
 
 #include <ringmesh/geogram_extension.h>
 
+#include <geogram/basic/line_stream.h>
+#include <geogram/basic/logger.h>
+
 #include <geogram/mesh/mesh.h>
-#include <geogram/mesh/mesh_io.h>
 #include <geogram/mesh/mesh_geometry.h>
+#include <geogram/mesh/mesh_io.h>
 #include <geogram/mesh/mesh_repair.h>
 
-#include <geogram/basic/logger.h>
-#include <geogram/basic/line_stream.h>
 #include <geogram/points/colocate.h>
+
+/*!
+ * @todo Re-orgarnize this mess
+ */
 
 namespace RINGMesh {
 
@@ -70,7 +70,7 @@ namespace RINGMesh {
     /*! 
      * @brief TSurfMeshIOHandler for importing .ts files into a mesh.
      */
-    class RINGMESH_API TSurfMeshIOHandler : public GEO::MeshIOHandler {
+    class TSurfMeshIOHandler : public GEO::MeshIOHandler {
     public:   
         TSurfMeshIOHandler() :
             mesh_dimension_(3),
@@ -122,6 +122,7 @@ namespace RINGMesh {
         }
 
     private:
+        // This function read the z_sign too [PA]
         void read_number_of_vertices_and_triangles()
         {
             GEO::LineInput in( filename_ ) ;
@@ -213,9 +214,55 @@ namespace RINGMesh {
         GEO::vector< index_t > triangles_ ;
     } ;
 
-    void RINGMESH_API ringmesh_mesh_io_initialize()
+    class LINMeshIOHandler: public GEO::MeshIOHandler {
+    public:
+        virtual bool load(
+            const std::string& filename,
+            GEO::Mesh& mesh,
+            const GEO::MeshIOFlags& flag = GEO::MeshIOFlags() )
+        {
+            GEO::LineInput file( filename ) ;
+
+            while( !file.eof() && file.get_line() ) {
+                file.get_fields() ;
+                if( file.nb_fields() > 0 ) {
+                    if( file.field_matches( 0, "v" ) ) {
+                        vec3 vertex = load_vertex( file, 1 ) ;
+                        mesh.vertices.create_vertex( vertex.data() ) ;
+                    } else if( file.field_matches( 0, "s" ) ) {
+                        mesh.edges.create_edge(
+                            file.field_as_uint( 1 ) - 1,
+                            file.field_as_uint( 2 ) - 1 ) ;
+                    }
+                }
+            }
+            return true ;
+
+        }
+        virtual bool save(
+            const GEO::Mesh& M,
+            const std::string& filename,
+            const GEO::MeshIOFlags& ioflags = GEO::MeshIOFlags() )
+        {
+            throw RINGMeshException( "I/O",
+                "Saving a Mesh into .lin format not implemented yet" ) ;
+            return false ;
+        }
+
+    private:
+        vec3 load_vertex( GEO::LineInput& file, index_t field ) const
+        {
+            return vec3(
+                file.field_as_double( field++ ),
+                file.field_as_double( field++ ),
+                file.field_as_double( field++ ) ) ;
+        }
+    } ;
+
+    void ringmesh_mesh_io_initialize()
     {
         geo_register_MeshIOHandler_creator( TSurfMeshIOHandler, "ts" ) ;
+        geo_register_MeshIOHandler_creator( LINMeshIOHandler, "lin" ) ;
     }
 
 
