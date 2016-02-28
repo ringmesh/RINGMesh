94e7737d4b4d84515666b3b60374e4cecdf8fe15
Arnaud Botella <botella@gocad.org>
2016-02-28 11:01:47 +0100

    Bug fix in LINMeshIOHandler

--- src/ringmesh/geogram_extension.cpp
+++ src/ringmesh/geogram_extension.cpp
@@ -252,10 +252,10 @@ namespace RINGMesh {
     private:
         vec3 load_vertex( GEO::LineInput& file, index_t field ) const
         {
-            return vec3(
-                file.field_as_double( field++ ),
-                file.field_as_double( field++ ),
-                file.field_as_double( field++ ) ) ;
+            double x = file.field_as_double( field++ ) ;
+            double y = file.field_as_double( field++ ) ;
+            double z = file.field_as_double( field++ ) ;
+            return vec3( x, y, z ) ;
         }
     } ;
 
