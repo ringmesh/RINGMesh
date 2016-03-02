ab272530b0bec83a44652b57ccdeea6dd8a9b33d
Benjamin Chauvin <benjamin.chauvin@univ-lorraine.fr>
2016-03-02 11:58:06 +0100

    add feature for topographic.

--- src/ringmesh/geo_model_element.cpp
+++ src/ringmesh/geo_model_element.cpp
@@ -177,6 +177,7 @@ namespace RINGMesh {
      * \li "normal_fault"
      * \li "fault"
      * \li "top"
+     * \li "topographic"
      * \li "none"
      * \li "unconformity"
      * \li "boundary"
@@ -198,6 +199,8 @@ namespace RINGMesh {
         } else if( in == "none" ) {
             // This might seem strange - but it seems that what's
             // Gocad is doing
+            return STRATI ;
+        } else if( in == "topographic" ) {
             return STRATI ;
         } else if( in == "unconformity" ) {
             return UNCONFORMITY ;
