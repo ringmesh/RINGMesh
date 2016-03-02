6325fefab1d392c474c00a78a6faa2a924da52ec
Francois Bonneau <francois.bonneau@univ-lorraine.fr>
2016-03-02 17:04:29 +0100

    pull request correction

--- src/ringmesh/geo_mesh.cpp
+++ src/ringmesh/geo_mesh.cpp
@@ -9,25 +9,20 @@
  *     * Redistributions in binary form must reproduce the above copyright
  *       notice, this list of conditions and the following disclaimer in the
  *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the <organization> nor the
+ *     * Neither the name of ASGA nor the
  *       names of its contributors may be used to endorse or promote products
  *       derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DISCLAIMED. IN NO EVENT SHALL ASGA BE LIABLE FOR ANY
  * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
  * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
  * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- *
- *
- *
- *
  *
  *     http://www.ring-team.org
  *
@@ -45,57 +40,57 @@
 
 namespace RINGMesh {
 
-    GeoMesh::GeoMesh()
+    Mesh::Mesh()
     {
-        mesh_ = new GEO::Mesh() ;
+        mesh_ = new GEO::Mesh ;
     }
 
-    GeoMesh::~GeoMesh()
+    Mesh::~Mesh()
     {
         delete mesh_ ;
     }
 
-    const vec3& GeoMesh::vertex( index_t v_id ) const
+    const vec3& Mesh::vertex( index_t v_id ) const
     {
         return mesh_->vertices.point( v_id ) ;
     }
 
-    index_t GeoMesh::nb_vetices() const
+    index_t Mesh::nb_vertices() const
     {
         return mesh_->vertices.nb() ;
     }
 
-    index_t GeoMesh::edge_vertex( index_t edge_id, index_t vertex_id ) const
+    index_t Mesh::edge_vertex( index_t edge_id, index_t vertex_id ) const
     {
         return mesh_->edges.vertex( edge_id, vertex_id ) ;
     }
 
-    index_t GeoMesh::nb_edges() const
+    index_t Mesh::nb_edges() const
     {
         return mesh_->edges.nb() ;
     }
 
-    index_t GeoMesh::facet_vertex( index_t facet_id, index_t vertex_id ) const
+    index_t Mesh::facet_vertex( index_t facet_id, index_t vertex_id ) const
     {
         return mesh_->facets.vertex( facet_id, vertex_id ) ;
     }
 
-    index_t GeoMesh::facet_adjacent( index_t facet_id, index_t vertex_id ) const
+    index_t Mesh::facet_adjacent( index_t facet_id, index_t vertex_id ) const
     {
         return mesh_->facets.adjacent( facet_id, vertex_id ) ;
     }
 
-    index_t GeoMesh::nb_facets() const
+    index_t Mesh::nb_facets() const
     {
         return mesh_->facets.nb() ;
     }
 
-    index_t GeoMesh::cell_vertex( index_t cell_id, index_t vertex_id ) const
+    index_t Mesh::cell_vertex( index_t cell_id, index_t vertex_id ) const
     {
         return mesh_->cells.vertex( cell_id, vertex_id ) ;
     }
 
-    index_t GeoMesh::cell_facet_vertex(
+    index_t Mesh::cell_facet_vertex(
         index_t cell_id,
         index_t facet_id,
         index_t vertex_id ) const
@@ -103,24 +98,24 @@ namespace RINGMesh {
         return mesh_->cells.facet_vertex( cell_id, facet_id, vertex_id ) ;
     }
 
-    index_t GeoMesh::cell_adjacent( index_t cell_id, index_t facet_id ) const
+    index_t Mesh::cell_adjacent( index_t cell_id, index_t facet_id ) const
     {
         return mesh_->cells.adjacent( cell_id, facet_id ) ;
     }
 
-    index_t GeoMesh::nb_cell_facet( index_t cell_id ) const
+    index_t Mesh::nb_cell_facets( index_t cell_id ) const
     {
         return mesh_->cells.nb_facets( cell_id ) ;
     }
 
-    index_t GeoMesh::nb_cell_facet_vertices(
+    index_t Mesh::nb_cell_facet_vertices(
         index_t cell_id,
         index_t facet_id ) const
     {
         return mesh_->cells.facet_nb_vertices( cell_id, facet_id ) ;
     }
 
-    index_t GeoMesh::nb_cells() const
+    index_t Mesh::nb_cells() const
     {
         return mesh_->cells.nb() ;
     }
