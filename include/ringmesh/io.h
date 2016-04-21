d9fbd63fa25742d35df472ff2d24a7645edc3508
Jeanne Pellerin <Jeanne Pellerin@localhost>
2016-04-21 08:21:55 +0200

    Merge with default

--- include/ringmesh/io.h
+++ include/ringmesh/io.h
@@ -42,8 +42,8 @@
 #include <geogram/basic/string.h>
 
 #ifdef MINIZIP_FIXED
-#include <third_party/minizip/zip.h>
-#include <third_party/minizip/unzip.h>
+#include <third_party/zlib/zip.h>
+#include <third_party/zlib/unzip.h>
 #endif 
 
 #include <geogram/basic/factory.h>
@@ -145,5 +145,21 @@ namespace RINGMesh {
 
     void RINGMESH_API unzip_file( unzFile uz, char filename[MAX_FILENAME] ) ;
 #endif
+
+    class RINGMESH_API BMIOHandler: public GeoModelIOHandler {
+    public:
+        virtual void load( const std::string& filename, GeoModel& model ) ;
+        virtual void save( const GeoModel& model, const std::string& filename ) ;
+    } ;
+
+
+    class RINGMESH_API MMIOHandler: public GeoModelIOHandler {
+    public:
+        virtual void load( const std::string& filename, GeoModel& gm ) ;
+
+        /// Save a \param[in] gm macro mesh in a .zip file which contains all the mesh file. Type of the export is
+        /// determined by the extension given in \param[in] filename
+        virtual void save( const GeoModel& gm, const std::string& filename ) ;
+    } ;
 }
 #endif
