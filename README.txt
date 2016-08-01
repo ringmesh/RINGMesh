765c820d66b9af6d48f271b9ad1a1c9ed8613050
Jeanne Pellerin <Jeanne Pellerin@localhost>
2016-08-01 11:34:56 +0200

    rename utils.h and utils.cpp to box3d.h /cpp

---
+++ README.txt
@@ -0,0 +1,89 @@
+Instructions for compiling RINGMesh
+
+RINGMesh is tested under Linux (64 bits) and Windows (64 bits).
+You will need CMake (version >= 2.8.11). There is no other dependency (everything
+ you need is shipped with RINGMesh). Follow the Linux or Windows instructions below.
+
+Linux
+=====
+
+Configuring RINGMesh
+------------------
+
+Execute cmake command in a RINGMesh/build directory.
+
+- mkdir build
+- cd build
+
+To configure using default options:
+- cmake ..
+To define the options, use the cmake interface:
+- cmake-gui .. or ccmake ..
+
+
+Compiling RINGMesh
+------------------
+
+- cd build/ringmesh/Release
+- make [-j4]
+
+To build in debug, go to build/ringmesh/Debug instead.
+
+Eclipse-cdt project is provided (.project and .cproject). You can import RINGMesh into
+Eclipse: File>Import...>General>Existing Projects into Workspace. Click on next, then
+select the root directory (RINGMesh directory) then click on Finish. There are two
+build configurations: release and debug. There is a target to clean and compile. There are
+also targets to build/rebuild geogram.
+
+Compiling the documentation
+---------------------------
+
+The documentation can be generated using [Doxygen](http://www.stack.nl/~dimitri/doxygen/):
+
+- Check the BUILD_DOCUMENTATION option when using cmake
+- cd build/ringmesh/Release
+- make doc-devkit OR make doc-devkit-lite
+
+doc-devkit include full documentation of RINGMesh and Geogram
+doc-devkit-lite include only the RINGMesh documentation
+
+Then you can go in doc/html and open the index.html with your web browser
+
+Windows
+=======
+
+Configuring RINGMesh
+------------------
+
+Launch CMake GUI, indicate where is the source code as the path to RINGMesh root and 
+where to put the binaries as this_root/build/ringmesh.
+Configuration options can be set in using the interface.
+
+RINGMesh has previously been compiled with:
+- Visual Studio 10 2010 Win64
+- Visual Studio 11 2012 Win64
+- Visual Studio 12 2013 Win64
+
+
+Compiling RINGMesh
+------------------
+
+You can either launch building in VisualStudio or calling cmake in command line
+in the build directory created at the configuration step.
+  cmake --build . --config Release
+
+Compiling the documentation
+---------------------------
+
+The documentation can be generated using [Doxygen](http://www.stack.nl/~dimitri/doxygen/):
+
+- Check the BUILD_DOCUMENTATION option when using cmake
+- Open the solution which is in build/ringmesh/RINGmesh.sln in VisualStudio
+- Build the doc-devkit or the doc-devkit-lite project
+
+doc-devkit include full documentation of RINGMesh and Geogram
+doc-devkit-lite include only the RINGMesh documentation
+
+Then you can go in doc/html and open the index.html with your web browser
+
+
---
+++ include/ringmesh/geo_model_geological_entity.h
@@ -0,0 +1,204 @@
+/*
+ * Copyright (c) 2012-2016, Association Scientifique pour la Geologie et ses Applications (ASGA)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of ASGA nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL ASGA BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *     http://www.ring-team.org
+ *
+ *     RING Project
+ *     Ecole Nationale Superieure de Geologie - GeoRessources
+ *     2 Rue du Doyen Marcel Roubault - TSA 70605
+ *     54518 VANDOEUVRE-LES-NANCY
+ *     FRANCE
+ */
+
+/*!
+ * @file Declaration of GeoModelGEologicalEntity and all its children classes
+ * @author Jeanne Pellerin and Arnaud Botella 
+ */
+
+#ifndef __RINGMESH_GEO_MODEL_GEOLOGICAL_ENTITY__
+#define __RINGMESH_GEO_MODEL_GEOLOGICAL_ENTITY__
+
+#include <ringmesh/common.h>
+
+#include <string>
+#include <vector>
+
+#include <ringmesh/geo_model_entity.h>
+#include <ringmesh/geo_model_mesh_entity.h>
+
+namespace RINGMesh {
+    class GeoModel ;
+    class GeoModelMeshEntity ;
+
+
+    class RINGMESH_API GeoModelGeologicalEntity: public GeoModelEntity {
+    public:
+        friend class GeoModelEditor ;
+
+        static void initialize() ;
+
+        virtual ~GeoModelGeologicalEntity()
+        {
+        }
+        virtual const std::string child_type_name() const = 0 ;
+        virtual const std::string type_name() const = 0 ;
+        virtual bool is_on_voi() const ;
+        virtual bool is_connectivity_valid() const ;
+
+        index_t nb_children() const
+        {
+            return static_cast< index_t >( children_.size() ) ;
+        }
+        const gme_t& child_gme( index_t x ) const
+        {
+            ringmesh_assert( x < nb_children() ) ;
+            return children_[x] ;
+        }
+        const GeoModelMeshEntity& child( index_t x ) const ;
+                                                    
+    protected:
+        GeoModelGeologicalEntity(
+            const GeoModel& model,
+            index_t id = NO_ID,
+            const std::string& name = "unnamed",
+            GEOL_FEATURE geological_feature = NO_GEOL )
+            : GeoModelEntity( model, id, name, geological_feature )
+        {
+        }
+        
+    protected:
+        /// Entities constituting this one - see child_type( TYPE )
+        std::vector< gme_t > children_ ;
+    } ;
+
+    class RINGMESH_API Contact: public GeoModelGeologicalEntity {
+    public:
+        Contact( const GeoModel& model )
+            : GeoModelGeologicalEntity( model )
+        {
+            id_.type = type_name_static() ;
+        }
+        ~Contact()
+        {
+        }
+
+        static const std::string type_name_static()
+        {
+            return "Contact" ;
+        }
+        virtual const std::string type_name() const
+        {
+            return type_name_static() ;
+        }
+        virtual const std::string child_type_name() const
+        {
+            return Line::type_name_static() ;
+        }
+
+        virtual bool is_valid() const
+        {
+            /// @todo to implement
+            return true ;
+        }
+    } ;
+
+    class RINGMESH_API Interface: public GeoModelGeologicalEntity {
+    public:
+        Interface( const GeoModel& model )
+            : GeoModelGeologicalEntity( model )
+        {
+            id_.type = type_name_static() ;
+        }
+        ~Interface()
+        {
+        }
+
+        static const std::string type_name_static()
+        {
+            return "Interface" ;
+        }
+        virtual const std::string type_name() const
+        {
+            return type_name_static() ;
+        }
+        virtual const std::string child_type_name() const
+        {
+            return Surface::type_name_static() ;
+        }
+
+        virtual bool is_valid() const
+        {
+            /// @todo to implement
+            return true ;
+        }
+    } ;
+
+    class RINGMESH_API Layer: public GeoModelGeologicalEntity {
+    public:
+        Layer( const GeoModel& model )
+            : GeoModelGeologicalEntity( model )
+        {
+            id_.type = type_name_static() ;
+        }
+        ~Layer()
+        {
+        }
+
+        static const std::string type_name_static()
+        {
+            return "Layer" ;
+        }
+        virtual const std::string type_name() const
+        {
+            return type_name_static() ;
+        }
+        virtual const std::string child_type_name() const
+        {
+            return Region::type_name_static() ;
+        }
+
+        virtual bool is_valid() const
+        {
+            /// @todo to implement
+            return true ;
+        }
+    } ;
+
+    
+    /*!
+    * @brief Check that  
+    *   1. Only faults have free borders
+    *   2. One stratigraphical interface can be on the boundary of maximum two layers
+    * See Building and Editing a Sealed Geological Model, Caumon et al. 2004
+    */
+    
+    /*! @todo I do not know what to do with this code.... 
+     *  because it depends on a given group of geological entities
+     */
+    bool RINGMESH_API is_geomodel_geology_valid( const GeoModel& geomodel ) ;
+}
+
+#endif
