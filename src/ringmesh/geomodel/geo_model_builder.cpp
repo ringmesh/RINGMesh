05cb21366719f366332db634723481c371fe0600
ArnaudBotella <3213882+BotellaA@users.noreply.github.com>
2016-09-30 11:49:47 +0200

    Fix indents

--- src/ringmesh/geomodel/geo_model_builder.cpp
+++ src/ringmesh/geomodel/geo_model_builder.cpp
@@ -2044,24 +2044,24 @@ namespace RINGMesh {
 
     void GeoModelBuilder::compute_universe()
     {
-    	if( model().universe().nb_boundaries() != 0 ) return;
-    	std::vector< bool > is_surafce_universe_boundary( model().nb_surfaces(),
-			false ) ;
-    	std::vector< bool > surface_side( model().nb_surfaces() ) ;
-    	for( index_t r = 0; r < model().nb_regions(); r++ ) {
-    		const Region& region = model().region( r ) ;
-    		for( index_t s = 0; s < region.nb_boundaries(); s++ ) {
-    			index_t surface_id = region.boundary_gme( s ).index ;
-    			is_surafce_universe_boundary[surface_id] =
-    					!is_surafce_universe_boundary[surface_id] ;
-    			surface_side[surface_id] = region.side( s ) ;
-    		}
-    	}
+        if( model().universe().nb_boundaries() != 0 ) return;
+        std::vector< bool > is_surafce_universe_boundary( model().nb_surfaces(),
+            false ) ;
+        std::vector< bool > surface_side( model().nb_surfaces() ) ;
+        for( index_t r = 0; r < model().nb_regions(); r++ ) {
+            const Region& region = model().region( r ) ;
+        for( index_t s = 0; s < region.nb_boundaries(); s++ ) {
+                index_t surface_id = region.boundary_gme( s ).index ;
+                is_surafce_universe_boundary[surface_id] =
+                    !is_surafce_universe_boundary[surface_id] ;
+                surface_side[surface_id] = region.side( s ) ;
+            }
+        }
 
-    	for( index_t s = 0; s < model().nb_surfaces(); s++ ) {
-    		if( !is_surafce_universe_boundary[s] ) continue ;
-    		add_universe_boundary( s, surface_side[s] ) ;
-    	}
+        for( index_t s = 0; s < model().nb_surfaces(); s++ ) {
+            if( !is_surafce_universe_boundary[s] ) continue ;
+            add_universe_boundary( s, surface_side[s] ) ;
+        }
     }
 
     /*************************************************************************/
