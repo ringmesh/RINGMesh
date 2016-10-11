61d89fa7b8a84e84ef2c7f7a84729c03fd637035
Pierre Anquez <32702237+panquez@users.noreply.github.com>
2016-10-11 11:43:38 +0200

    Test passed on Release not on debug

--- src/ringmesh/geomodel/geo_model_builder.cpp
+++ src/ringmesh/geomodel/geo_model_builder.cpp
@@ -984,12 +984,12 @@ namespace RINGMesh {
 
             if( !backward ) {
                 S.next_on_border( f, f_v0, next_f, next_f_v0 ) ;
+                ringmesh_assert( next_f_v0 != NO_ID ) ;
                 next_f_v1 = S.next_facet_vertex_index( next_f, next_f_v0 ) ;
-//                S.next_on_border( f, f_v0, f_v1, next_f, next_f_v0, next_f_v1 ) ;
             } else {
-                S.prev_on_border( f, f_v1, next_f, next_f_v0 ) ;
+                S.prev_on_border( f, f_v0, next_f, next_f_v0 ) ;
+                ringmesh_assert( next_f_v0 != NO_ID ) ;
                 next_f_v1 = S.next_facet_vertex_index( next_f, next_f_v0 ) ;
-//                S.next_on_border( f, f_v1, f_v0, next_f, next_f_v0, next_f_v1 ) ;
             }
 
             // Finds the BorderTriangle that is corresponding to this
