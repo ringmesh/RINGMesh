8324f040d4f7ab563276c81142262a8b7db51789
Pierre Anquez <32702237+panquez@users.noreply.github.com>
2016-10-14 15:02:49 +0200

    fix problem of degenerate cells with new epsilon definition

--- src/ringmesh/geomodel/geo_model_mesh_entity.cpp
+++ src/ringmesh/geomodel/geo_model_mesh_entity.cpp
@@ -181,7 +181,9 @@ namespace {
             corners_global[v] = S.model_vertex_id( f, v ) ;
             v++ ;
         }
-        return check_mesh_entity_vertices_are_different( corners, corners_global ) ;
+        double area = S.mesh_element_size( f ) ;
+        return check_mesh_entity_vertices_are_different( corners, corners_global )
+            || area < S.model().epsilon2() ;
     }    
     
     /*!
@@ -199,7 +201,7 @@ namespace {
         }
         double volume = region.mesh_element_size( cell_index ) ;
         return check_mesh_entity_vertices_are_different( vertices, vertices_global )
-            || volume < region.model().epsilon() ;
+            || volume < region.model().epsilon3() ;
 	}
 
     /*!
