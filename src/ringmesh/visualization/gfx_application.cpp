c656919d66e176c7c88e9e272ce7ef3f20007505
Benjamin Chauvin <benjamin.chauvin@univ-lorraine.fr>
2016-11-07 18:32:27 +0100

    Reput layer random colors.

--- src/ringmesh/visualization/gfx_application.cpp
+++ src/ringmesh/visualization/gfx_application.cpp
@@ -222,19 +222,25 @@ namespace RINGMesh {
 
     void RINGMeshApplication::GeoModelViewer::toggle_colored_layers()
     {
-        /*! Implement something more adaptable - layers are optional */
-        /*     colored_cells_.new_status = false ;
-         show_colored_regions_.new_status = false ;
-         for( GEO::index_t l = 0; l < GM_.nb_layers(); l++ ) {
-         float red = std::fmod( GEO::Numeric::random_float32(), 1 ) ;
-         float green = std::fmod( GEO::Numeric::random_float32(), 1 ) ;
-         float blue = std::fmod( GEO::Numeric::random_float32(), 1 ) ;
-         const GeoModelEntity& cur_layer = GM_.layer( l ) ;
-         for( index_t r = 0; r < cur_layer.nb_children(); ++r )
-         GM_gfx_.regions.set_mesh_element_color( cur_layer.child( r ).index(),
-         red, green, blue ) ;
-         } */
-
+        /*! TODO Implement something more adaptable - layers are optional */
+        if( GM_.nb_geological_entities( Layer::type_name_static() ) == 0 )
+        {
+            show_colored_layers_.new_status = false ;
+            return ;
+        }
+        colored_cells_.new_status = false ;
+        show_colored_regions_.new_status = false ;
+        for( GEO::index_t l = 0;
+            l < GM_.nb_geological_entities( Layer::type_name_static() ); l++ ) {
+            float red = std::fmod( GEO::Numeric::random_float32(), 1 ) ;
+            float green = std::fmod( GEO::Numeric::random_float32(), 1 ) ;
+            float blue = std::fmod( GEO::Numeric::random_float32(), 1 ) ;
+            const GeoModelGeologicalEntity& cur_layer = GM_.geological_entity(
+                Layer::type_name_static(), l ) ;
+            for( index_t r = 0; r < cur_layer.nb_children(); ++r )
+                GM_gfx_.regions.set_mesh_element_color( cur_layer.child( r ).index(),
+                    red, green, blue ) ;
+        }
     }
 
     void RINGMeshApplication::GeoModelViewer::draw_scene()
