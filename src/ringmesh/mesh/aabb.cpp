ceb081621b6c40e6fe28f352302dfc0674d523b9
arnaud botella <3213882+BotellaA@users.noreply.github.com>
2016-11-09 17:03:29 +0100

    Try to fix pow in aabb

--- src/ringmesh/mesh/aabb.cpp
+++ src/ringmesh/mesh/aabb.cpp
@@ -287,7 +287,7 @@ namespace RINGMesh {
     void AABBTree::save_tree( const std::string& name ) const
     {
         index_t nb_nodes = 0 ;
-        for( index_t level = 1; nb_nodes < tree_.size(); level++ ) {
+        for( double level = 1.; nb_nodes < tree_.size(); level++ ) {
             index_t start_node = static_cast< index_t >( std::pow( 2., level ) ) ;
             nb_nodes = 2 * start_node ;
             GEO::Mesh M ;
