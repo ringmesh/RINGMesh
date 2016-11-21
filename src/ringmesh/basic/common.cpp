125463d72b6ac842e45ec22e4928f7f0b81edfc3
Margaux Raguenel <margaux.raguenel@univ-lorraine.fr>
2016-11-21 16:45:59 +0100

    correct spelling mistake and add open-source

--- src/ringmesh/basic/common.cpp
+++ src/ringmesh/basic/common.cpp
@@ -99,12 +99,12 @@ namespace RINGMesh {
     
     void print_header_information()
     {
-        Logger::out( "" ) << "This project is developped by the RINGMesh"
+        Logger::out( "" ) << "This project is developed by the RINGMesh"
             << " developpers team:" << std::endl ;
         Logger::out( "" ) << "RINGMesh-dev <georessources-ringmesh-dev@univ-lorraine.fr> "
             << std::endl ;
-        Logger::out( "" ) << "You can have access to the full code through "
-            << "its Bitbucket repository: " << std::endl ;
+        Logger::out( "" ) << "You can have access to the full open-source " 
+            << "code through its Bitbucket repository: " << std::endl ;
         Logger::out( "" ) << "https://bitbucket.org/ring_team/ringmesh" 
             << std::endl ; 
         Logger::out( "" ) << "More information on this project and other " 
