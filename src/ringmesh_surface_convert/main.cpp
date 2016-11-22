7bd76977d72295ee619b20917ccf2c384b586e4c
arnaud botella <3213882+BotellaA@users.noreply.github.com>
2016-11-22 15:40:13 +0100

    Merge with e652a0f0612e2ffca1baa57879e461e7be4ab9a3

--- src/ringmesh_surface_convert/main.cpp
+++ src/ringmesh_surface_convert/main.cpp
@@ -59,13 +59,9 @@ int main( int argc, char** argv )
         default_configure() ;
 
         // welcome
+        print_header_information() ;
         Logger::div( "RINGMeshConvert" ) ;
-        Logger::out( "" ) << "Welcome to RINGMeshSurfaceConvert !"
-            << std::endl ;
-        Logger::out( "" ) << "People working on this project in RING"
-            << std::endl ;
-        Logger::out( "" ) << "Gautier Laurent<g.laurent.research@gmail.com> "
-            << std::endl ;
+        Logger::out( "" ) << "Welcome to RINGMeshSurfaceConvert !" << std::endl ;
 
         // help
         if( argc == 1 ) {
