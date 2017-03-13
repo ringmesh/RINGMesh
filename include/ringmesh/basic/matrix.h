5074e36f055794cfff1e0d64b247d7ded5507d2b
Francois Bonneau <francois.bonneau@univ-lorraine.fr>
2017-03-13 16:10:14 +0000

    matrix.h edited online with Bitbucket

--- include/ringmesh/basic/matrix.h
+++ include/ringmesh/basic/matrix.h
@@ -297,7 +297,7 @@ namespace RINGMesh {
          * */
         void get_element_in_line( index_t i, index_t e, T& value ) const
         {
-            ringmesh_assert_not_reache ;
+            ringmesh_assert_not_reached ;
         }
 
     protected:
