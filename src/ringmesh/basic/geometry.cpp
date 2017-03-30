b403ee67351d3f2aa3d42c0ed82cb8ff5b4fff34
Pierre Anquez <pierre.anquez3@gmail.com>
2017-03-30 17:25:56 +0200

    End test geometry intersection

--- src/ringmesh/basic/geometry.cpp
+++ src/ringmesh/basic/geometry.cpp
@@ -649,13 +649,13 @@ namespace RINGMesh {
         vec3 norm_N_P0 = normalize( N_P0 ) ;
         vec3 norm_N_P1 = normalize( N_P1 ) ;
         double norm_d = dot( norm_N_P0, norm_N_P1 ) ;
+
         // Planes are parallel
         if( std::fabs( norm_d ) - 1 < global_epsilon
             && std::fabs( norm_d ) - 1 > -global_epsilon ) {
             return false ;
         }
 
-//        double d = dot( N_P0, N_P1 ) ;
         double invDet = 1.0 / ( 1.0 - norm_d * norm_d ) ;
         double const_P0 = dot( norm_N_P0, O_P0 ) ;
         double const_P1 = dot( norm_N_P1, O_P1 ) ;
