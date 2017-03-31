644717f35bd6f64e293b7036f3a9f3ae74f3826e
Pierre Anquez <32702237+panquez@users.noreply.github.com>
2017-03-31 13:53:55 +0000

    geogram_mesh_builder.h edited online with Bitbucket

--- include/ringmesh/mesh/geogram_mesh_builder.h
+++ include/ringmesh/mesh/geogram_mesh_builder.h
@@ -146,8 +146,8 @@ namespace RINGMesh {
         }
 
         /*!
-         * @brief set vertex coordinantes from a std::vector of coordinates
-         * @param[in] points_xyz_coordinates std::vector with a set of x, y, z coordinates
+         * @brief set vertex coordinates from a std::vector of coordinates
+         * @param[in] points_xyz_coordinates a set of x, y, z coordinates
          */
         virtual void assign_vertices(
             const std::vector< double >& points_xyz_coordinates ) override
