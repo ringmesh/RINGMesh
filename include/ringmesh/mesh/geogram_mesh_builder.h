389d2f6ba8da92a9a9d7149168c7fe932779d942
arnaud botella <3213882+BotellaA@users.noreply.github.com>
2017-04-20 17:25:23 +0200

    Some cleaning

--- include/ringmesh/mesh/geogram_mesh_builder.h
+++ include/ringmesh/mesh/geogram_mesh_builder.h
@@ -158,31 +158,18 @@ namespace RINGMesh {
         {
             set_geogram_mesh( dynamic_cast< GeogramMesh1D& >( mesh ) );
         }
-        /*!
-         * @brief Create a new edge.
-         * @param[in] v1_id index of the starting vertex.
-         * @param[in] v2_id index of the ending vertex.
-         */
+
         virtual void create_edge( index_t v1_id, index_t v2_id ) override
         {
             mesh_->mesh_->edges.create_edge( v1_id, v2_id );
             clear_edge_linked_objects();
         }
-        /*!
-         * \brief Creates a contiguous chunk of edges
-         * \param[in] nb_edges number of edges to create
-         * \return the index of the first edge
-         */
+
         virtual index_t create_edges( index_t nb_edges ) override
         {
             return mesh_->mesh_->edges.create_edges( nb_edges );
         }
-        /*!
-         * @brief Sets a vertex of a facet by local vertex index.
-         * @param[in] edge_id index of the edge, in 0..nb()-1.
-         * @param[in] local_vertex_id index of the vertex in the facet. Local index between 0 and @function nb_vertices(cell_id) - 1.
-         * @param[in] vertex_id specifies the vertex \param local_vertex_id of facet \param of the facet facet_id. Index between 0 and @function nb() - 1.
-         */
+
         virtual void set_edge_vertex(
             index_t edge_id,
             index_t local_vertex_id,
@@ -191,13 +178,7 @@ namespace RINGMesh {
             mesh_->mesh_->edges.set_vertex( edge_id, local_vertex_id, vertex_id );
             clear_edge_linked_objects();
         }
-        /*!
-         * @brief Deletes a set of edges.
-         * @param[in] to_delete     a vector of size @function nb().
-         * If to_delete[e] is true, then entity e will be destroyed, else it will be kept.
-         * @param[in] remove_isolated_vertices if true, then the vertices that are
-         * no longer incident to any entity are deleted.
-         */
+
         virtual void delete_edges(
             const std::vector< bool >& to_delete,
             bool remove_isolated_vertices ) override
@@ -210,22 +191,13 @@ namespace RINGMesh {
             }
             clear_edge_linked_objects();
         }
-        /*!
-         * @brief Removes all the edges and attributes.
-         * @param[in] keep_attributes if true, then all the existing attribute
-         * names / bindings are kept (but they are cleared). If false, they are destroyed.
-         * @param[in] keep_memory if true, then memory is kept and can be reused
-         * by subsequent mesh entity creations.
-         */
+
         virtual void clear_edges( bool keep_attributes, bool keep_memory ) override
         {
             mesh_->mesh_->edges.clear( keep_attributes, keep_memory );
             clear_edge_linked_objects();
         }
 
-        /*!
-         * @brief Remove vertices not connected to any mesh element
-         */
         virtual void remove_isolated_vertices() override
         {
             std::vector< bool > to_delete( mesh_->nb_vertices(), true );
@@ -275,14 +247,7 @@ namespace RINGMesh {
         {
             set_geogram_mesh( dynamic_cast< GeogramMesh2D& >( mesh ) );
         }
-        /*!
-         * \brief Removes the connected components that have an area
-         *  smaller than a given threshold.
-         * \param[in] min_area the connected components with an
-         *  area smaller than this threshold are removed
-         * \param[in] min_facets the connected components with
-         *  less than \param min_facets facets are removed
-         */
+
         virtual void remove_small_connected_components(
             double min_area,
             index_t min_facets ) override
@@ -303,11 +268,7 @@ namespace RINGMesh {
             CVT.compute_surface( mesh_->mesh_.get(), false );
             Logger::instance()->set_minimal( false );
         }
-        /*!
-         * brief create facet polygons
-         * @param[in] facets is the vector of vertex index for each facet
-         * @param[in] facet_ptr is the vector addressing the first facet vertex for each facet.
-         */
+
         virtual void create_facet_polygons(
             const std::vector< index_t >& facets,
             const std::vector< index_t >& facet_ptr ) override
@@ -321,12 +282,7 @@ namespace RINGMesh {
             }
             clear_facet_linked_objects();
         }
-        /*!
-         * \brief Creates a polygonal facet
-         * \param[in] vertices a const reference to a vector that
-         *  contains the vertices
-         * \return the index of the created facet
-         */
+
         virtual index_t create_facet_polygon(
             const std::vector< index_t >& vertices ) override
         {
@@ -337,31 +293,17 @@ namespace RINGMesh {
             return index;
         }
 
-        /*!
-         * \brief Creates a contiguous chunk of triangles
-         * \param[in] nb_triangles number of triangles to create
-         * \return the index of the first triangle
-         */
         virtual index_t create_facet_triangles( index_t nb_triangles ) override
         {
             return mesh_->mesh_->facets.create_triangles( nb_triangles );
 
         }
-        /*!
-         * \brief Creates a contiguous chunk of quads
-         * \param[in] nb_quads number of quads to create
-         * \return the index of the first quad
-         */
+
         virtual index_t create_facet_quads( index_t nb_quads ) override
         {
             return mesh_->mesh_->facets.create_quads( nb_quads );
         }
-        /*!
-         * @brief Sets a vertex of a facet by local vertex index.
-         * @param[in] facet_id index of the facet, in 0.. @function nb() - 1.
-         * @param[in] local_vertex_id index of the vertex in the facet. Local index between 0 and @function nb_vertices(cell_id) - 1.
-         * @param[in] vertex_id specifies the vertex \param local_vertex_id of the facet \param facet_id. Index between 0 and @function nb() - 1.
-         */
+
         virtual void set_facet_vertex(
             index_t facet_id,
             index_t local_vertex_id,
@@ -370,12 +312,7 @@ namespace RINGMesh {
             mesh_->mesh_->facets.set_vertex( facet_id, local_vertex_id, vertex_id );
             clear_facet_linked_objects();
         }
-        /*!
-         * @brief Sets an adjacent facet by both its facet \param facet_id and its local edge index \param edge_id.
-         * @param[in] facet_id the facet index
-         * @param[in] edge_id the local index of an edge in facet \p facet_id
-         * @param[in] specifies the facet adjacent to \param facet_id along edge \param edge_id or GEO::NO_FACET if the parameter \param edge_id is on the border.
-         */
+
         virtual void set_facet_adjacent(
             index_t facet_id,
             index_t edge_id,
@@ -383,13 +320,7 @@ namespace RINGMesh {
         {
             mesh_->mesh_->facets.set_adjacent( facet_id, edge_id, specifies );
         }
-        /*!
-         * \brief Copies a triangle mesh into this Mesh.
-         * \details Facet adjacence are not computed.
-         *   Facet and corner attributes are zeroed.
-         * \param[in] triangles facet to vertex links
-         * (using vector::swap).
-         */
+
         virtual void assign_facet_triangle_mesh(
             const std::vector< index_t >& triangles ) override
         {
@@ -398,20 +329,12 @@ namespace RINGMesh {
             mesh_->mesh_->facets.assign_triangle_mesh( geo_triangles, false );
             clear_facet_linked_objects();
         }
-        /*!
-         * @brief Removes all the facets and attributes.
-         * @param[in] keep_attributes if true, then all the existing attribute
-         * names / bindings are kept (but they are cleared). If false, they are destroyed.
-         * @param[in] keep_memory if true, then memory is kept and can be reused
-         * by subsequent mesh entity creations.
-         */
+
         virtual void clear_facets( bool keep_attributes, bool keep_memory ) override
         {
             mesh_->mesh_->facets.clear( keep_attributes, keep_memory );
         }
-        /*!
-         * @brief Retrieve the adjacencies of facets
-         */
+
         virtual void connect_facets() override
         {
             mesh_->mesh_->facets.connect();
@@ -422,13 +345,7 @@ namespace RINGMesh {
                 copy_std_vector_to_geo_vector( permutation );
             mesh_->mesh_->facets.permute_elements( geo_vector_permutation );
         }
-        /*!
-         * @brief Deletes a set of facets.
-         * @param[in] to_delete     a vector of size @function nb().
-         * If to_delete[e] is true, then entity e will be destroyed, else it will be kept.
-         * @param[in] remove_isolated_vertices if true, then the vertices that are
-         * no longer incident to any entity are deleted.
-         */
+
         virtual void delete_facets(
             const std::vector< bool >& to_delete,
             bool remove_isolated_vertices ) override
@@ -441,9 +358,7 @@ namespace RINGMesh {
             }
             clear_facet_linked_objects();
         }
-        /*!
-         * @brief Remove vertices not connected to any mesh element
-         */
+
         virtual void remove_isolated_vertices() override
         {
             std::vector< bool > to_delete( mesh_->nb_vertices(), true );
@@ -492,24 +407,12 @@ namespace RINGMesh {
         {
             set_geogram_mesh( dynamic_cast< GeogramMesh3D& >( mesh ) );
         }
-        /*!
-         * @brief Creates a contiguous chunk of cells of the same type.
-         * @param[in] nb_cells  number of cells to create
-         * @param[in] type   type of the cells to create, one of GEO::MESH_TET, GEO::MESH_HEX,
-         * GEO::MESH_PRISM, GEO::MESH_PYRAMID, GEO::MESH_CONNECTOR.
-         * @return the first created cell.
-         */
+
         virtual index_t create_cells( index_t nb_cells, GEO::MeshCellType type ) override
         {
             return mesh_->mesh_->cells.create_cells( nb_cells, type );
         }
 
-        /*!
-         * \brief Copies a tets mesh into this Mesh.
-         * \details Cells adjacence are not computed.
-         *   cell and corner attributes are zeroed.
-         * \param[in] tets cells to vertex links
-         */
         virtual void assign_cell_tet_mesh( const std::vector< index_t >& tets ) override
         {
             GEO::vector< index_t > copy = copy_std_vector_to_geo_vector( tets );
@@ -517,12 +420,6 @@ namespace RINGMesh {
             clear_cell_linked_objects();
         }
 
-        /*!
-         * @brief Sets a vertex of a cell by local vertex index.
-         * @param[in] cell_id index of the cell, in 0.. @function nb() - 1.
-         * @param[in] local_vertex_id index of the vertex in the cell. Local index between 0 and @function nb_vertices(cell_id) - 1.
-         * @param[in] vertex_id specifies the global index of the vertex \param local_vertex_id in the cell \param cell_id. Index between 0 and @function nb() - 1.
-         */
         virtual void set_cell_vertex(
             index_t cell_id,
             index_t local_vertex_id,
@@ -531,11 +428,7 @@ namespace RINGMesh {
             mesh_->mesh_->cells.set_vertex( cell_id, local_vertex_id, vertex_id );
             clear_cell_linked_objects();
         }
-        /*!
-         * \brief Sets the vertex that a corner is incident to
-         * \param[in] corner_index the corner, in 0.. @function nb() - 1
-         * \param[in] vertex_index specifies the vertex that corner \param corner_index is incident to
-         */
+
         virtual void set_cell_corner_vertex_index(
             index_t corner_index,
             index_t vertex_index ) override
@@ -543,12 +436,7 @@ namespace RINGMesh {
             mesh_->mesh_->cell_corners.set_vertex( corner_index, vertex_index );
             clear_cell_linked_objects();
         }
-        /*!
-         * \brief Sets the cell adjacent
-         * \param[in] cell_index index of the cell
-         * \param[in] facet_index local index of the cell facet
-         * \param[in] cell_adjacent adjacent value to set
-         */
+
         virtual void set_cell_adjacent(
             index_t cell_index,
             index_t facet_index,
@@ -557,49 +445,24 @@ namespace RINGMesh {
             mesh_->mesh_->cells.set_adjacent( cell_index, facet_index,
                 cell_adjacent );
         }
-        /*!
-         * @brief Retrieve the adjacencies
-         */
+
         virtual void connect_cells() override
         {
             mesh_->mesh_->cells.connect();
         }
 
-        /*!
-         * @brief Removes all the cells and attributes.
-         * @param[in] keep_attributes if true, then all the existing attribute
-         * names / bindings are kept (but they are cleared). If false, they are destroyed.
-         * @param[in] keep_memory if true, then memory is kept and can be reused
-         * by subsequent mesh entity creations.
-         */
         virtual void clear_cells( bool keep_attributes, bool keep_memory ) override
         {
             mesh_->mesh_->cells.clear( keep_attributes, keep_memory );
         }
-        /*!
-         * @brief Applies a permutation to the entities and their attributes.
-         * On exit, permutation is modified (used for internal bookkeeping).
-         * Applying a permutation permutation is equivalent to:
-         * <code>
-         *  for(i=0; i<permutation.size(); i++) {
-         *      data2[i] = data[permutation[i]]
-         *       }
-         *  data = data2 ;
-         *  </code>
-         */
+
         virtual void permute_cells( const std::vector< index_t >& permutation ) override
         {
             GEO::vector< index_t > geo_vector_permutation =
                 copy_std_vector_to_geo_vector( permutation );
             mesh_->mesh_->cells.permute_elements( geo_vector_permutation );
         }
-        /*!
-         * @brief Deletes a set of cells.
-         * @param[in] to_delete     a vector of size @function nb().
-         * If to_delete[e] is true, then entity e will be destroyed, else it will be kept.
-         * @param[in] remove_isolated_vertices if true, then the vertices that are
-         * no longer incident to any entity are deleted.
-         */
+
         virtual void delete_cells(
             const std::vector< bool >& to_delete,
             bool remove_isolated_vertices ) override
@@ -612,9 +475,7 @@ namespace RINGMesh {
             }
             clear_cell_linked_objects();
         }
-        /*!
-         * @brief Remove vertices not connected to any mesh element
-         */
+
         virtual void remove_isolated_vertices() override
         {
             std::vector< bool > to_delete( mesh_->nb_vertices(), true );
