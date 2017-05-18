c8b3c455c136f8c63ae2b64a4011243ad4a75ad5
Arnaud Botella <arnaud.botella@total.com>
2017-05-18 10:44:20 +0200

    Some cleanning

--- include/ringmesh/geomodel/geomodel_builder.h
+++ include/ringmesh/geomodel/geomodel_builder.h
@@ -191,8 +191,6 @@ namespace RINGMesh {
             const gmme_id& child_gmme,
             const gmge_id& parent_gmge )
         {
-            DEBUG( child_gmme );
-            DEBUG( parent_gmge );
             GeoModelMeshEntity& mesh_entity =
                 geomodel_access_.modifiable_mesh_entity( child_gmme );
             GeoModelMeshEntityAccess gmme_access( mesh_entity );
