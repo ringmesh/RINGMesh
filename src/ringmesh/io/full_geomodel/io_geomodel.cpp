9efb59e4746afb53df9a8f3be9c469a627a5e26a
Arnaud Botella <3213882+BotellaA@users.noreply.github.com>
2017-05-29 17:39:20 +0200

    Merge with 87befad336d198cc119d27cd46ab3960869ea2da

---
+++ src/ringmesh/io/full_geomodel/io_geomodel.cpp
@@ -0,0 +1,298 @@
+/*
+ * Copyright (c) 2012-2017, Association Scientifique pour la Geologie et ses Applications (ASGA)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the <organization> nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *     http://www.ring-team.org
+ *
+ *     RING Project
+ *     Ecole Nationale Superieure de Geologie - GeoRessources
+ *     2 Rue du Doyen Marcel Roubault - TSA 70605
+ *     54518 VANDOEUVRE-LES-NANCY
+ *     FRANCE
+ */
+
+namespace {
+    /*!
+     * @brief Write in the out stream things to save for CONTACT, INTERFACE and LAYERS
+     */
+    void save_geological_entity(
+        std::ofstream& out,
+        const GeoModelGeologicalEntity& E )
+    {
+        /// First line:  TYPE - ID - NAME - GEOL
+        out << E.gmge() << " " << E.name() << " ";
+        out << GeoModelGeologicalEntity::geol_name( E.geological_feature() )
+            << std::endl;
+
+        /// Second line:  IDS of children
+        for( index_t j = 0; j < E.nb_children(); ++j ) {
+            out << E.child_gmme( j ).index() << " ";
+        }
+        out << std::endl;
+    }
+
+    /*!
+     * @brief Save the connectivity of a GeoModel in a file
+     * @param[in] M the GeoModel
+     * @param[in] file_name path to the input file
+     */
+    void save_geological_entities( const GeoModel& M, const std::string& file_name )
+    {
+        std::ofstream out( file_name.c_str() );
+        out.precision( 16 );
+        if( out.bad() ) {
+            throw RINGMeshException( "I/O",
+                "Error when opening the file: " + file_name );
+        }
+
+        if( M.nb_geological_entity_types() == 0 ) {
+            // Compression of an empty files crashes ? (in debug on windows at least)
+            out << "No geological entity in the geomodel" << std::endl;
+            return;
+        }
+        for( index_t i = 0; i < M.nb_geological_entity_types(); i++ ) {
+            const std::string& type = M.geological_entity_type( i );
+            index_t nb = M.nb_geological_entities( type );
+            out << "Nb " << type << " " << nb << std::endl;
+        }
+        for( index_t i = 0; i < M.nb_geological_entity_types(); i++ ) {
+            const std::string& type = M.geological_entity_type( i );
+            index_t nb = M.nb_geological_entities( type );
+            for( index_t j = 0; j < nb; ++j ) {
+                save_geological_entity( out, M.geological_entity( type, j ) );
+            }
+        }
+    }
+
+    template< typename ENTITY >
+    void save_mesh_entities_of_type( const GeoModel& M, std::ofstream& out )
+    {
+        const std::string& type = ENTITY::type_name_static();
+        for( index_t e = 0; e < M.nb_mesh_entities( type ); e++ ) {
+            const ENTITY& cur_mesh_entity =
+                dynamic_cast< const ENTITY& >( M.mesh_entity( type, e ) );
+            out << type << " " << e << " " << cur_mesh_entity.name() << " "
+                << cur_mesh_entity.low_level_mesh_storage().type_name() << std::endl;
+            out << "boundary ";
+            for( index_t b = 0; b < cur_mesh_entity.nb_boundaries(); b++ ) {
+                out << cur_mesh_entity.boundary_gmme( b ).index() << " ";
+            }
+            out << std::endl;
+        }
+    }
+
+    /*!
+     * @brief Save the topology of a GeoModelin a file
+     * @param[in] M the GeoModel
+     * @param[in] file_name the output file name
+     */
+    void save_mesh_entities( const GeoModel& M, const std::string& file_name )
+    {
+        std::ofstream out( file_name.c_str() );
+        out.precision( 16 );
+        if( out.bad() ) {
+            throw RINGMeshException( "I/O",
+                "Error when opening the file: " + file_name );
+        }
+
+        out << "Version 2" << std::endl;
+        out << "GeoModel name " << M.name() << std::endl;
+
+        // Numbers of the different types of mesh entities
+        out << "Nb " << Corner::type_name_static() << " " << M.nb_corners()
+            << std::endl;
+        out << "Nb " << Line::type_name_static() << " " << M.nb_lines() << std::endl;
+        out << "Nb " << Surface::type_name_static() << " " << M.nb_surfaces()
+            << std::endl;
+        out << "Nb " << Region::type_name_static() << " " << M.nb_regions()
+            << std::endl;
+
+        save_mesh_entities_of_type< Corner >( M, out );
+        save_mesh_entities_of_type< Line >( M, out );
+        save_mesh_entities_of_type< Surface >( M, out );
+
+        // Regions
+        for( index_t i = 0; i < M.nb_regions(); ++i ) {
+            const Region& E = M.region( i );
+            // Save ID - NAME
+            out << Region::type_name_static() << " " << i << " " << E.name() << " "
+                << E.low_level_mesh_storage().type_name() << std::endl;
+            // Second line Signed ids of boundary surfaces
+            for( index_t j = 0; j < E.nb_boundaries(); ++j ) {
+                if( E.side( j ) ) {
+                    out << "+";
+                } else {
+                    out << "-";
+                }
+                out << E.boundary_gmme( j ).index() << " ";
+            }
+            out << std::endl;
+        }
+
+        // Universe
+        out << "Universe " << std::endl;
+        for( index_t j = 0; j < M.universe().nb_boundaries(); ++j ) {
+            if( M.universe().side( j ) ) {
+                out << "+";
+            } else {
+                out << "-";
+            }
+            out << M.universe().boundary_gmme( j ).index() << " ";
+        }
+        out << std::endl;
+    }
+
+    bool save_mesh(
+        const GeoModelMeshEntity& geomodel_entity_mesh,
+        const std::string& name )
+    {
+        if( geomodel_entity_mesh.type_name() == Region::type_name_static() ) {
+            const Region& region = geomodel_entity_mesh.geomodel().region(
+                geomodel_entity_mesh.index() );
+            if( !region.is_meshed() ) {
+                // a region is not necessary meshed.
+                return false;
+            }
+        }
+        geomodel_entity_mesh.save( name );
+        return true;
+    }
+
+    template< typename ENTITY >
+    std::string build_string_for_geomodel_entity_export( const ENTITY& entity )
+    {
+        const gmme_id& id = entity.gmme();
+        std::string base_name = static_cast< std::string >( id.type() ) + "_"
+            + GEO::String::to_string( id.index() );
+        return base_name + "." + entity.low_level_mesh_storage().default_extension();
+    }
+
+    /*!
+     * @brief Save the GeoModelMeshEntity in a meshb file
+     * @param[in] geomodel_entity_mesh the GeoModelMeshEntity you want to save
+     * @param[in] zf the zip file
+     */
+    template< typename ENTITY >
+    void save_geomodel_mesh_entity(
+        const ENTITY& geomodel_entity_mesh,
+        std::vector< std::string >& filenames )
+    {
+        std::string name = build_string_for_geomodel_entity_export(
+            geomodel_entity_mesh );
+        if( save_mesh( geomodel_entity_mesh, name ) ) {
+#pragma omp critical
+            {
+                filenames.push_back( name );
+            }
+        }
+    }
+
+    void zip_files( const std::vector< std::string >& filenames, zipFile& zf )
+    {
+        for( index_t i = 0; i < filenames.size(); i++ ) {
+            const std::string& name = filenames[i];
+            zip_file( zf, name );
+            GEO::FileSystem::delete_file( name );
+        }
+    }
+
+    template< typename ENTITY >
+    void save_geomodel_mesh_entities(
+        const GeoModel& geomodel,
+        std::vector< std::string >& filenames )
+    {
+        const std::string& type = ENTITY::type_name_static();
+        GEO::Logger* logger = Logger::instance();
+        bool logger_status = logger->is_quiet();
+        logger->set_quiet( true );
+        RINGMESH_PARALLEL_LOOP_DYNAMIC
+        for( index_t e = 0; e < geomodel.nb_mesh_entities( type ); e++ ) {
+            const ENTITY& entity =
+                dynamic_cast< const ENTITY& >( geomodel.mesh_entity( type, e ) );
+            save_geomodel_mesh_entity< ENTITY >( entity, filenames );
+        }
+        logger->set_quiet( logger_status );
+    }
+
+    class GeoModelHandlerGM final: public GeoModelIOHandler {
+    public:
+        virtual bool load( const std::string& filename, GeoModel& geomodel ) final
+        {
+            std::string pwd = GEO::FileSystem::get_current_working_directory();
+            GEO::FileSystem::set_current_working_directory(
+                GEO::FileSystem::dir_name( filename ) );
+            GeoModelBuilderGM builder( geomodel,
+                GEO::FileSystem::base_name( filename, false ) );
+            builder.build_geomodel();
+            Logger::out( "I/O", " Loaded geomodel ", geomodel.name(), " from ",
+                filename );
+            print_geomodel( geomodel );
+            bool is_valid = is_geomodel_valid( geomodel );
+            GEO::FileSystem::set_current_working_directory( pwd );
+            return is_valid;
+
+        }
+        virtual void save( const GeoModel& geomodel, const std::string& filename ) final
+        {
+            const std::string pwd = GEO::FileSystem::get_current_working_directory();
+            bool valid_new_working_directory =
+                GEO::FileSystem::set_current_working_directory(
+                    GEO::FileSystem::dir_name( filename ) );
+            if( !valid_new_working_directory ) {
+                throw RINGMeshException( "I/O", "Output directory does not exist" );
+            }
+
+            zipFile zf = zipOpen(
+                GEO::FileSystem::base_name( filename, false ).c_str(),
+                APPEND_STATUS_CREATE );
+            ringmesh_assert( zf != nil );
+
+            const std::string mesh_entity_file( "mesh_entities.txt" );
+            save_mesh_entities( geomodel, mesh_entity_file );
+            zip_file( zf, mesh_entity_file );
+            GEO::FileSystem::delete_file( mesh_entity_file );
+
+            const std::string geological_entity_file( "geological_entities.txt" );
+            save_geological_entities( geomodel, geological_entity_file );
+            zip_file( zf, geological_entity_file );
+            GEO::FileSystem::delete_file( geological_entity_file );
+
+            index_t nb_mesh_entites = geomodel.nb_corners() + geomodel.nb_lines()
+                + geomodel.nb_surfaces() + geomodel.nb_regions();
+            std::vector< std::string > filenames;
+            filenames.reserve( nb_mesh_entites );
+            save_geomodel_mesh_entities< Corner >( geomodel, filenames );
+            save_geomodel_mesh_entities< Line >( geomodel, filenames );
+            save_geomodel_mesh_entities< Surface >( geomodel, filenames );
+            save_geomodel_mesh_entities< Region >( geomodel, filenames );
+            std::sort( filenames.begin(), filenames.end() );
+            zip_files( filenames, zf );
+
+            zipClose( zf, NULL );
+            GEO::FileSystem::set_current_working_directory( pwd );
+        }
+    };
+
+}
---
+++ src/ringmesh/tetrahedralize/tetgen_mesher.cpp
@@ -0,0 +1,320 @@
+/*
+ * Copyright (c) 2012-2017, Association Scientifique pour la Geologie et ses Applications (ASGA)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of ASGA nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL ASGA BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *     http://www.ring-team.org
+ *
+ *     RING Project
+ *     Ecole Nationale Superieure de Geologie - GeoRessources
+ *     2 Rue du Doyen Marcel Roubault - TSA 70605
+ *     54518 VANDOEUVRE-LES-NANCY
+ *     FRANCE
+ */
+
+#include <ringmesh/tetrahedralize/tetgen_mesher.h>
+
+#include <cstring>
+
+#include <geogram/mesh/mesh.h>
+#include <ringmesh/mesh/mesh.h>
+#include <ringmesh/mesh/mesh_builder.h>
+
+#ifdef RINGMESH_WITH_TETGEN
+/*!
+ * @file Implementation of the interface of GEO::Mesh with Tetgen
+ * @author Jeanne Pellerin
+ */
+
+namespace {
+    using namespace RINGMesh;
+
+    bool is_mesh_tetrahedralizable( const GEO::Mesh& M )
+    {
+        if( M.facets.nb() == 0 ) {
+            Logger::err( "RING", "Mesh to tetrahedralize has no polygons " );
+            return false;
+        }
+        if( !M.facets.are_simplices() ) {
+            Logger::err( "RING", "Mesh to tetrahedralize is not triangulated" );
+            return false;
+        }
+        if( M.cells.nb() != 0 ) {
+            Logger::warn( "RING", "Mesh to tetrahedralize already have cells" );
+        }
+        return true;
+    }
+}
+
+namespace RINGMesh {
+
+    TetgenMesher::~TetgenMesher()
+    {
+        // Take over polygon deletion of tetgen that does not set to
+        // nullptr pointers to polygonlist or holelist in polygon
+        delete[] tetgen_in_.facetlist;
+        tetgen_in_.facetlist = nullptr;
+        tetgen_in_.numberoffacets = 0;
+    }
+
+    void TetgenMesher::tetrahedralize(
+        const GEO::Mesh& input_mesh,
+        VolumeMeshBuilder< 3 >& output_mesh_builder )
+    {
+        initialize();
+        copy_mesh_to_tetgen_input( input_mesh );
+        tetrahedralize();
+        assign_result_tetmesh_to_mesh( output_mesh_builder );
+    }
+
+    void TetgenMesher::initialize()
+    {
+        initialize_tetgen_args();
+        tetgen_in_.initialize();
+        tetgen_out_.initialize();
+    }
+
+    void TetgenMesher::tetrahedralize()
+    {
+        try {
+            GEO_3rdParty::tetrahedralize( &tetgen_args_, &tetgen_in_, &tetgen_out_ );
+        } catch( int code ) {
+            Logger::err( "Tetgen", "Encountered a problem: " );
+            switch( code ) {
+                case 1:
+                    Logger::err( "Tetgen", "Out of memory" );
+                    break;
+                case 2:
+                    Logger::err( "Tetgen",
+                        "Please report this bug to Hang.Si@wias-berlin.de. Include" );
+                    Logger::err( "Tetgen",
+                        "  the message above, your input data set, and the exact" );
+                    Logger::err( "Tetgen",
+                        "  command line you used to run this program, thank you" );
+                    ;
+                    break;
+                case 3:
+                    Logger::err( "Tetgen",
+                        "A self-intersection was detected. Program stopped" );
+                    Logger::err( "Tetgen",
+                        "Hint: use -d option to detect all self-intersections" );
+                    break;
+                case 4:
+                    Logger::err( "Tetgen",
+                        "A very small input feature size was detected. Program stopped." );
+                    Logger::err( "Tetgen",
+                        "Hint: use -T option to set a smaller tolerance." );
+                    break;
+                case 5:
+                    Logger::err( "Tetgen",
+                        "Two very close input polygons were detected. Program stopped." );
+                    Logger::err( "Tetgen",
+                        "Hint: use -Y option to avoid adding Steiner points in boundary." );
+                    break;
+                case 10:
+                    Logger::err( "Tetgen",
+                        "An input error was detected. Program stopped." );
+                    break;
+            }
+        }
+    }
+
+    void TetgenMesher::copy_mesh_to_tetgen_input( const GEO::Mesh& M )
+    {
+        if( M.vertices.nb() != 0 ) {
+            copy_vertices_to_tetgen_input( M );
+        }
+        if( M.edges.nb() != 0 ) {
+            copy_edges_to_tetgen_input( M );
+        }
+        if( M.facets.nb() != 0 ) {
+            copy_polygons_to_tetgen_input( M );
+        }
+    }
+
+    void TetgenMesher::copy_vertices_to_tetgen_input( const GEO::Mesh& M )
+    {
+        tetgen_in_.numberofpoints = static_cast< int >( M.vertices.nb() );
+        tetgen_in_.pointlist = new double[3 * tetgen_in_.numberofpoints];
+        GEO::Memory::copy( tetgen_in_.pointlist, M.vertices.point_ptr( 0 ),
+            M.vertices.nb() * 3 * sizeof(double) );
+    }
+
+    void TetgenMesher::copy_edges_to_tetgen_input( const GEO::Mesh& M )
+    {
+        tetgen_in_.numberofedges = static_cast< int >( M.edges.nb() );
+        tetgen_in_.edgelist = new int[2 * tetgen_in_.numberofedges];
+        GEO::Memory::copy( tetgen_in_.edgelist, M.edges.vertex_index_ptr( 0 ),
+            M.edges.nb() * 2 * sizeof(int) );
+    }
+
+    void TetgenMesher::copy_polygons_to_tetgen_input( const GEO::Mesh& M )
+    {
+        polygons_.reset( new GEO_3rdParty::tetgenio::polygon[M.facets.nb()] );
+
+        tetgen_in_.numberoffacets = static_cast< int >( M.facets.nb() );
+        tetgen_in_.facetlist =
+            new GEO_3rdParty::tetgenio::facet[tetgen_in_.numberoffacets];
+
+        polygon_corners_.reset( new int[M.facet_corners.nb()] );
+        GEO::Memory::copy( polygon_corners_.get(),
+            M.facet_corners.vertex_index_ptr( 0 ),
+            M.facet_corners.nb() * sizeof(int) );
+
+        for( index_t f = 0; f < M.facets.nb(); ++f ) {
+            GEO_3rdParty::tetgenio::facet& F = tetgen_in_.facetlist[f];
+            GEO_3rdParty::tetgenio::init( &F );
+            F.numberofpolygons = 1;
+            F.polygonlist = &polygons_[f];
+
+            GEO_3rdParty::tetgenio::polygon& P = F.polygonlist[0];
+            GEO_3rdParty::tetgenio::init( &P );
+            P.numberofvertices = static_cast< int >( M.facets.nb_corners( f ) );
+            P.vertexlist = &polygon_corners_[M.facets.corners_begin( f )];
+        }
+    }
+
+    void TetgenMesher::set_regions(
+        const std::vector< vec3 >& one_point_in_each_region )
+    {
+        index_t nb_regions = one_point_in_each_region.size();
+        tetgen_in_.numberofregions = static_cast< int >( nb_regions );
+        tetgen_in_.regionlist = new double[5 * nb_regions];
+
+        for( index_t i = 0; i != nb_regions; ++i ) {
+            tetgen_in_.regionlist[5 * i] = one_point_in_each_region[i].x;
+            tetgen_in_.regionlist[5 * i + 1] = one_point_in_each_region[i].y;
+            tetgen_in_.regionlist[5 * i + 2] = one_point_in_each_region[i].z;
+            tetgen_in_.regionlist[5 * i + 3] = i;
+            tetgen_in_.regionlist[5 * i + 4] = DBL_MAX; // Used only with the a switch
+        }
+    }
+
+    void TetgenMesher::initialize_tetgen_args()
+    {
+        std::unique_ptr< char[] > copy(
+            new char[tetgen_command_line_.length() + 1] );
+        std::strcpy( copy.get(), tetgen_command_line_.c_str() );
+        tetgen_args_.parse_commandline( copy.get() );
+    }
+
+    void TetgenMesher::assign_result_tetmesh_to_mesh(
+        VolumeMeshBuilder< 3 >& output_mesh_builder ) const
+    {
+        output_mesh_builder.assign_vertices( get_result_tetmesh_points() );
+        output_mesh_builder.assign_cell_tet_mesh( get_result_tetmesh_tets() );
+        output_mesh_builder.remove_isolated_vertices();
+        output_mesh_builder.connect_cells();
+    }
+
+    std::vector< double > TetgenMesher::get_result_tetmesh_points() const
+    {
+        index_t nb_points = static_cast< index_t >( tetgen_out_.numberofpoints );
+        std::vector< double > points( 3 * nb_points );
+        double* points_ptr = tetgen_out_.pointlist;
+        RINGMESH_PARALLEL_LOOP
+        for( index_t i = 0; i < 3 * nb_points; ++i ) {
+            points[i] = points_ptr[i];
+        }
+        return points;
+    }
+
+    std::vector< index_t > TetgenMesher::get_result_tetmesh_tets() const
+    {
+        std::vector< index_t > tets_to_keep = determine_tets_to_keep();
+
+        index_t nb_tets = static_cast< index_t >( tets_to_keep.size() );
+        std::vector< index_t > tets( 4 * nb_tets );
+        int* tets_ptr = tetgen_out_.tetrahedronlist;
+        RINGMESH_PARALLEL_LOOP
+        for( index_t i = 0; i < nb_tets; ++i ) {
+            index_t tetra = tets_to_keep[i];
+            for( index_t v = 0; v < 4; v++ ) {
+                tets[4 * i + v] = static_cast< index_t >( tets_ptr[4 * tetra + v] );
+            }
+        }
+        return tets;
+    }
+
+    std::set< double > TetgenMesher::determine_tet_regions_to_keep() const
+    {
+        // Determine which regions are incident to
+        // the 'exterior' (neighbor = -1).
+        // The region Id of tet t is determined by:
+        //  tetgen_out_.tetrahedronattributelist[t]
+        std::set< double > regions_to_keep;
+        index_t nb_tets = static_cast< index_t >( tetgen_out_.numberoftetrahedra );
+        for( index_t t = 0; t < nb_tets; ++t ) {
+            for( index_t f = 0; f < 4; ++f ) {
+                signed_index_t n = tetgen_out_.neighborlist[t * 4 + f];
+                if( n == -1 ) {
+                    regions_to_keep.insert(
+                        tetgen_out_.tetrahedronattributelist[t] );
+                    break;
+                }
+            }
+        }
+        return regions_to_keep;
+    }
+
+    std::vector< index_t > TetgenMesher::determine_tets_to_keep() const
+    {
+        std::vector< index_t > tets_to_keep;
+        std::set< double > regions_to_keep = determine_tet_regions_to_keep();
+
+        index_t nb_tets = static_cast< index_t >( tetgen_out_.numberoftetrahedra );
+        tets_to_keep.reserve( nb_tets );
+        for( index_t t = 0; t < nb_tets; ++t ) {
+            if( regions_to_keep.find( tetgen_out_.tetrahedronattributelist[t] )
+                != regions_to_keep.end() ) {
+                tets_to_keep.push_back( t );
+            }
+        }
+        return tets_to_keep;
+    }
+
+    void tetrahedralize_mesh_tetgen(
+        VolumeMeshBuilder< 3 >& out_tet_mesh,
+        const GEO::Mesh& in_mesh,
+        bool refine,
+        double quality )
+    {
+        if( !is_mesh_tetrahedralizable( in_mesh ) ) {
+            throw RINGMeshException( "TetGen", "Mesh cannot be tetrahedralized" );
+        }
+        TetgenMesher mesher;
+        if( refine ) {
+            mesher.add_points_to_match_quality( quality );
+        }
+        mesher.tetrahedralize( in_mesh, out_tet_mesh );
+    }
+
+    void TetgenMesher::add_points_to_match_quality( double quality )
+    {
+        tetgen_command_line_ += "q" + GEO::String::to_string( quality );
+    }
+}
+
+#endif
+
