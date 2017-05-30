0140c07486dc876d2f9941586359fd0ac622f6ac
Benjamin Chauvin <31856424+benjaminchauvin@users.noreply.github.com>
2017-05-30 06:30:35 +0000

    geomodel_mesh_entity.h edited online with Bitbucket

--- include/ringmesh/geomodel/geomodel_mesh_entity.h
+++ include/ringmesh/geomodel/geomodel_mesh_entity.h
@@ -155,7 +155,8 @@ namespace RINGMesh {
         /*!
          * @brief Returns the gmge_id of the parent of the given type.
          * @pre The code assumes that this entity has a parent of the given type
-         * \p parent_type_name, else it will crash.
+         * \p parent_type_name, else it will crash in debug and return an invalid
+         * entity in release.
          * The method has_parent(GeologicalEntityType) is a safe way to check
          * if the type \p parent_type_name is a valid parent type for the
          * GeoModelMeshEntity.
