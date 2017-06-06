576678b6265b2fedd8020276d4a95c1c08591f18
Arnaud Botella <3213882+BotellaA@users.noreply.github.com>
2017-06-06 09:11:50 +0200

    Split GeoModelBuilder file

--- include/ringmesh/geomodel/geomodel_builder.h
+++ include/ringmesh/geomodel/geomodel_builder.h
@@ -42,6 +42,8 @@
 #include <ringmesh/geomodel/geomodel_mesh_entity.h>
 #include <ringmesh/geomodel/geomodel_geological_entity.h>
 
+#include <ringmesh/geomodel/geomodel_builder_from_surfaces.h>
+#include <ringmesh/geomodel/geomodel_builder_geology.h>
 #include <ringmesh/geomodel/geomodel_builder_geometry.h>
 #include <ringmesh/geomodel/geomodel_builder_remove.h>
 #include <ringmesh/geomodel/geomodel_builder_repair.h>
@@ -54,39 +56,6 @@
 
 namespace RINGMesh {
     template< index_t DIMENSION > class GeoModelBuilder;
-}
-
-namespace RINGMesh {
-    /*!
-     * @brief First draft of flags to build a GeoModel
-     * @todo Implements functions to set, access the values, depending on what ?
-     * To check the consistency of the options. What do we do about the other entities ? [JP] 
-     * 
-     * @todo We need to keep track of the status of the GeoModel when building it:
-     * same flags or some others ?    
-     *
-     * @todo To separate in two classes ? One providing the low level functions set, assign etc,
-     * and the other one some high level functions. [JP]
-     */
-    class GeoModelBuildingFlags {
-    public:
-        GeoModelBuildingFlags()
-        {
-            compute_corners = false;
-            compute_lines = false;
-            compute_surfaces = false;
-            compute_regions_brep = false;
-            compute_regions_mesh = false;
-        }
-        bool compute_corners;
-        bool compute_lines;
-        bool compute_surfaces;
-        bool compute_regions_brep;
-        bool compute_regions_mesh;
-    };
-
-    // Implementation details
-    class GeoModelRegionFromSurfaces;
 }
 
 namespace RINGMesh {
@@ -142,109 +111,6 @@ namespace RINGMesh {
     };
 
     template< index_t DIMENSION >
-    class GeoModelBuilderGeology {
-    ringmesh_disable_copy( GeoModelBuilderGeology );
-        ringmesh_template_assert_2d_or_3d( DIMENSION );
-        friend class GeoModelBuilder< DIMENSION >;
-
-    public:
-        void copy_geology( const GeoModel< DIMENSION >& from );
-
-        /*!
-         * @brief Create and store a geological entity of the given type
-         * @return The index of the created geological entity
-         */
-        gmge_id create_geological_entity( const GeologicalEntityType& type );
-
-        bool create_geological_entities(
-            const GeologicalEntityType& type,
-            index_t nb );
-
-        void set_geological_entity_geol_feature(
-            const gmge_id& gmge_id,
-            typename GeoModelGeologicalEntity< DIMENSION >::GEOL_FEATURE geol_feature );
-
-        void set_mesh_entity_parent(
-            const gmme_id& child_gmme,
-            index_t id,
-            const gmge_id& parent_gmge )
-        {
-            /// No check on the validity of the index of the entity parents_
-            /// NO_ID is used to flag entities to delete
-            GeoModelMeshEntity< DIMENSION >& mesh_entity =
-                geomodel_access_.modifiable_mesh_entity( child_gmme );
-            ringmesh_assert( id < mesh_entity.nb_parents() );
-            GeoModelMeshEntityAccess< DIMENSION > gmme_access( mesh_entity );
-            index_t relationship_id = gmme_access.modifiable_parents()[id];
-            RelationshipManager& manager =
-                geomodel_access_.modifiable_entity_type_manager().relationship_manager;
-            manager.set_parent_to_parent_child_relationship( relationship_id,
-                parent_gmge );
-        }
-
-        void add_parent_children_relation(
-            const gmge_id& parent,
-            const gmme_id& children );
-
-        void remove_parent_children_relation(
-            const gmge_id& parent,
-            const gmme_id& children );
-        void set_geological_entity_child(
-            const gmge_id& parent_gmge,
-            index_t id,
-            index_t child_id )
-        {
-            /// No check on the validity of the index of the entity child_index
-            /// NO_ID is used to flag entities to delete
-            GeoModelGeologicalEntity< DIMENSION >& geol_entity =
-                geomodel_access_.modifiable_geological_entity( parent_gmge );
-            const MeshEntityType& child_type =
-                geomodel_.entity_type_manager().relationship_manager.child_type(
-                    parent_gmge.type() );
-            gmme_id child( child_type, child_id );
-            GeoModelGeologicalEntityAccess< DIMENSION > gmge_access( geol_entity );
-            index_t relationship_id = gmge_access.modifiable_children()[id];
-            RelationshipManager& manager =
-                geomodel_access_.modifiable_entity_type_manager().relationship_manager;
-            manager.set_child_to_parent_child_relationship( relationship_id, child );
-        }
-
-        void delete_geological_entity(
-            const GeologicalEntityType& type,
-            index_t index );
-
-        /*!
-         * @brief Build the Contacts
-         * @details One contact is a group of lines shared by the same Interfaces
-         */
-        void build_contacts();
-
-    protected:
-        GeoModelBuilderGeology(
-            GeoModelBuilder< DIMENSION >& builder,
-            GeoModel< DIMENSION >& geomodel );
-
-    private:
-        index_t create_geological_entity_type( const GeologicalEntityType& type );
-
-        index_t find_or_create_geological_entity_type(
-            const GeologicalEntityType& type );
-
-        void copy_geological_entity_topology(
-            const GeoModel< DIMENSION >& from,
-            const GeologicalEntityType& type );
-
-        bool check_if_boundary_incident_entity_relation_already_exists(
-            const gmge_id& parent,
-            const gmme_id& children );
-
-    private:
-        GeoModelBuilder< DIMENSION >& builder_;
-        GeoModel< DIMENSION >& geomodel_;
-        GeoModelAccess< DIMENSION > geomodel_access_;
-    };
-
-    template< index_t DIMENSION >
     class GeoModelBuilderCopy {
     ringmesh_disable_copy( GeoModelBuilderCopy );
         ringmesh_template_assert_2d_or_3d( DIMENSION );
@@ -261,54 +127,6 @@ namespace RINGMesh {
         GeoModelBuilder< DIMENSION >& builder_;
         GeoModel< DIMENSION >& geomodel_;
         GeoModelAccess< DIMENSION > geomodel_access_;
-    };
-
-    template< index_t DIMENSION >
-    class GeoModelBuilderFromSurfaces {
-    ringmesh_disable_copy( GeoModelBuilderFromSurfaces );
-        ringmesh_template_assert_2d_or_3d( DIMENSION );
-        friend class GeoModelBuilder< DIMENSION >;
-
-    public:
-        /*!
-         * Delete all GeoModelRegionFromSurfaces owned by the builder
-         */
-        virtual ~GeoModelBuilderFromSurfaces();
-        /*
-         * @brief From a GeoModel in which only Surfaces are defined,
-         * create Corners, Lines and Regions depending on the building flags
-         * @note Validity is not checked
-         */
-        void build();
-
-        /*!
-         * @brief From the Surfaces of the GeoModel, build its Lines and Corners
-         */
-        bool build_lines_and_corners_from_surfaces();
-
-        /*!
-         * @brief Build the regions of the GeoModel from the Surfaces
-         * @pre Function build_lines_and_corners_from_surfaces
-         * must have been called before
-         */
-        bool build_brep_regions_from_surfaces();
-
-    private:
-        GeoModelBuilderFromSurfaces(
-            GeoModelBuilder< DIMENSION >& builder,
-            GeoModel< DIMENSION >& geomodel );
-
-    public:
-        /*! Options to toggle the building of entities from the available entities */
-        GeoModelBuildingFlags options_;
-
-    private:
-        GeoModelBuilder< DIMENSION >& builder_;
-        GeoModel< DIMENSION >& geomodel_;
-        GeoModelAccess< DIMENSION > geomodel_access_;
-
-        /*! Internal information */
-        std::vector< GeoModelRegionFromSurfaces* > regions_info_;
     };
 
     /*!
