c35447ac09cef4b27fd394fc0590e2e96609277d
Benjamin Chauvin <31856424+benjaminchauvin@users.noreply.github.com>
2017-07-10 23:51:59 -0400

    More tests.

--- tests/src/test-transfer-attributes-gm-gmm.cpp
+++ tests/src/test-transfer-attributes-gm-gmm.cpp
@@ -60,7 +60,7 @@ namespace {
         }
     }
 
-    void run_tests()
+    void tests_transfer_from_geomodel_regions_to_geomodelmesh()
     {
         GeoModel in;
         load_geomodel( in, "modelA1_volume_meshed.gm" );
@@ -68,18 +68,35 @@ namespace {
         // test cell from gm to gmm
         for( index_t reg_i = 0; reg_i < in.nb_regions(); ++reg_i ) {
             const Region& cur_reg = in.region( reg_i );
-            GEO::Attribute< double > cell_double_attr(
-                cur_reg.cell_attribute_manager(), "cell_double_attr" );
-            GEO::Attribute< vec3 > cell_vec3_attr( cur_reg.cell_attribute_manager(),
-                "cell_vec3_attr" );
-            /*GEO::Attribute< const char* > cell_string_attr(
-                cur_reg.cell_attribute_manager(), "cell_string_attr" );*/
+
+            GEO::AttributesManager& reg_attr_mgr = cur_reg.cell_attribute_manager();
+            GEO::Attribute< long int > cell_long_int_attr( reg_attr_mgr,
+                "cell_long_int_attr" );
+            GEO::Attribute< bool > cell_bool_attr( reg_attr_mgr, "cell_bool_attr" );
+            GEO::Attribute< double > cell_double_attr( reg_attr_mgr,
+                "cell_double_attr" );
+            GEO::Attribute< vec3 > cell_vec3_attr( reg_attr_mgr, "cell_vec3_attr" );
+            GEO::Attribute< double > cell_dim_6_double_attr;
+            cell_dim_6_double_attr.create_vector_attribute( reg_attr_mgr,
+                "cell_dim_6_double_attr", 6 );
+            GEO::Attribute< char > cell_char_attr( reg_attr_mgr, "cell_char_attr" );
+
             for( index_t cell_i = 0; cell_i < cur_reg.nb_mesh_elements();
                 ++cell_i ) {
-                cell_double_attr[cell_i] = cur_reg.mesh_element_size( cell_i );
+                const double cell_volume = cur_reg.mesh_element_size( cell_i );
+                const long int rounded_volume = std::lrint( cell_volume );
+                cell_long_int_attr[cell_i] = rounded_volume;
+                cell_bool_attr[cell_i] = ( rounded_volume % 2 == 0 );
+                cell_double_attr[cell_i] = cell_volume;
                 cell_vec3_attr[cell_i] = cur_reg.mesh_element_barycenter( cell_i );
-                /*cell_string_attr[cell_i] =
-                    std::to_string( cell_double_attr[cell_i] ).data()*/;
+                cell_dim_6_double_attr[cell_i * 6 + 0] = cell_vec3_attr[cell_i].x;
+                cell_dim_6_double_attr[cell_i * 6 + 1] = cell_vec3_attr[cell_i].y;
+                cell_dim_6_double_attr[cell_i * 6 + 2] = cell_vec3_attr[cell_i].z;
+                cell_dim_6_double_attr[cell_i * 6 + 3] = cell_vec3_attr[cell_i].x;
+                cell_dim_6_double_attr[cell_i * 6 + 4] = cell_vec3_attr[cell_i].y;
+                cell_dim_6_double_attr[cell_i * 6 + 5] = cell_vec3_attr[cell_i].z;
+                cell_char_attr[cell_i] =
+                    std::to_string( cell_vec3_attr[cell_i].y ).data()[0];
             }
         }
 
@@ -90,6 +107,14 @@ namespace {
         GEO::AttributesManager& gmmc_attr_mgr = gmmc.attribute_manager();
 
         /// TODO store attribute in string to avoid to write them several times...
+        if( !gmmc_attr_mgr.is_defined( "cell_long_int_attr" ) ) {
+            throw RINGMeshException( "RINGMesh Test",
+                "Problem of transfer from the GeoModel regions and the GeoModelMesh for cell_long_int_attr" );
+        }
+        if( !gmmc_attr_mgr.is_defined( "cell_bool_attr" ) ) {
+            throw RINGMeshException( "RINGMesh Test",
+                "Problem of transfer from the GeoModel regions and the GeoModelMesh for cell_bool_attr" );
+        }
         if( !gmmc_attr_mgr.is_defined( "cell_double_attr" ) ) {
             throw RINGMeshException( "RINGMesh Test",
                 "Problem of transfer from the GeoModel regions and the GeoModelMesh for cell_double_attr" );
@@ -98,34 +123,92 @@ namespace {
             throw RINGMeshException( "RINGMesh Test",
                 "Problem of transfer from the GeoModel regions and the GeoModelMesh for cell_vec3_attr" );
         }
-        /*if( !gmmc_attr_mgr.is_defined( "cell_string_attr" ) ) {
-            /// TODO put a better message, attr not defined in gmm...
+        if( !gmmc_attr_mgr.is_defined( "cell_dim_6_double_attr" ) ) {
             throw RINGMeshException( "RINGMesh Test",
-                "Problem of transfer from the GeoModel regions and the GeoModelMesh for cell_string_attr" );
-        }*/
+                "Problem of transfer from the GeoModel regions and the GeoModelMesh for cell_dim_6_double_attr" );
+        }
+        if( !gmmc_attr_mgr.is_defined( "cell_char_attr" ) ) {
+            throw RINGMeshException( "RINGMesh Test",
+                "Problem of transfer from the GeoModel regions and the GeoModelMesh for cell_char_attr" );
+        }
+
+        GEO::Attribute< long int > cell_long_int_attr( gmmc_attr_mgr,
+            "cell_long_int_attr" );
+        GEO::Attribute< bool > cell_bool_attr( gmmc_attr_mgr, "cell_bool_attr" );
         GEO::Attribute< double > cell_double_attr( gmmc_attr_mgr,
             "cell_double_attr" );
         GEO::Attribute< vec3 > cell_vec3_attr( gmmc_attr_mgr, "cell_vec3_attr" );
-        /*GEO::Attribute< const char* > cell_string_attr( gmmc_attr_mgr,
-            "cell_string_attr" );*/
+        GEO::Attribute< double > cell_dim_6_double_attr( gmmc_attr_mgr,
+            "cell_dim_6_double_attr" );
+        GEO::Attribute< char > cell_char_attr( gmmc_attr_mgr, "cell_char_attr" );
+
         for( index_t cell_i = 0; cell_i < gmmc.nb_cells(); ++cell_i ) {
-            double cell_volume = gmmc.volume( cell_i );
+            const double cell_volume = gmmc.volume( cell_i );
+            const long int rounded_volume = std::lrint( cell_volume );
+
+            if( rounded_volume != cell_long_int_attr[cell_i] ) {
+                throw RINGMeshException( "RINGMesh Test", "Bad transfer" ); //TODO improve message
+            }
+
+            bool is_pair = ( rounded_volume % 2 == 0 );
+            if( is_pair != cell_bool_attr[cell_i] ) {
+                throw RINGMeshException( "RINGMesh Test", "Bad transfer" ); //TODO improve message
+            }
+
             if( std::abs( cell_volume - cell_double_attr[cell_i] )
                 > in.epsilon3() ) {
                 throw RINGMeshException( "RINGMesh Test", "Bad transfer" ); //TODO improve message
             }
-            vec3 diff = gmmc.barycenter( cell_i ) - cell_vec3_attr[cell_i];
+
+            const vec3 cell_barycenter = gmmc.barycenter( cell_i );
+            const vec3 diff = cell_barycenter - cell_vec3_attr[cell_i];
             if( std::abs( diff.x ) > in.epsilon()
                 || std::abs( diff.y ) > in.epsilon()
                 || std::abs( diff.z ) > in.epsilon() ) {
                 throw RINGMeshException( "RINGMesh Test", "Bad transfer" ); //TODO improve message
             }
-            /*std::string volume_in_string = std::to_string( cell_volume );
-            if( volume_in_string != std::string( cell_string_attr[cell_i] ) ) {
+
+            if( std::abs(
+                cell_barycenter.x - cell_dim_6_double_attr[6 * cell_i + 0] )
+                > in.epsilon()
+                || std::abs(
+                    cell_barycenter.y - cell_dim_6_double_attr[6 * cell_i + 1] )
+                    > in.epsilon()
+                || std::abs(
+                    cell_barycenter.z - cell_dim_6_double_attr[6 * cell_i + 2] )
+                    > in.epsilon()
+                || std::abs(
+                    cell_barycenter.x - cell_dim_6_double_attr[6 * cell_i + 3] )
+                    > in.epsilon()
+                || std::abs(
+                    cell_barycenter.y - cell_dim_6_double_attr[6 * cell_i + 4] )
+                    > in.epsilon()
+                || std::abs(
+                    cell_barycenter.z - cell_dim_6_double_attr[6 * cell_i + 5] )
+                    > in.epsilon() ) {
                 throw RINGMeshException( "RINGMesh Test", "Bad transfer" ); //TODO improve message
-            }*/
+            }
+
+            const char char_vec3_y = std::to_string( cell_barycenter.y ).data()[0];
+            if( char_vec3_y != cell_char_attr[cell_i] ) {
+                throw RINGMeshException( "RINGMesh Test", "Bad transfer" ); //TODO improve message
+            }
         }
     }
+
+    void tests_transfer_from_geomodelmesh_to_geomodel_regions()
+    {
+
+    }
+
+    void run_tests()
+    {
+        // long int is not a default attribute type in geogram.
+        GEO::geo_register_attribute_type< long int >( "long int" );
+        tests_transfer_from_geomodel_regions_to_geomodelmesh();
+        tests_transfer_from_geomodelmesh_to_geomodel_regions();
+    }
+
 }
 
 int main()
