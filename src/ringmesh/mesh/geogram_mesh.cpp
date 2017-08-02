0e63681c7da4c53f23547873e5435fd345768c6d
AntoineMazuyer <32493098+AntoineMazuyer@users.noreply.github.com>
2017-08-02 10:05:22 +0200

    fake mesh register for surface

--- src/ringmesh/mesh/geogram_mesh.cpp
+++ src/ringmesh/mesh/geogram_mesh.cpp
@@ -43,31 +43,33 @@ namespace RINGMesh {
     {
         PointSetMeshFactory2D::register_creator< GeogramPointSetMesh2D >(
             GeogramPointSetMesh2D::type_name_static() );
-        PointSetMeshBuilderFactory2D::register_creator< GeogramPointSetMeshBuilder2D >(
-            GeogramPointSetMesh2D::type_name_static() );
         LineMeshFactory2D::register_creator< GeogramLineMesh2D >(
             GeogramLineMesh2D::type_name_static() );
-        LineMeshBuilderFactory2D::register_creator< GeogramLineMeshBuilder2D >(
-            GeogramLineMesh2D::type_name_static() );
-        SurfaceMeshBuilderFactory2D::register_creator< GeogramSurfaceMeshBuilder2D >(
-            GeogramSurfaceMesh2D::type_name_static() );
-        SurfaceMeshBuilderFactory2D::register_creator< GeogramSurfaceMeshBuilder2D >(
+        SurfaceMeshFactory2D::register_creator< GeogramSurfaceMesh2D >(
             GeogramSurfaceMesh2D::type_name_static() );
 
         PointSetMeshFactory3D::register_creator< GeogramPointSetMesh3D >(
             GeogramPointSetMesh3D::type_name_static() );
-        PointSetMeshBuilderFactory3D::register_creator< GeogramPointSetMeshBuilder3D >(
-            GeogramPointSetMesh3D::type_name_static() );
         LineMeshFactory3D::register_creator< GeogramLineMesh3D >(
             GeogramLineMesh3D::type_name_static() );
+        SurfaceMeshFactory3D::register_creator< GeogramSurfaceMesh3D >(
+            GeogramSurfaceMesh3D::type_name_static() );
+        VolumeMeshFactory3D::register_creator< GeogramVolumeMesh3D >(
+            GeogramVolumeMesh3D::type_name_static() );
+
+        PointSetMeshBuilderFactory2D::register_creator< GeogramPointSetMeshBuilder2D >(
+            GeogramPointSetMesh2D::type_name_static() );
+        LineMeshBuilderFactory2D::register_creator< GeogramLineMeshBuilder2D >(
+            GeogramLineMesh2D::type_name_static() );
+        SurfaceMeshBuilderFactory2D::register_creator< GeogramSurfaceMeshBuilder2D >(
+            GeogramSurfaceMesh2D::type_name_static() );
+
+        PointSetMeshBuilderFactory3D::register_creator< GeogramPointSetMeshBuilder3D >(
+            GeogramPointSetMesh3D::type_name_static() );
         LineMeshBuilderFactory3D::register_creator< GeogramLineMeshBuilder3D >(
             GeogramLineMesh3D::type_name_static() );
         SurfaceMeshBuilderFactory3D::register_creator< GeogramSurfaceMeshBuilder3D >(
             GeogramSurfaceMesh3D::type_name_static() );
-        SurfaceMeshBuilderFactory3D::register_creator< GeogramSurfaceMeshBuilder3D >(
-            GeogramSurfaceMesh3D::type_name_static() );
-        VolumeMeshFactory3D::register_creator< GeogramVolumeMesh3D >(
-            GeogramVolumeMesh3D::type_name_static() );
         VolumeMeshBuilderFactory3D::register_creator< GeogramVolumeMeshBuilder3D >(
             GeogramVolumeMesh3D::type_name_static() );
     }
