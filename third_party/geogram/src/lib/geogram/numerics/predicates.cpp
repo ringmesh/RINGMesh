ab5214513af79eadad03503df90ea8fc36aded88
Arnaud Botella <3213882+BotellaA@users.noreply.github.com>
2017-08-10 09:36:54 +0200

    Revert geogram predicates

--- third_party/geogram/src/lib/geogram/numerics/predicates.cpp
+++ third_party/geogram/src/lib/geogram/numerics/predicates.cpp
@@ -1246,7 +1246,6 @@ namespace {
 
         len_orient3d = geo_max(len_orient3d, Delta.length());
 
-        Delta.show( std::cout );
         return Delta.sign();
     }
 
@@ -1906,10 +1905,8 @@ namespace GEO {
             ) {
             cnt_orient3d_total++;
             Sign result = Sign(orient_3d_filter(p0, p1, p2, p3));
-            std::cout << result << '\n';
             if(result == 0) {
                 result = orient_3d_exact(p0, p1, p2, p3);
-                std::cout << result << '\n';
             }
             return result;
         }
