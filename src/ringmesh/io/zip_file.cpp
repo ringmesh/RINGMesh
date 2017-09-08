ad1ef32b6760ecf5f223ad859330cb3cc5349d41
bonneau <24669995+francoisbonneau@users.noreply.github.com>
2017-09-08 10:01:54 +0200

    remove do_create_polygons()

---
+++ src/ringmesh/io/zip_file.cpp
@@ -0,0 +1,230 @@
+/*
+ * Copyright (c) 2012-2017, Association Scientifique pour la Geologie et ses
+ * Applications (ASGA). All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of ASGA nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL ASGA BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *     http://www.ring-team.org
+ *
+ *     RING Project
+ *     Ecole Nationale Superieure de Geologie - GeoRessources
+ *     2 Rue du Doyen Marcel Roubault - TSA 70605
+ *     54518 VANDOEUVRE-LES-NANCY
+ *     FRANCE
+ */
+
+#include <ringmesh/io/zip_file.h>
+
+#include <minizip/unzip.h>
+#include <minizip/zip.h>
+
+#include <geogram/basic/file_system.h>
+
+#include <ringmesh/basic/pimpl_impl.h>
+
+#define MAX_FILENAME 512
+#define READ_SIZE 8192
+
+/*!
+ * @file Manage zip files
+ * @author Arnaud Botella
+ */
+
+namespace RINGMesh
+{
+    class ZipFile::Impl
+    {
+    public:
+        Impl( const std::string& filename )
+        {
+            zip_file_ = zipOpen( filename.c_str(), APPEND_STATUS_CREATE );
+            if( zip_file_ == nullptr )
+            {
+                throw RINGMeshException(
+                    "ZipFile", "Could not read file ", filename );
+            }
+        }
+
+        ~Impl()
+        {
+            zipClose( zip_file_, NULL );
+        }
+
+        void add_file( const std::string& filename )
+        {
+            std::fstream file(
+                filename.c_str(), std::ios::in | std::ios::binary );
+            file.seekg( 0, std::ios::end );
+            auto size = static_cast< index_t >( file.tellg() );
+            file.seekg( 0, std::ios::beg );
+            std::vector< char > buffer( size );
+            file.read( &buffer[0], size );
+            zipOpenNewFileInZip( zip_file_, filename.c_str(), nullptr, nullptr,
+                0, nullptr, 0, nullptr, Z_DEFLATED, Z_DEFAULT_COMPRESSION );
+            zipWriteInFileInZip( zip_file_, size == 0 ? "" : &buffer[0], size );
+            zipCloseFileInZip( zip_file_ );
+            file.close();
+        }
+
+    private:
+        zipFile zip_file_{ nullptr };
+    };
+
+    ZipFile::ZipFile( const std::string& filename ) : impl_{ filename } {}
+
+    ZipFile::~ZipFile() {}
+
+    void ZipFile::add_file( const std::string& filename )
+    {
+        impl_->add_file( filename );
+    }
+
+    class UnZipFile::Impl
+    {
+    public:
+        Impl( const std::string& filename )
+        {
+            zip_file_ = unzOpen( filename.c_str() );
+            if( zip_file_ == nullptr )
+            {
+                throw RINGMeshException(
+                    "UnZipFile", "Could not read file ", filename );
+            }
+        }
+
+        ~Impl()
+        {
+            unzClose( zip_file_ );
+        }
+
+        void get_file( const std::string& filename )
+        {
+            unzLocateFile( zip_file_, filename.c_str(), 0 );
+            unzip_current_file( zip_file_, filename );
+        }
+
+        void start_extract()
+        {
+            if( unzGoToFirstFile( zip_file_ ) != UNZ_OK )
+            {
+                throw RINGMeshException(
+                    "UnZipFile", "Unable to uncompress the first file" );
+            }
+        }
+
+        void get_current_file()
+        {
+            unzip_current_file( zip_file_, get_current_filename().c_str() );
+        }
+
+        std::string get_current_filename()
+        {
+            char char_file_name[MAX_FILENAME];
+            if( unzGetCurrentFileInfo64( zip_file_, nullptr, char_file_name,
+                    MAX_FILENAME, nullptr, 0, nullptr, 0 )
+                != UNZ_OK )
+            {
+                throw RINGMeshException(
+                    "UnZipFile", "Unable to get file name" );
+            }
+            return { char_file_name };
+        }
+
+        bool next_file()
+        {
+            return unzGoToNextFile( zip_file_ ) == UNZ_OK;
+        }
+
+    private:
+        void unzip_current_file( unzFile uz, const std::string& filename )
+        {
+            char read_buffer[READ_SIZE];
+            if( unzOpenCurrentFile( uz ) != UNZ_OK )
+            {
+                unzClose( uz );
+                throw RINGMeshException( "UnZipFile", "Could not open file" );
+            }
+            FILE* out{ fopen( filename.c_str(), "wb" ) };
+            if( out == nullptr )
+            {
+                unzCloseCurrentFile( uz );
+                unzClose( uz );
+                throw RINGMeshException(
+                    "UnZipFile", "Could not open destination file" );
+            }
+            int error{ UNZ_OK };
+            do
+            {
+                error = unzReadCurrentFile( uz, read_buffer, READ_SIZE );
+                if( error < 0 )
+                {
+                    unzCloseCurrentFile( uz );
+                    unzClose( uz );
+                    fclose( out );
+                    throw RINGMeshException(
+                        "UnZipFile", "Invalid error: ", error );
+                }
+                if( error > 0 )
+                {
+                    fwrite( read_buffer, static_cast< std::size_t >( error ),
+                        std::size_t( 1 ), out );
+                }
+            } while( error > 0 );
+            fclose( out );
+            unzCloseCurrentFile( uz );
+        }
+
+    private:
+        unzFile zip_file_{ nullptr };
+    };
+
+    UnZipFile::UnZipFile( const std::string& filename ) : impl_{ filename } {}
+
+    UnZipFile::~UnZipFile() {}
+
+    void UnZipFile::get_file( const std::string& filename )
+    {
+        impl_->get_file( filename );
+    }
+
+    void UnZipFile::start_extract()
+    {
+        impl_->start_extract();
+    }
+
+    void UnZipFile::get_current_file()
+    {
+        impl_->get_current_file();
+    }
+
+    std::string UnZipFile::get_current_filename()
+    {
+        return impl_->get_current_filename();
+    }
+
+    bool UnZipFile::next_file()
+    {
+        return impl_->next_file();
+    }
+
+} // namespace RINGMesh
