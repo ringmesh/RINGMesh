4db67114a2a5de325e34d22baff13400470bf922
Pierre Anquez <pierre.anquez@univ-lorraine.fr>
2017-10-04 12:25:31 +0000

    well.h edited online with Bitbucket

--- include/ringmesh/mesh/well.h
+++ include/ringmesh/mesh/well.h
@@ -288,7 +288,7 @@ namespace RINGMesh
         /*!
          * Finds if a corner at a given geometric position exist
          * @param[in] vertex the geometric position to test
-         * @param[in] epsilon the numerical prÃ©cision used to compare the
+         * @param[in] epsilon the numerical précision used to compare the
          * vertices
          * @return the id of the corner or NO_ID if not found any corresponding
          * to \p p
@@ -398,7 +398,7 @@ namespace RINGMesh
     // --------------------------------------------------------------------------
 
     /*!
-     * @todo Comment
+     * Set of wells associated to a GeoModel
      */
     template < index_t DIMENSION >
     class RINGMESH_API WellGroup
