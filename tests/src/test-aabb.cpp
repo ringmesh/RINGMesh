d14e0d2d4adef30c6a5c017d23b24c20eba59b60
Arnaud Botella <3213882+BotellaA@users.noreply.github.com>
2017-10-13 08:35:12 +0200

    Split mesh/aabb files into basic/aabb and mesh/mesh_aabb

--- tests/src/test-aabb.cpp
+++ tests/src/test-aabb.cpp
@@ -40,7 +40,7 @@
 #include <ringmesh/basic/geometry.h>
 #include <ringmesh/basic/matrix.h>
 
-#include <ringmesh/mesh/aabb.h>
+#include <ringmesh/mesh/mesh_aabb.h>
 #include <ringmesh/mesh/geogram_mesh.h>
 #include <ringmesh/mesh/mesh_builder.h>
 
