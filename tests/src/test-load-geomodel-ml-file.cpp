9359eae9079c932ded3a1890aa70c8603250f540
Arnaud Botella <3213882+BotellaA@users.noreply.github.com>
2017-10-23 10:23:06 +0200

    Starting 'clean_zlib_build' branch

---
+++ tests/src/test-load-geomodel-ml-file.cpp
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2012-2017, Association Scientifique pour la Geologie et ses
+ * Applications (ASGA). All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of ASGA nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL ASGA BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *     http://www.ring-team.org
+ *
+ *     RING Project
+ *     Ecole Nationale Superieure de Geologie - GeoRessources
+ *     2 Rue du Doyen Marcel Roubault - TSA 70605
+ *     54518 VANDOEUVRE-LES-NANCY
+ *     FRANCE
+ */
+
+#include <ringmesh/ringmesh_tests_config.h>
+
+#include <ringmesh/geomodel/geomodel.h>
+#include <ringmesh/geomodel/geomodel_validity.h>
+#include <ringmesh/io/io.h>
+
+/*! Tests the loading and writing of a Gocad model (.ml) file.
+ * Loads a .ml file generated with Gocad, saves it, loads it again, saves it
+ * again and compares the two saved versions.
+ * @returns 0 if success or an error code if not.
+ * @author Arnaud Botella
+ */
+int main()
+{
+    using namespace RINGMesh;
+
+    try
+    {
+        default_configure();
+        // model filename to test
+        std::vector< std::string > input_model_file_name_list = {
+            "CloudSpin_fixed.ml", "modelA6.ml"
+        };
+        for( const std::string& input_model_name : input_model_file_name_list )
+        {
+            std::string input_model_file_name =
+                ringmesh_test_data_path + input_model_name;
+
+            Logger::out( "TEST", "GeoModel input test. Loading file ",
+                input_model_file_name );
+
+            GeoModel3D in;
+            bool loaded_model_is_valid =
+                geomodel_load( in, input_model_file_name );
+
+            if( !loaded_model_is_valid )
+            {
+                throw RINGMeshException( "RINGMesh Test",
+                    "Failed when loading model ", in.name(),
+                    ": the loaded model is not valid." );
+            }
+        }
+    }
+    catch( const RINGMeshException& e )
+    {
+        Logger::err( e.category(), e.what() );
+        return 1;
+    }
+    catch( const std::exception& e )
+    {
+        Logger::err( "Exception", e.what() );
+        return 1;
+    }
+    Logger::out( "TEST", "SUCCESS" );
+    return 0;
+}
