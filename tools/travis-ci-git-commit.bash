d4b43fc05b04ebf34eb1f3fb771084f1397351d4
Arnaud Botella <arnaud.botella@total.com>
2017-10-30 09:33:32 +0100

    another try without

--- tools/travis-ci-git-commit.bash
+++ tools/travis-ci-git-commit.bash
@@ -33,12 +33,9 @@ function travis-branch-commit() {
         err "failed to commit updates"
         return 1
     fi
-    local remote=origin
-    if [[ $GITHUB_TOKEN ]]; then
-        remote=https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG
-    fi
-    if ! git push --quiet --follow-tags "$remote" "$TRAVIS_BRANCH" > /dev/null 2>&1; then
+    if ! git push --quiet --follow-tags https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG "$TRAVIS_BRANCH" > /dev/null 2>&1; then
         err "failed to push git changes"
+        err $GITHUB_TOKEN
         return 1
     fi
 }
