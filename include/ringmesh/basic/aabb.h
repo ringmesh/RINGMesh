89bc9b617857bc507f6b1b921c632f71eb62af63
Pierre Anquez <pierre.anquez@univ-lorraine.fr>
2017-11-03 11:30:58 +0100

    Use a lambda expression

--- include/ringmesh/basic/aabb.h
+++ include/ringmesh/basic/aabb.h
@@ -434,13 +434,13 @@ namespace RINGMesh
             get_recursive_iterators( node_index2, element_begin2, element_end2,
                 middle_box2, child_left2, child_right2 );
             std::future< void > left_result = std::async( std::launch::async,
-                &AABBTree::self_intersect_recursive< ACTION >, this,
+                [&]{ AABBTree::self_intersect_recursive(
                 node_index1, element_begin1, element_end1, child_left2,
-                element_begin2, middle_box2, action );
+                element_begin2, middle_box2, action ); } );
             std::future< void > right_result = std::async( std::launch::async,
-                &AABBTree::self_intersect_recursive< ACTION >, this,
+                [&]{ AABBTree::self_intersect_recursive(
                 node_index1, element_begin1, element_end1, child_right2,
-                middle_box2, element_end2, action );
+                middle_box2, element_end2, action ); } );
             left_result.get();
             right_result.get();
         }
@@ -450,13 +450,13 @@ namespace RINGMesh
             get_recursive_iterators( node_index1, element_begin1, element_end1,
                 middle_box1, child_left1, child_right1 );
             std::future< void > left_result = std::async( std::launch::async,
-                &AABBTree::self_intersect_recursive< ACTION >, this,
+                [&]{ AABBTree::self_intersect_recursive(
                 child_left1, element_begin1, middle_box1, node_index2,
-                element_begin2, element_end2, action );
+                element_begin2, element_end2, action ); } );
             std::future< void > right_result = std::async( std::launch::async,
-                &AABBTree::self_intersect_recursive< ACTION >, this,
+                [&]{ AABBTree::self_intersect_recursive(
                 child_right1, middle_box1, element_end1, node_index2,
-                element_begin2, element_end2, action );
+                element_begin2, element_end2, action ); } );
             left_result.get();
             right_result.get();
         }
