c0a87f6352f9b1beaf062fc7aaaa87a0e9216b40
Arnaud Botella <arnaud.botella@total.com>
2017-11-24 08:57:37 +0100

    test

--- cmake/RINGMesh.cmake
+++ cmake/RINGMesh.cmake
@@ -208,6 +208,7 @@ if(CLANG_TIDY)
 endif()
 
 # additional target to perform clang-format run, requires clang-format
+message(STATUS "#####################################")
 find_program(CLANG_FORMAT "clang-format" REQUIRED)
 if(CLANG_FORMAT)
     message(STATUS "Configuring RINGMesh with clang-format")
