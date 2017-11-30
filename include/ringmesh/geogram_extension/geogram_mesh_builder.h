c1ce4176850646041344650c8a8b853a668eb7b3
Travis CI User <travis@example.org>
2017-11-30 22:54:08 +0000

    Travis CI update

--- include/ringmesh/geogram_extension/geogram_mesh_builder.h
+++ include/ringmesh/geogram_extension/geogram_mesh_builder.h
@@ -117,8 +117,7 @@ private:                                                                       \
     Class< DIMENSION >& mesh_
 
     template < index_t DIMENSION >
-    class GeogramPointSetMeshBuilder
-        : public PointSetMeshBuilder< DIMENSION >
+    class GeogramPointSetMeshBuilder : public PointSetMeshBuilder< DIMENSION >
     {
         COMMON_GEOGRAM_MESH_BUILDER_IMPLEMENTATION( GeogramPointSetMesh );
         ringmesh_template_assert_2d_or_3d( DIMENSION );
@@ -134,8 +133,7 @@ private:                                                                       \
     ALIAS_2D_AND_3D( GeogramPointSetMeshBuilder );
 
     template < index_t DIMENSION >
-    class GeogramLineMeshBuilder
-        : public LineMeshBuilder< DIMENSION >
+    class GeogramLineMeshBuilder : public LineMeshBuilder< DIMENSION >
     {
         COMMON_GEOGRAM_MESH_BUILDER_IMPLEMENTATION( GeogramLineMesh );
         ringmesh_template_assert_2d_or_3d( DIMENSION );
@@ -188,8 +186,7 @@ private:                                                                       \
     ALIAS_2D_AND_3D( GeogramLineMeshBuilder );
 
     template < index_t DIMENSION >
-    class GeogramSurfaceMeshBuilder
-        : public SurfaceMeshBuilder< DIMENSION >
+    class GeogramSurfaceMeshBuilder : public SurfaceMeshBuilder< DIMENSION >
     {
         COMMON_GEOGRAM_MESH_BUILDER_IMPLEMENTATION( GeogramSurfaceMesh );
         ringmesh_template_assert_2d_or_3d( DIMENSION );
@@ -282,8 +279,7 @@ private:                                                                       \
     ALIAS_2D_AND_3D( GeogramSurfaceMeshBuilder );
 
     template < index_t DIMENSION >
-    class GeogramVolumeMeshBuilder
-        : public VolumeMeshBuilder< DIMENSION >
+    class GeogramVolumeMeshBuilder : public VolumeMeshBuilder< DIMENSION >
     {
         COMMON_GEOGRAM_MESH_BUILDER_IMPLEMENTATION( GeogramVolumeMesh );
         ringmesh_template_assert_3d( DIMENSION );
