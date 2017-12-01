b90bfd4a0f3f4dfcb72cec8a89730288ab689bce
Arnaud Botella <arnaud.botella@univ-lorraine.fr>
2017-12-01 21:21:41 +0100

    test

--- include/ringmesh/basic/factory.h
+++ include/ringmesh/basic/factory.h
@@ -60,7 +60,7 @@ namespace RINGMesh
      *      std::unique_ptr< A > c = MyFactory::create( "C", 2, 8.6 );
      */
     template < typename Key, typename BaseClass, typename... Args >
-    class Factory
+    class basic_api Factory
     {
         static_assert( std::has_virtual_destructor< BaseClass >::value,
             "BaseClass must have a virtual destructor" );
