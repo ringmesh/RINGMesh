098f56cec0baaf49e14c940012110e93fe5a23d5
Arnaud Botella <arnaud.botella@univ-lorraine.fr>
2017-12-03 17:25:55 +0100

    change stol to stoll

--- tests/src/test-save-geomodel.cpp
+++ tests/src/test-save-geomodel.cpp
@@ -109,9 +109,9 @@ bool compare_double( std::string word1, std::string word2 )
     try
     {
         DEBUG( word1 );
-        auto value1 = std::stol( word1 );
+        auto value1 = std::stoll( word1 );
         DEBUG( word2 );
-        auto value2 = std::stol( word2 );
+        auto value2 = std::stoll( word2 );
         return std::abs( value1 - value2 ) <= MAX_DIFF_DIGIT;
     }
     catch( const std::invalid_argument& )
