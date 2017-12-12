7e4ce35b3139a7f994f289aa0c0f241f354aff8f
Benjamin Chauvin <chauvineps@fas.harvard.edu>
2017-12-12 15:22:39 -0500

    revert an entire file

--- third_party/geogram/src/lib/geogram/mesh/mesh_io.cpp
+++ third_party/geogram/src/lib/geogram/mesh/mesh_io.cpp
@@ -3615,6 +3615,7 @@ namespace GEO {
 namespace GEO {
 
     GEO::Process::spinlock lock = {0};
+
     MeshIOFlags::MeshIOFlags() {
         dimension_ = 3;
         attributes_ = MESH_NO_ATTRIBUTES;
@@ -3627,9 +3628,11 @@ namespace GEO {
         const std::string& filename, Mesh& M,
         const MeshIOFlags& ioflags
     ) {
+        GEO::Process::acquire_spinlock( lock );
         Logger::out("I/O")
             << "Loading file " << filename << "..."
             << std::endl;
+        GEO::Process::release_spinlock( lock );
 
         M.clear();
 
@@ -3679,7 +3682,9 @@ namespace GEO {
             }
         }
 
+        GEO::Process::acquire_spinlock( lock );
         M.show_stats("I/O");
+        GEO::Process::release_spinlock( lock );
         
         return true;
     }
@@ -3688,9 +3693,11 @@ namespace GEO {
         const Mesh& M, const std::string& filename,
         const MeshIOFlags& ioflags
     ) {
+        GEO::Process::acquire_spinlock( lock );
         Logger::out("I/O")
             << "Saving file " << filename << "..."
             << std::endl;
+        GEO::Process::release_spinlock( lock );
 
         MeshIOHandler_var handler = MeshIOHandler::get_handler(filename);
         if(handler != nil && handler->save(M, filename, ioflags)) {
