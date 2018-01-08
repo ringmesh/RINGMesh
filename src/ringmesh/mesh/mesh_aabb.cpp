cfb4987ec82f73444e3aa271ef2bbcf51c316152
Pierre Anquez <32702237+panquez@users.noreply.github.com>
2018-01-08 17:27:24 +0100

    Happy new year 2018 (#121)

--- src/ringmesh/mesh/mesh_aabb.cpp
+++ src/ringmesh/mesh/mesh_aabb.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2017, Association Scientifique pour la Geologie et ses
+ * Copyright (c) 2012-2018, Association Scientifique pour la Geologie et ses
  * Applications (ASGA). All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
