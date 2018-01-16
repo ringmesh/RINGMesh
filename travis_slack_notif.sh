16673c0042c017081d59165d21b362642388b7ff
Pierre Anquez <32702237+panquez@users.noreply.github.com>
2018-01-16 11:12:26 +0100

    Update travis_slack_notif.sh

--- travis_slack_notif.sh
+++ travis_slack_notif.sh
@@ -7,7 +7,9 @@ body='{
 curl -X POST \
   --data-urlencode "payload={ \
   \"channel\": \"#travis\", \
-  \"username\": \"webhookbot\", \
-  \"text\": \"This is posted to #travis and comes from a bot named webhookbot.\", \
+  \"username\": \"Travis CI\", \
+  \"on_success\": \"never\", \
+  \"on_failure\": \"always\", \
+  \"text\": \"This is posted to #travis channel and comes from a bot named Travis CI.\", \
   \"icon_emoji\": \":x:\"}" \
   https://hooks.slack.com/services/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
