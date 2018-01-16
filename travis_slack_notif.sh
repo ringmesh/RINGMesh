363ad6c4f6ec6caa2106bcea72a4b17645af7719
Pierre Anquez <32702237+panquez@users.noreply.github.com>
2018-01-16 11:51:33 +0100

    Update travis_slack_notif.sh

--- travis_slack_notif.sh
+++ travis_slack_notif.sh
@@ -9,5 +9,5 @@ curl -X POST \
   --data-urlencode "payload={ \
   \"channel\": \"#travis\", \
   \"username\": \"Travis CI\", \
-  \"text\": \"Problem with $travis_job_id and $travis_job_number.\"}" \
+  \"text\": \"Problem on master: see <https://travis-ci.org/ringmesh/RINGMesh/jobs/329406106|Failed job> $travis_job_id and $travis_job_number.\"}" \
   https://hooks.slack.com/services/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
