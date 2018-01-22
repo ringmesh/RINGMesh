b614370ae7d29f861d29477889ed7b2127d77e02
Zakari <zakari@geor-eg-per-l13.univ-lorraine.fr>
2018-01-22 17:13:56 +0100

    sortie classe MeshBase de mesh + MAJ CMakeList

--- src/ringmesh/mesh/mesh.cpp
+++ src/ringmesh/mesh/mesh.cpp
@@ -43,24 +43,25 @@
 #include <stack>
 
 #include <ringmesh/mesh/mesh_index.h>
+#include <ringmesh/mesh/mesh_base.h>
 
 namespace RINGMesh
 {
-    template < index_t DIMENSION >
-    const NNSearch< DIMENSION >& MeshBase< DIMENSION >::vertex_nn_search() const
-    {
-        if( !vertex_nn_search_ )
-        {
-            std::vector< vecn< DIMENSION > > vec_vertices( nb_vertices() );
-            for( auto v : range( nb_vertices() ) )
-            {
-                vec_vertices[v] = vertex( v );
-            }
-            vertex_nn_search_.reset(
-                new NNSearch< DIMENSION >( vec_vertices, true ) );
-        }
-        return *vertex_nn_search_.get();
-    }
+//    template < index_t DIMENSION >
+//    const NNSearch< DIMENSION >& MeshBase< DIMENSION >::vertex_nn_search() const
+//    {
+//        if( !vertex_nn_search_ )
+//        {
+//            std::vector< vecn< DIMENSION > > vec_vertices( nb_vertices() );
+//            for( auto v : range( nb_vertices() ) )
+//            {
+//                vec_vertices[v] = vertex( v );
+//            }
+//            vertex_nn_search_.reset(
+//                new NNSearch< DIMENSION >( vec_vertices, true ) );
+//        }
+//        return *vertex_nn_search_.get();
+//    }
 
     template < index_t DIMENSION >
     std::unique_ptr< PointSetMesh< DIMENSION > >
