4346970b81ac1f3696dd29c8b4fe508f8277a779
Arnaud Botella <arnaud.botella@total.com>
2018-01-23 11:39:30 +0100

    another try

--- include/ringmesh/geogram_extension/geogram_mesh.h
+++ include/ringmesh/geogram_extension/geogram_mesh.h
@@ -117,7 +117,7 @@ protected:                                                                     \
     std::unique_ptr< GEO::Mesh > mesh_
 
     template < index_t DIMENSION >
-    class geogram_extension_api GeogramPointSetMesh
+    class  GeogramPointSetMesh
         : public PointSetMesh< DIMENSION >
     {
         COMMON_GEOGRAM_MESH_IMPLEMENTATION( GeogramPointSetMesh );
@@ -126,7 +126,7 @@ protected:                                                                     \
     ALIAS_2D_AND_3D( GeogramPointSetMesh );
 
     template < index_t DIMENSION >
-    class geogram_extension_api GeogramLineMesh : public LineMesh< DIMENSION >
+    class  GeogramLineMesh : public LineMesh< DIMENSION >
     {
         COMMON_GEOGRAM_MESH_IMPLEMENTATION( GeogramLineMesh );
 
@@ -152,7 +152,7 @@ protected:                                                                     \
     ALIAS_2D_AND_3D( GeogramLineMesh );
 
     template < index_t DIMENSION >
-    class geogram_extension_api GeogramSurfaceMesh
+    class  GeogramSurfaceMesh
         : public SurfaceMesh< DIMENSION >
     {
         COMMON_GEOGRAM_MESH_IMPLEMENTATION( GeogramSurfaceMesh );
@@ -196,7 +196,7 @@ protected:                                                                     \
     ALIAS_2D_AND_3D( GeogramSurfaceMesh );
 
     template < index_t DIMENSION >
-    class geogram_extension_api GeogramVolumeMesh
+    class  GeogramVolumeMesh
         : public VolumeMesh< DIMENSION >
     {
         COMMON_GEOGRAM_MESH_IMPLEMENTATION( GeogramVolumeMesh );
