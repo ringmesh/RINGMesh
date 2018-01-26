fe270b06e051d623f709340a08c4eff8f20b9b80
Pierre Anquez <32702237+panquez@users.noreply.github.com>
2018-01-26 07:18:27 +0100

    Travis via incoming webhook (#138)
    * first try
    
    * update travis.yml
    
    * fix typo
    
    * Update travis_slack_notif.sh
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update travis_slack_notif.sh
    
    * Update travis_slack_notif.sh
    
    * Update .travis.yml
    
    * Update travis_slack_notif.sh
    
    * Update travis_slack_notif.sh
    
    * Update .travis.yml
    
    * Update travis_slack_notif.sh
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update travis_slack_notif.sh
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update travis_slack_notif.sh
    
    * Update .travis.yml
    
    * Update travis_slack_notif.sh
    
    * avoid notif when pull request
    
    * remove chmod
    
    * add shebang
    
    * Update .travis.yml
    
    * set quiet gcov log
    
    * add echo
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml

---
+++ travis_slack_notif.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+travis_job_id=$1
+travis_branch=$2
+  
+echo Script_OK  
+
+curl -X POST \
+  --data-urlencode "payload={ \
+  \"channel\": \"#travis\", \
+  \"username\": \"Travis CI\", \
+  \"text\": \"Problem on $travis_branch branch: see <https://travis-ci.org/ringmesh/RINGMesh/jobs/$travis_job_id|Failed job on Travis>.\"}" \
+  https://hooks.slack.com/services/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
