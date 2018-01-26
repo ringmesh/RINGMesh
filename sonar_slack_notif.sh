3e2899f2cc0ec16ecea4db1172a34bf2a6de2e4b
Pierre Anquez <32702237+panquez@users.noreply.github.com>
2018-01-26 15:56:37 +0100

    Create sonar_slack_notif.sh

---
+++ sonar_slack_notif.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+  
+echo Script_sonar_OK  
+
+curl -X POST \
+  --data-urlencode "payload={ \
+  \"channel\": \"#general\", \
+  \"username\": \"Sonar\", \
+  \"text\": \"Coverage: see https://sonarcloud.io/api/badges/gate?key=ringmesh and https://sonarcloud.io/dashboard/index/ringmesh"}" \
+https://hooks.slack.com/services/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
