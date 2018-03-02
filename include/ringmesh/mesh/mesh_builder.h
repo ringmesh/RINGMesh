e31c1764df981bc52445f8412b5b391e6b582c8e
Zakari <zakari@geor-eg-per-l13.univ-lorraine.fr>
2018-03-02 14:39:14 +0100

    corrected merge

--- include/ringmesh/mesh/mesh_builder.h
+++ include/ringmesh/mesh/mesh_builder.h
@@ -51,8 +51,13 @@ namespace RINGMesh
     FORWARD_DECLARATION_DIMENSION_CLASS( PointSetMesh );
     FORWARD_DECLARATION_DIMENSION_CLASS( LineMesh );
     FORWARD_DECLARATION_DIMENSION_CLASS( SurfaceMeshBase );
+    FORWARD_DECLARATION_DIMENSION_CLASS( SurfaceMesh );
+    FORWARD_DECLARATION_DIMENSION_CLASS( VolumeMesh );
 
+    struct CellLocalFacet;
     struct EdgeLocalVertex;
+    struct ElementLocalVertex;
+    struct PolygonLocalEdge;
 } // namespace RINGMesh
 
 namespace RINGMesh
@@ -580,6 +585,7 @@ namespace RINGMesh
                     vertex2polygon_local_vertex[vertex] = local_vertex_count;
                 }
             }
+
             local_vertex_count = 0;
             for( auto polygon : polygons_to_connect )
             {
