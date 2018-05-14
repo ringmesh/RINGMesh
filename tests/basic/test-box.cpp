bcc53888aafa990bcb0638d8817f3fb50899ffa4
Travis CI User <travis@example.org>
2018-05-14 08:32:54 +0000

    Travis CI format update

--- tests/basic/test-box.cpp
+++ tests/basic/test-box.cpp
@@ -33,11 +33,11 @@
  *     FRANCE
  */
 
-#include <ringmesh/ringmesh_tests_config.h>
+#include <iostream>
 #include <ringmesh/basic/box.h>
 #include <ringmesh/basic/logger.h>
+#include <ringmesh/ringmesh_tests_config.h>
 #include <tuple>
-#include <iostream>
 /*!
  * @author Marine Hubert
  */
@@ -55,146 +55,148 @@ namespace
 
 void test_boxes()
 {
-	 Logger::out( "TEST", "Test box functions" );
-	 Box3D B;
-	 vec3 pt0 {0,0,0};
-	 vec3 pt1 {1,1,1};
-	 vec3 pt2 {2,2,2};
-	 vec3 pt3 {3,3,3};
-	 vec3 pt4 {4,4,4};
+    Logger::out( "TEST", "Test box functions" );
+    Box3D B;
+    vec3 pt0{ 0, 0, 0 };
+    vec3 pt1{ 1, 1, 1 };
+    vec3 pt2{ 2, 2, 2 };
+    vec3 pt3{ 3, 3, 3 };
+    vec3 pt4{ 4, 4, 4 };
 
-	 // At initialization, initialized_ = false;
-	 if(B.initialized())
-	 {
-		 throw RINGMeshException("TEST", "Error in initialized" );
-	 }
+    // At initialization, initialized_ = false;
+    if( B.initialized() )
+    {
+        throw RINGMeshException( "TEST", "Error in initialized" );
+    }
 
-	 //Test addition of a point
-	 B.add_point( pt1 );
-	 if(B.min() != pt1 )
-	 {
-		 throw RINGMeshException("TEST", "Error in addition" );
-	 }
+    // Test addition of a point
+    B.add_point( pt1 );
+    if( B.min() != pt1 )
+    {
+        throw RINGMeshException( "TEST", "Error in addition" );
+    }
 
-	 if(B.max() != pt1 )
-	 {
-		 throw RINGMeshException("TEST", "Error in addition" );
-	 }
+    if( B.max() != pt1 )
+    {
+        throw RINGMeshException( "TEST", "Error in addition" );
+    }
 
-	 // Test initialized_ = true;
-	 if(!B.initialized())
-	 {
-		 throw RINGMeshException("TEST", "Error in initialized" );
-	 }
+    // Test initialized_ = true;
+    if( !B.initialized() )
+    {
+        throw RINGMeshException( "TEST", "Error in initialized" );
+    }
 
-	// Check min
-	B.add_point( pt3 );
+    // Check min
+    B.add_point( pt3 );
 
-	if(B.min() != pt1)
-	{
-		throw RINGMeshException("TEST", "Error in minimum" );
-	}
-	// Check max
-	if(B.max() != pt3)
-	{
-		throw RINGMeshException("TEST", "Error in maximum" );
-	}
+    if( B.min() != pt1 )
+    {
+        throw RINGMeshException( "TEST", "Error in minimum" );
+    }
+    // Check max
+    if( B.max() != pt3 )
+    {
+        throw RINGMeshException( "TEST", "Error in maximum" );
+    }
 
-	// Check center
-	vec3 center_test = pt2;
-	if (B.center() != center_test)
-	{
-		throw RINGMeshException("TEST", "Error in center" );
-	}
+    // Check center
+    vec3 center_test = pt2;
+    if( B.center() != center_test )
+    {
+        throw RINGMeshException( "TEST", "Error in center" );
+    }
 
-	// Check diagonal
-	vec3 diag = pt2;
+    // Check diagonal
+    vec3 diag = pt2;
 
-	if(B.diagonal()!= diag)
-	{
-		throw RINGMeshException("TEST", "Error in diagonal" );
-	}
+    if( B.diagonal() != diag )
+    {
+        throw RINGMeshException( "TEST", "Error in diagonal" );
+    }
 
-	// Check add_box
-	Box3D A;
-	A.add_box(B);
-	if((B.min()!=A.min()) || (A.max()!=B.max()))
-	{
-		throw RINGMeshException("TEST", "Error in add_box" );
-	}
+    // Check add_box
+    Box3D A;
+    A.add_box( B );
+    if( ( B.min() != A.min() ) || ( A.max() != B.max() ) )
+    {
+        throw RINGMeshException( "TEST", "Error in add_box" );
+    }
 
-	// Check clear
-	A.clear();
-	if (A.initialized())
-	{
-		throw RINGMeshException("TEST", "Error in clear" );
-	}
+    // Check clear
+    A.clear();
+    if( A.initialized() )
+    {
+        throw RINGMeshException( "TEST", "Error in clear" );
+    }
 
-	// Check bboxes_overlap
-	Box3D C;
+    // Check bboxes_overlap
+    Box3D C;
 
-	C.add_point(pt2);
-	C.add_point(pt0);
-	A.add_point(pt3);
-	A.add_point(pt1);
+    C.add_point( pt2 );
+    C.add_point( pt0 );
+    A.add_point( pt3 );
+    A.add_point( pt1 );
 
-	if(!C.bboxes_overlap(A))
-	{
-			throw RINGMeshException("TEST", "Error in overlap maximum" );
-		}
-	if(!A.bboxes_overlap(C))
-	{
-			throw RINGMeshException("TEST", "Error in overlap minimum" );
-		}
+    if( !C.bboxes_overlap( A ) )
+    {
+        throw RINGMeshException( "TEST", "Error in overlap maximum" );
+    }
+    if( !A.bboxes_overlap( C ) )
+    {
+        throw RINGMeshException( "TEST", "Error in overlap minimum" );
+    }
 
-	// Check bbox_union
-	Box3D E = A.bbox_union(C);
+    // Check bbox_union
+    Box3D E = A.bbox_union( C );
 
-	if ((E.min() != C.min()) || (E.max() != A.max()))
-	{
-		throw RINGMeshException("TEST", "Error in box union" );
-	}
+    if( ( E.min() != C.min() ) || ( E.max() != A.max() ) )
+    {
+        throw RINGMeshException( "TEST", "Error in box union" );
+    }
 
-	// Check bbox intersection
-	bool do_boxes_intersect;
-	Box3D F;
-	std::tie(do_boxes_intersect,F) = (C.bbox_intersection(A));
-	if(!do_boxes_intersect)
-	{
-		throw RINGMeshException("TEST", "Error in box intersection " );
-	}
-	if( (F.min()!= pt1) || (F.max()!= pt2))
-	{
-		throw RINGMeshException("TEST", "Error in box intersection" );
-	}
+    // Check bbox intersection
+    bool do_boxes_intersect;
+    Box3D F;
+    std::tie( do_boxes_intersect, F ) = ( C.bbox_intersection( A ) );
+    if( !do_boxes_intersect )
+    {
+        throw RINGMeshException( "TEST", "Error in box intersection " );
+    }
+    if( ( F.min() != pt1 ) || ( F.max() != pt2 ) )
+    {
+        throw RINGMeshException( "TEST", "Error in box intersection" );
+    }
 
-	// Check contains
-	if(!E.contains(pt0))
-	{
-		throw RINGMeshException("TEST", "Error in box contains" );
-	}
-	if(!E.contains(pt2))
-	{
-		throw RINGMeshException("TEST", "Error in box contains" );
-	}
-	if(E.contains(pt4))
-	{
-		throw RINGMeshException("TEST", "Error in box contains" );
-	}
+    // Check contains
+    if( !E.contains( pt0 ) )
+    {
+        throw RINGMeshException( "TEST", "Error in box contains" );
+    }
+    if( !E.contains( pt2 ) )
+    {
+        throw RINGMeshException( "TEST", "Error in box contains" );
+    }
+    if( E.contains( pt4 ) )
+    {
+        throw RINGMeshException( "TEST", "Error in box contains" );
+    }
 
-	//Check signed distance
-	if(A.signed_distance(pt2)!=-1)
-	{
-		throw RINGMeshException("TEST", "Error in box signed distance (inside)" );
-	}
-	if(A.signed_distance(pt1)!=0)
-	{
-		throw RINGMeshException("TEST", "Error in box signed distance (min)" );
-	}
-	if(A.signed_distance(pt4)!=3)
-	{
-		throw RINGMeshException("TEST", "Error in box signed distance (outside)" );
-	}
+    // Check signed distance
+    if( A.signed_distance( pt2 ) != -1 )
+    {
+        throw RINGMeshException(
+            "TEST", "Error in box signed distance (inside)" );
+    }
+    if( A.signed_distance( pt1 ) != 0 )
+    {
+        throw RINGMeshException( "TEST", "Error in box signed distance (min)" );
+    }
+    if( A.signed_distance( pt4 ) != 3 )
+    {
+        throw RINGMeshException(
+            "TEST", "Error in box signed distance (outside)" );
+    }
 }
 
 int main()
